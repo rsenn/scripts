2c2c3406b9e28bfdf62235e033e978c4e2913395
Roman Senn <roman.l.senn@gmail.com>
2013-09-20 22:29:59 +0200

    ...

--- functions/player-file.sh
+++ functions/player-file.sh
@@ -1,4 +1,15 @@
 player-file()
 { 
-  ( lsof -n $(pid-args "${@-mplayer}") 2> /dev/null 2> /dev/null 2> /dev/null 2> /dev/null | grep --color=auto --color=auto --color=auto ' REG ' | grep --color=auto --color=auto --color=auto -vE ' (mem|txt|DEL) ' | cut-lsof NAME |sed 's, ([^)]*)$,,' )
+  ( SED_SCRIPT=
+	while :; do 
+					case "$1" in
+									-H|--no*hidden) SED_SCRIPT="${SED_SCRIPT:+$SED_SCRIPT ;; }\\|/\\.|d" ; shift ;;
+									-P|--no*proc) SED_SCRIPT="${SED_SCRIPT:+$SED_SCRIPT ;; }\\|^/proc|d" ; shift ;;
+					-x|--exclude) SED_SCRIPT="${SED_SCRIPT:+$SED_SCRIPT ;; }\\|${2//*/.*}|d" ; shift 2  ;;
+					-x=*|--exclude=*) P=${1#*=}; SED_SCRIPT="${SED_SCRIPT:+$SED_SCRIPT ;; }\\|^"${P//"*"/".*"}"\$|d" ; shift   ;;
+					*) break ;;
+					esac
+	done
+	SED_SCRIPT="${SED_SCRIPT:+$SED_SCRIPT ;; }s| ([^)]*)\$||"
+	  lsof -n $(pid-args "${@-mplayer}") 2> /dev/null 2> /dev/null 2> /dev/null 2> /dev/null | grep  -E ' [0-9]+[^ ]* +REG ' | grep --color=auto -vE ' (mem|txt|DEL) ' | cut-lsof NAME |sed "$SED_SCRIPT" )
 }
