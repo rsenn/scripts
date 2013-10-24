08fc8554d21b830fe56d9ebbbc12983908313528
Roman Senn <roman.l.senn@gmail.com>
2013-10-24 02:40:45 +0000

    ...

--- filename-to-lower.sh
+++ filename-to-lower.sh
@@ -1,14 +1,25 @@
-#!/bin/sh
+#!/bin/bash
 
 for FILE; do
+
+[ -e "$FILE" ] || continue
+
   BASE=`basename "$FILE"`
   DIR=`dirname "$FILE"`
 
   LOWER=`echo "$BASE" | tr [:{upper,lower}:]`
 
+  TMP="$LOWER".tmpXXXXXX
+
   if [ "$LOWER" != "$BASE" ]; then
    (cd "$DIR"
-    mv -v -f "$BASE" "$LOWER"
+     TMPFILE=` mktemp "$TMP"`  
+     rm -f "$TMPFILE"
+
+    
+    mv -v -f "$BASE" "$TMPFILE"
+    mv -v -f "$TMPFILE" "$LOWER"
+
    )
   fi
 
