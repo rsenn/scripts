d30ba79cf0815d8155c52cb80a0bd69349812de8
Roman Senn <roman.l.senn@gmail.com>
2013-10-24 02:39:58 +0000

    ...

--- filename-tolower.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-for FILE; do 
-  BASE=`basename "$FILE"` DIR=`dirname "$FILE"`
-  LOWER=`echo "$BASE" | tr "[:upper:]" "[:lower:]"`
-  
-  if [ "$LOWER" != "$BASE" ]; then
-    mv -v "$DIR/$BASE" "$DIR/$LOWER" || break
-  fi
-done
--- make-archive.sh
+++ make-archive.sh
@@ -1,8 +1,18 @@
-#!/bin/sh
+#!/bin/bash
 
 LEVEL=3
+EXCLUDE="*.git* *~ *.stackdump"
+
+while :; do
+  case "$1" in
+    -[0-9]) LEVEL=${1#-}; shift ;;
+    -t) TYPE=$2; shift 2 ;;
+    *) break ;;
+  esac
+done
+
+ARCHIVE=${1-"../${PWD##*/}-`date ${DIR:+-r "$DIR"} +%Y%m%d`.${TYPE:-7z}"}
 DIR=${2-"."}
-ARCHIVE=${1-"../${PWD##*/}-`date +%Y%m%d`.7z"}
 
 bce()
 {
@@ -14,15 +24,35 @@ bci()
     (IFS=" "; : echo "EXPR: bci '$*'" 1>&2; bce "($*) + 0.5") | sed -u 's,\.[0-9]\+$,,'
 }
 
+create-list()
+{
+ (OUTPUT=
+  SWITCH="$1"
+  shift
+  for ARG; do
+    OUTPUT="${OUTPUT:+$OUTPUT }${SWITCH}'$ARG'"
+  done
+  echo "$OUTPUT"
+  )
+}
+dir-contents()
+{
+			case "$1" in 
+							.) ls -a -1 --color=no  |grep -v -E '^(\.|\.\.)$' |sort -u ;;
+			*) echo "$*" ;;
+			esac
+}
 
+set -f
 case "$ARCHIVE" in
-  *.7z) CMD='7z a -mx=$(( $LEVEL * 5 / 9 )) "$ARCHIVE" "$DIR"' ;;
-  *.zip) CMD='zip -${LEVEL} -r "$ARCHIVE" "$DIR"' ;;
-  *.rar) CMD='rar a -m$(($LEVEL * 5 / 9)) -r "$ARCHIVE" "$DIR"' ;;
-  *.txz|*.tar.xz) CMD='tar -cvJf "$ARCHIVE" "$DIR"' ;;
-  *.tgz|*.tar.gz) CMD='tar -cvzf "$ARCHIVE" "$DIR"' ;;
-  *.tbz2|*.tbz|*.tar.bz2) CMD='tar -cvjf "$ARCHIVE" "$DIR"' ;;
+  *.7z) CMD='${SEVENZIP:-7z} a -mx=$(( $LEVEL * 5 / 9 )) "$ARCHIVE" '$(create-list '-x!' $EXCLUDE)' "$DIR"' ;;
+  *.zip) CMD='zip -${LEVEL} -r "$ARCHIVE" "$DIR" '$(create-list '-x' $EXCLUDE)'' ;;
+  *.rar) CMD='rar a -m$(($LEVEL * 5 / 9)) -r '$(create-list '-x' $EXCLUDE)' "$ARCHIVE" "$DIR"' ;;
+	*.txz|*.tar.xz) CMD='tar -cvJf "$ARCHIVE" '$(create-list '--exclude=' $EXCLUDE)' $(dir-contents "$DIR")' ;;
+  *.tgz|*.tar.gz) CMD='tar -cvzf "$ARCHIVE" '$(create-list '--exclude=' $EXCLUDE)' $(dir-contents "$DIR")' ;;
+  *.tbz2|*.tbz|*.tar.bz2) CMD='tar -cvjf "$ARCHIVE" '$(create-list '--exclude=' $EXCLUDE)' $(dir-contents "$DIR")' ;;
 esac
-
+CMD='rm -vf "$ARCHIVE";'$CMD
+echo "CMD='$CMD'" 1>&2
 eval "(set -x; $CMD)" &&
 echo "Created archive '$ARCHIVE'"
