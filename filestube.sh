3961c8562b7c39ca5e2017d5b46c39bbc3ae496b
Roman Senn <roman.l.senn@gmail.com>
2013-11-16 18:04:37 +0100

    ...

--- filestube.sh
+++ filestube.sh
@@ -7,11 +7,20 @@ require xml
 X="+x"
 N=99
 Q=`url_encode_args q="$@"`
-(  CMD="curl -q -s \"http://www.filestube.com/look_for.html?${Q}&select=All&Submit=Search&${HOSTING:+hosting=${HOSTING}&}page=\""{`seq -s, 1 $N `}""
+IFS="
+ "
+(
+
+SEQ=$(set -- `seq -s , 1 $N`; IFS=","; echo "${*%,}")
+
+#echo "CMD='$CMD' SEQ='$SEQ'" 1>&2
+#eval "$CMD"
+CMD="curl -q --location -s \"http://www.filestube.com/look_for.html?${Q}&select=All&Submit=Search&${HOSTING:+hosting=${HOSTING}&}page=\"{$SEQ}"
 echo "$CMD" 1>&2
+
 eval "(set ${X:--x}; $CMD)") |
-sed -u 's,<a,\n&,g ; s,</a>,&\n,g' |
-sed -u -n 's,^<a href="\([^"]*.html\)".*resultsLink.*,http://www.filestube.com\1,p'   | 
+sed -u 's,<a,\n&,g ; s,</a>,&\n,g' | sed -n "/\"resultsLink\"/ s|\(href=\"\?\)\(/\)|\1http://www.filestube.com\2|p" |xml_get a href |
+#sed -u -n 's,^<a href="\([^"]*.html\)".*resultsLink.*,http://www.filestube.com\1,p'   | 
 while read -r LINK; do
 
     (   curl -s "$LINK" )  |xml_get 'pre id="copy_paste_links"[^>]*'
