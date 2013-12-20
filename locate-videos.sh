3ce9c24ccdecdd58b4f71797dafff526c960bef1
Roman Senn <roman.l.senn@gmail.com>
2013-12-20 07:13:40 +0100

    ...

--- locate-videos.sh
+++ locate-videos.sh
@@ -4,8 +4,19 @@ locate_videos()
 {
     (IFS="
  "; EXTS="avi flv wmv mpg mpeg mp4 mkv mov 3gp"
+ 
+  while :; do
+    case "$1" in
+      -c | --compl*) COMPLETE=true ; shift ;;
+      *) break ;;
+    esac
+  done
 
- locate -i -r '.*' |grep -iE "\.($(IFS='| '; set -- $EXTS; echo "$*"))(\.!..|\.part|)\$"
+  if [ "$COMPLETE" != true ]; then
+    TRAILING="(\.!..|\.part|)"
+  fi
+
+ locate -i -r '.*' |grep -iE "\.($(IFS='| '; set -- $EXTS; echo "$*"))${TRAILING}\$"
  )
 }
 
