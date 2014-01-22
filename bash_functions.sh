54b3990ddd012591264bc036525e8b48c7b14d69
Roman Senn <roman.l.senn@gmail.com>
2014-01-22 19:48:03 +0100

    ...

--- bash_functions.sh
+++ bash_functions.sh
@@ -263,6 +263,17 @@ count-in-dir()
  done)
 }
 
+count-lines()
+{ 
+    ( [ $# -le 0 ] && set -- -;
+    N=$#;
+    for ARG in "$@";
+    do
+        ( set -- $( (xzcat "$ARG" 2>/dev/null ||zcat "$ARG" 2>/dev/null || bzcat "$ARG" 2>/dev/null || cat "$ARG") | wc -l);
+        [ "$N" -le 1 ] && echo "$1" || printf "%10d %s\n" "$1" "$ARG" );
+    done )
+}
+
 count()
 { 
     local IFS="$newline";
