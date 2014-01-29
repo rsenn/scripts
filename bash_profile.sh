ad1604692a8d142f6f021ce83a13e7b731f8b2ac
Roman Senn <roman.l.senn@gmail.com>
2014-01-29 21:42:56 +0000

    ...

--- bash_profile.sh
+++ bash_profile.sh
@@ -112,7 +112,7 @@ set-prompt()
 
 case "${OS=`uname -o`}" in
    msys* | Msys* |MSys* | MSYS*)
-    MEDIAPATH="$CYGDRIVE/{a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z}" 
+    MEDIAPATH="$SYSDRIVE/{a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z}" 
    ;;
   *cygwin* |Cygwin* | CYGWIN*) 
     MEDIAPATH="$CYGDRIVE/{a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z}" 
