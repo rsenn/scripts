25709fcecb51cca92a32f065636a808c46eb87a2
Roman Senn <rsenn@vac-star.com>
2014-01-29 17:11:12 +0100

    ...

--- locate32.sh
+++ locate32.sh
@@ -47,7 +47,8 @@ esac
 [ "$WHOLE" = true ] && addopt -w
 [ "$LOOKPATH" ] && addopt -p "$(pathconv "$LOOKPATH" "\\")"
 
-SED_EXPR="s|\\\\\\\\|/|g"
+#SED_EXPR="s|\\\\\\\\|/|g"
+SED_EXPR="s|\\\\|/|g"
 #SED_EXPR="${SED_EXPR}; s|^a|A|; s|^b|B|; s|^c|C|; s|^d|D|; s|^e|E|; s|^f|F|; s|^g|G|; s|^h|H|; s|^i|I|; s|^j|J|; s|^k|K|; s|^l|L|; s|^m|M|; s|^n|N|; s|^o|O|; s|^p|P|; s|^q|Q|; s|^r|R|; s|^s|S|; s|^t|T|; s|^u|U|; s|^v|V|; s|^w|W|; s|^x|X|; s|^y|Y|; s|^z|Z|"
 SED_EXPR="${SED_EXPR}; s|^A|a|; s|^B|b|; s|^C|c|; s|^D|d|; s|^E|e|; s|^F|f|; s|^G|g|; s|^H|h|; s|^I|i|; s|^J|j|; s|^K|k|; s|^L|l|; s|^M|m|; s|^N|n|; s|^O|o|; s|^P|p|; s|^Q|q|; s|^R|r|; s|^S|s|; s|^T|t|; s|^U|u|; s|^V|v|; s|^W|w|; s|^X|x|; s|^Y|y|; s|^Z|z|"
 
