3f236f5c290c5ce45f65cf877aaf0bd3637d9cb0
Roman Senn <rsenn@vac-star.com>
2014-02-23 01:48:27 +0100

    ...

--- filestube.sh
+++ filestube.sh
@@ -16,7 +16,7 @@ IFS="
 USER_AGENT="Mozilla/5.0 (X11; Linux x86_64; rv:26.0) Gecko/20100101 Firefox/26.0"
 #SOCKS_PROXY="127.0.0.1:8123"
 
-DLCMD="curl --location -A '$USER_AGENT' ${HTTP_PROXY:+--proxy \"http://${HTTP_PROXY#*://}\"}"
+DLCMD="curl -q -s --location -A '$USER_AGENT' ${HTTP_PROXY:+--proxy \"http://${HTTP_PROXY#*://}\"}"
 #DLCMD="${HTTP_PROXY:+http_proxy=\"http://${HTTP_PROXY#*://}\" }wget -q -O - -U '$USER_AGENT'"
 #DLCMD="${HTTP_PROXY:+http_proxy=\"http://${HTTP_PROXY#*://}\" https_proxy=\"http://${HTTP_PROXY#*://}\" }lynx -source -useragent '$USER_AGENT' 2>/dev/null"
 #DLCMD="${HTTP_PROXY:+http_proxy=\"http://${HTTP_PROXY#*://}\" }links -source"
@@ -25,7 +25,7 @@ DLCMD="curl --location -A '$USER_AGENT' ${HTTP_PROXY:+--proxy \"http://${HTTP_PR
 SEQ=$(set -- `seq -s , 1 $N`; IFS=","; echo "${*%,}")
 #echo "CMD='$CMD' SEQ='$SEQ'" 1>&2
 
-#CMD="$DLCMD \"http://www.filestube.com/look_for.html?${Q}&select=All&Submit=Search&${HOSTING:+hosting=${HOSTING}&}page=\"{$SEQ}"
+#CMD="$DLCMD \"http://www.filestube.com/search.html?${Q}&select=All&Submit=Search&${HOSTING:+hosting=${HOSTING}&}page=\"{$SEQ}"
 CMD="$DLCMD \"http://www.filestube.to/query.html?${Q}&select=All&Submit=Search&${HOSTING:+hosting=${HOSTING}&}page=\"{$SEQ}"
 
 [ "${X:--x}" = -x ] && echo "+ $CMD" 1>&2; eval "$CMD"|
