f6986f36bf0721ab4a48ce782bb1ef7f2f519fbc
Roman Senn <roman.l.senn@gmail.com>
2014-02-23 06:04:24 +0100

    ...

--- bash_profile.sh
+++ bash_profile.sh
@@ -118,15 +118,24 @@ set-prompt()
 [ -d /cygdrive ]  && { CYGDRIVE="/cygdrive"; : ${OS="Cygwin"}; }
 [ -d /sysdrive ]  && SYSDRIVE="/sysdrive" || SYSDRIVE=
 
+currentpath()
+{
+   if [ "$PWD" != "${PWD#$HOME}" ]; then
+     echo "~${PWD#$HOME}"
+   else
+     $PATHTOOL -m "$PWD"
+   fi
+}
+
 case "${OS=`uname -o |head -n1`}" in
    msys* | Msys* |MSys* | MSYS*)
     MEDIAPATH="$SYSDRIVE/{a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z}" 
     PATHTOOL=msyspath
-    set-prompt '\e[32m\]\u@\h \[\e[33m\]`$PATHTOOL -m $PWD`\[\e[0m\]\n\$ '
+    set-prompt '\e[32m\]\u@\h \[\e[33m\]`currentpath`\[\e[0m\]\n\$ '
    ;;
   *cygwin* |Cygwin* | CYGWIN*) 
     MEDIAPATH="$CYGDRIVE/{a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z}" 
-   set-prompt '\[\e]0;${OS}\w\a\]\n\[\e[32m\]$USERNAME@${HOSTNAME%.*} \[\e[33m\]`$PATHTOOL -m $PWD`\[\e[0m\]\n\$ '
+   set-prompt '\[\e]0;${OS}\w\a\]\n\[\e[32m\]$USERNAME@${HOSTNAME%.*} \[\e[33m\]`currentpath`\[\e[0m\]\n\$ '
    PATHTOOL=cygpath
   ;;
 *) 
@@ -284,7 +293,7 @@ case "$MSYSTEM" in
   *MINGW32*) [ -d /mingw/bin ] && pathmunge /mingw/bin ;;
   *MINGW64*) [ -d /mingw64/bin ] && pathmunge /mingw64/bin ;;
   *)
-LS_COLORS='di=01;34:ln=01;36:pi=33:so=01;35:do=01;35:bd=33;01:cd=33;01:or=31;01:su=37:sg=30:tw=30:ow=34:st=37:ex=01;33:'
+LS_COLORS='di=01;34:ln=01;36:pi=33:so=01;35:do=01;35:bd=33;01:cd=33;01:or=31;01:ex=01;33:'
 export LS_COLORS
 ;;
 esac
