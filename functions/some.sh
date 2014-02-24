2147fbe5cd25121c13d9d24bb54b57bafd3922aa
Roman Senn <roman.l.senn@gmail.com>
2014-02-24 08:35:31 +0100

    ...

--- functions/some.sh
+++ functions/some.sh
@@ -2,13 +2,8 @@ some()
 { 
     eval "while shift
   do
-<<<<<<< HEAD
   case \"\$1\" in
     $1 ) return 0 ;;
-=======
-  case \"\`str_tolower \"\$1\"\`\" in
-    $(str_tolower "$1") ) return 0 ;;
->>>>>>> 920a4a7eb2d8d4ebe7a624d237d7d9aad809de43
   esac
   done
   return 1"
