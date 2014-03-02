48ab29a7b08586cb15e99b5463f60b1da9231ebc
Roman Senn <rsenn@vac-star.com>
2014-03-02 06:26:26 +0100

    ...

--- killall.sh
+++ killall.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-IFS="
-"
 while :; do
 	case "$1" in
 		 -*) KILLARGS="${KILLARGS+$KILLARGS
@@ -9,23 +7,46 @@ while :; do
 	esac
 done
 
-IFS="|$IFS"
+IFS=$' \t\r\n'
 
-if ps -X -Y -Z 2>&1 | grep -q '\-W'; then
-  PSARGS="-aW"
-else
-  PSARGS="axw"	
+if type wmic 2>/dev/null >/dev/null; then
+  PS="wmic"
+  PSARGS="process get ProcessID,CommandLine /VALUE"
+  PSFILTER=' | { while read -r LINE; do
+  case $LINE in
+   CommandLine=*) CMDLINE="${CMDLINE:+$CMDLINE }${LINE#*=}" ;;
+   ProcessId=*) echo ${LINE#*=} $CMDLINE; CMDLINE=; ;;
+  esac
+done; }'   
+elif type tlist 2>/dev/null >/dev/null; then
+  PS="tlist"
+  PSARGS="-c"
+  PSFILTER="2>&1 | sed ':lp; N; \$! { b lp; } ; s,\\n\\s\\+,\\t,g'"
+elif type ps 2>/dev/null >/dev/null; then
+  PS="ps"
+  if ps -X -Y -Z 2>&1 | grep -q '\-W'; then
+    PSARGS="-aW"
+  else
+    PSARGS="axw"	
+  fi
 fi
 
-
-if type kill.exe 2>/dev/null >/dev/null; then
-				KILL="kill.exe"
-				KILLARGS="$KILLARGS
+if type taskkill.exe 2>/dev/null >/dev/null; then
+  KILL="taskkill"
+  KILLARGS="$KILLARGS
+/F /PID"
+elif type kill.exe 2>/dev/null >/dev/null; then
+  KILL="kill.exe"
+  KILLARGS="$KILLARGS
 -f"
 fi
 
-PSOUT=`${PS-ps} $PSARGS`
-PSMATCH=` echo "$PSOUT" | grep -iE "($*)" `
+
+
+PATTERN=\(`IFS='|'; echo "$*"`\)
+
+PSOUT=`eval "$PS $PSARGS $PSFILTER"`
+PSMATCH=` echo "$PSOUT" | grep -i -E "$PATTERN" `
 PIDS=` echo "$PSMATCH" | sed -n "/${0##*/}/! s,^[^0-9]*\([0-9]\+\).*,\1,p"`
 
 if [ -z "$PIDS" ]; then
@@ -33,8 +54,7 @@ if [ -z "$PIDS" ]; then
   exit 2
 fi
 echo "$PSMATCH"
-set -x
-exec "${KILL:-kill}" $KILLARGS  $PIDS
 
-
-
+for PID in $PIDS; do
+  (set -x; "${KILL:-kill}" $KILLARGS $PID)
+done
