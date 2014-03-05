29128a793794cef88393de34c030a5286422b5eb
Roman Senn <rsenn@vac-star.com>
2014-03-05 22:20:04 +0100

    ...

--- bash_functions.sh
+++ bash_functions.sh
@@ -1990,7 +1990,11 @@ link-mpd-music-dirs()
 
 list-7z()
 { 
-    7z l "$1" | sed -n '/^\s*Date\s\+Time\s\+Attr/ {   :lp; N; $! b lp;  s/[^\n]*files[^\n]*folders$//; s/\n[- ]*\n/\n/g; s/\n[0-9][-0-9]\+\s\+[0-9:]\+\s\+[^ ]*[.[:alnum:]][^ ]*\+\s\s*\([0-9]\+\)\s\s/\n  /g; s/\n\s\+[0-9]\+\s\s*/\n  /g; s/\n\s\+/\n/g; s/^\s*Date\s\+Time[^\n]*//; s/\n[^/]*files[^/]*folders$//; p; }'
+  (FILTER="sed -n '/^\\s*Date\\s\\+Time\\s\\+Attr/ {   :lp; N; \$! b lp;  s/[^\\n]*files[^\\n]*folders\$//; s/\\n[- ]*\\n/\\n/g; s/\\n[0-9][-0-9]\\+\\s\\+[0-9:]\\+\\s\\+[^ ]*[.[:alnum:]][^ ]*\\+\\s\\s*\\([0-9]\\+\\)\\s\\s/\\n  /g; s/\\n\\s\\+[0-9]\\+\\s\\s*/\\n  /g; s/\\n\\s\\+/\\n/g; s/^\\s*Date\\s\\+Time[^\\n]*//; s/\\n[^/]*files[^/]*folders\$//; p; }'"
+  [ $# -gt 1 ] && FILTER="$FILTER | addprefix \"\$ARG: \""
+  for ARG; do  
+    7z l "$ARG" | eval "$FILTER"
+   done)
 }
 
 list-dotfiles()
