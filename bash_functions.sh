bfef3b4f31f4a818d61b2183dc788c1a88daf098
Roman Senn <roman.l.senn@gmail.com>
2014-04-04 03:15:02 +0200

    ...

--- bash_functions.sh
+++ bash_functions.sh
@@ -295,6 +295,14 @@ command-exists()
     type "$1" 2> /dev/null > /dev/null
 }
 
+compare-dirs()
+{
+     diff -ru "$@" | sed -n \
+         -e "/^Binary files/ s,^Binary files \(.*\) and \(.*\) differ,\1 \2,p" \
+         -e "s,^Only in \(.*\): \(.*\),\1/\2,p" \
+         -e "/^diff/ { N; /\n---/ { N; /\n+++/ { s,\n.*,, ;; s,^diff\s\+,, ;; s,^-[^ ]* ,,g ;; p } } }"
+}
+
 convert-boot-entries()
 {
   ([ -z "$FORMAT" ] && FORMAT="$1"
--- functions/compare-dirs.sh
+++ functions/compare-dirs.sh
@@ -3,6 +3,6 @@ compare-dirs()
      diff -ru "$@" | sed -n \
          -e "/^Binary files/ s,^Binary files \(.*\) and \(.*\) differ,\1 \2,p" \
          -e "s,^Only in \(.*\): \(.*\),\1/\2,p" \
-         -e "/^diff/ { N; /\n---/ { N; /\n+++/ { s,\n.*,, ;; s,^diff\s\+,, ;; s,^-[^ ]* ,, ;; p } } }"
+         -e "/^diff/ { N; /\n---/ { N; /\n+++/ { s,\n.*,, ;; s,^diff\s\+,, ;; s,^-[^ ]* ,,g ;; p } } }"
 }
 
