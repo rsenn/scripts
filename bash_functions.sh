bce52d24719cc67da6f9a6a6f1e25f78d3de92b1
Roman Senn <roman.l.senn@gmail.com>
2014-07-04 12:53:39 +0000

    ...

--- bash_functions.sh
+++ bash_functions.sh
@@ -3726,6 +3726,30 @@ symlink-lib()
     done )
 }
 
+tcp-check() {
+        (TIMEOUT=10
+        for ARG; do
+        HOST=${ARG%:*}
+        PORT=${ARG#$HOST:}
+        
+        if type tcping 2>/dev/null >/dev/null; then
+          CMD='tcping -q -t "$TIMEOUT" "$HOST" "$PORT"; echo "$?"'
+        else
+          CMD='echo -n |(nc -w 10 "$HOST" "$PORT" 2>/dev/null >/dev/null;  echo "$?")'
+        fi
+
+        RET=`eval "$CMD"`
+        
+        if [ "$RET" -eq 0 ]; then
+          echo "$HOST:$PORT"
+        fi
+
+        if [ $# -le 1 ]; then
+          exit "$RET"
+        fi
+      done)
+}
+
 tempnam()
 {
     local IFS=" $newline";
