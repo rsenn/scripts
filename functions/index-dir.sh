bfa8ac3f959c2f52322356c8add4bf5566643ad5
Roman Senn <roman.l.senn@gmail.com>
2014-08-22 22:56:03 +0200

    ...

--- functions/index-dir.sh
+++ functions/index-dir.sh
@@ -1,19 +1,18 @@
-index-dir()
-{
+index-dir () 
+{ 
     [ -z "$*" ] && set -- .;
     ( for ARG in "$@";
     do
         ( cd "$ARG";
-        if ! test -w "$PWD" ; then
-          echo "Cannot write to $PWD ..." 1>&2
-          exit
-        fi
+        if ! test -w "$PWD"; then
+            echo "Cannot write to $PWD ..." 1>&2;
+            exit;
+        fi;
         echo "Indexing directory $PWD ..." 1>&2;
-        TEMP=`mktemp /tmp/"${PWD##*/}XXXXXX.list"`
-        trap 'rm -f "$TEMP"; unset TEMP' EXIT
-        (list-r 2>/dev/null || list-recursive) >"$TEMP";
-        (install -m 644 "$TEMP" "$PWD/files.list" && rm -f "$TEMP") ||
-        mv -f "$TEMP" "$PWD/files.list";
+        TEMP=`mktemp "$PWD/XXXXXX.list"`;
+        trap 'rm -f "$TEMP"; unset TEMP' EXIT;
+        ( list-r 2> /dev/null || list-recursive ) > "$TEMP";
+        ( install -m 644 "$TEMP" "$PWD/files.list" && rm -f "$TEMP" ) || mv -f "$TEMP" "$PWD/files.list";
         wc -l "$PWD/files.list" 1>&2 );
     done )
 }
