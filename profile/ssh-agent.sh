53ac1d3ee5e90feacaccfd623f10a3974e6332fa
Roman Senn <roman.l.senn@gmail.com>
2014-08-22 21:36:46 +0200

    ...

--- profile/ssh-agent.sh
+++ profile/ssh-agent.sh
@@ -1,22 +1,24 @@
 if type ssh-agent 2>/dev/null >/dev/null; then
 
-	if [ -z "$SSH_AUTH_SOCK" -o ! -r "$SSH_AUTH_SOCK" ]; then
-		unset SSH_AUTH_SOCK
-	else
-		if ! ssh-add -l  >/dev/null 2>/dev/null; then
-			unset SSH_AUTH_SOCK
-		fi
-	fi
+  OLD_SSH_AUTH_SOCK="$SSH_AUTH_SOCK"
 
+	if [ -z "$SSH_AUTH_SOCK" -o ! -S "$SSH_AUTH_SOCK" ] || 
+		(! ssh-add -l  >/dev/null 2>/dev/null); then
+
+		if [ -n "$SSH_AUTH_SOCK" -a -e "$SSH_AUTH_SOCK" ]; then
+			echo "SSH_AUTH_SOCK=$SSH_AUTH_SOCK unusable" 1>&2
+		fi
+		unset SSH_AUTH_SOCK
+	fi
 
 	SSH_AGENT_PIDS=`pgrep -f ssh-agent`
 	if [ -n "$SSH_AGENT_PIDS" ]; then
 
-		(set -- $SSH_AGENT_PIDS
-		 echo "Got $# agent processes" 1>&2)
+#	 (set -- $SSH_AGENT_PIDS
+# 	 echo "Got $# agent processes" 1>&2)
 		for SSH_AGENT_PID in $SSH_AGENT_PIDS; do
 			SSH_AGENT_PID=`expr $SSH_AGENT_PID - 1`
-			echo "Checking PID $SSH_AGENT_PID" 1>&2
+#			echo "Checking PID $SSH_AGENT_PID" 1>&2
 
 			for S in /tmp/ssh-*/agent.$SSH_AGENT_PID; do
 				if [ -n "$S" -a -e "$S" -a -S "$S" ]; then
@@ -33,8 +35,7 @@ if type ssh-agent 2>/dev/null >/dev/null; then
 
 	if [ "${SSH_AUTH_SOCK-unset}" = unset ]; then
 
-		(set -- $SSH_AUTH_SOCKS
-		 echo "Got $# auth socks" 1>&2)
+#		(set -- $SSH_AUTH_SOCKS; echo "Got $# auth socks" 1>&2)
 
 		for SSH_AUTH_SOCK in $SSH_AUTH_SOCKS; do
 			[ -r "$SSH_AUTH_SOCK" -a -w "$SSH_AUTH_SOCK" ] && break
@@ -43,8 +44,12 @@ if type ssh-agent 2>/dev/null >/dev/null; then
 		if ! [ -r "$SSH_AUTH_SOCK" -a -w "$SSH_AUTH_SOCK" ]  ; then
 			eval `ssh-agent`
 			echo "Started new SSH agent (PID ${SSH_AUTH_SOCK##*.})" 1>&2
+		elif [ "$OLD_SSH_AUTH_SOCK" != "$SSH_AUTH_SOCK" ]; then
+		  echo "Changed SSH_AUTH_SOCK to $SSH_AUTH_SOCK" 1>&2	
 		fi
 	fi
 
-	unset SSH_AGENT_PIDS SSH_AUTH_SOCKS
+  export SSH_AUTH_SOCK
+
+	unset SSH_AGENT_PIDS SSH_AUTH_SOCKS OLD_SSH_AUTH_SOCK
 fi
