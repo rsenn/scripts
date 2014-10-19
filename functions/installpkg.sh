bc3c00fec59578583bd9b8972d68e08a9cbc9349
Roman Senn <roman.l.senn@gmail.com>
2014-10-19 00:16:35 +0000

    ...

--- functions/installpkg.sh
+++ functions/installpkg.sh
@@ -1,6 +1,11 @@
 installpkg() {
- (: ${PKGDIR="$PWD"}
-  for ARG; do
+ (IFS="
+"
+  ARGS="$*"
+  set -- $(ls -d /m*/*/pmagic/pmodules{/extra,} 2>/dev/null)
+  test -d "$1" && PKGDIR="$1"
+  : ${PKGDIR="$PWD"}
+  for ARG in $ARGS; do
      case "$ARG" in
        *://*) (cd "$PKGDIR"; wget -c "$ARG"); ARG="$PKGDIR/${ARG##*/}" ;;
      esac
