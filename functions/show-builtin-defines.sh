9c14771c3668fea55fe8dc131583497900f4c03e
Roman Senn <rls@crowdguard.org>
2014-10-25 01:13:28 +0200

    ...

--- functions/show-builtin-defines.sh
+++ functions/show-builtin-defines.sh
@@ -1,11 +1,11 @@
 show-builtin-defines() {
  (NARG=$#
-  CMD='"$ARG" -dM -E - <<<""'
+  CMD='"$ARG" -dM -E - <<EOF
+EOF'
   if [ "$NARG" -gt 1 ]; then
-    CMD="$CMD <<EOF | addprefix \"\$ARG\":"
+    CMD="$CMD | addprefix \"\$ARG\":"
   fi
   eval "for ARG; do
     $CMD
-EOF
   done")
 }
