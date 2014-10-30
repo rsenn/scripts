b73b2ecd76b617b26e7c6a07fc8da68f6e425687
Roman Senn <roman.l.senn@gmail.com>
2014-10-30 02:21:45 +0000

    ...

--- make-archive.sh
+++ make-archive.sh
@@ -77,12 +77,12 @@ case "$ARCHIVE" in
   *.7z) CMD='${SEVENZIP:-7za} a -mx=$(( $LEVEL * 5 / 9 )) "$ARCHIVE" '$(create-list '-x!' $EXCLUDE)' "$DIR"' ;;
   *.zip) CMD='zip -${LEVEL} -r "$ARCHIVE" "$DIR" '$(create-list '-x ' $EXCLUDE)' ' ;;
   *.rar) CMD='rar a -m$(($LEVEL * 5 / 9)) -r '$(create-list '-x' $EXCLUDE)' "$ARCHIVE" "$DIR"' ;;
-  *.txz|*.$TAR.xz) CMD='$TAR -cvJf "$ARCHIVE" '$(create-list '--exclude=' $EXCLUDE)' $(dir-contents "$DIR")' ;;
-  *.tlzma|*.$TAR.lzma) CMD='$TAR --lzma -cvf "$ARCHIVE" '$(create-list '--exclude=' $EXCLUDE)' $(dir-contents "$DIR")' ;;
-  *.tlz|*.$TAR.lzip) CMD='$TAR --lzip -cvf "$ARCHIVE" '$(create-list '--exclude=' $EXCLUDE)' $(dir-contents "$DIR")' ;;
-  *.tlzo|*.$TAR.lzo) CMD='$TAR --lzop -cvf "$ARCHIVE" '$(create-list '--exclude=' $EXCLUDE)' $(dir-contents "$DIR")' ;;
-  *.tgz|*.$TAR.gz) CMD='$TAR -cvzf "$ARCHIVE" '$(create-list '--exclude=' $EXCLUDE)' $(dir-contents "$DIR")' ;;
-  *.tbz2|*.tbz|*.$TAR.bz2) CMD='$TAR -cvjf "$ARCHIVE" '$(create-list '--exclude=' $EXCLUDE)' $(dir-contents "$DIR")' ;;
+  *.txz|*.tar.xz) CMD='$TAR -cvJf "$ARCHIVE" '$(create-list '--exclude=' $EXCLUDE)' $(dir-contents "$DIR")' ;;
+  *.tlzma|*.tar.lzma) CMD='$TAR --lzma -cvf "$ARCHIVE" '$(create-list '--exclude=' $EXCLUDE)' $(dir-contents "$DIR")' ;;
+  *.tlz|*.tar.lzip) CMD='$TAR --lzip -cvf "$ARCHIVE" '$(create-list '--exclude=' $EXCLUDE)' $(dir-contents "$DIR")' ;;
+  *.tlzo|*.tar.lzo) CMD='$TAR --lzop -cvf "$ARCHIVE" '$(create-list '--exclude=' $EXCLUDE)' $(dir-contents "$DIR")' ;;
+  *.tgz|*.tar.gz) CMD='$TAR -cvzf "$ARCHIVE" '$(create-list '--exclude=' $EXCLUDE)' $(dir-contents "$DIR")' ;;
+  *.tbz2|*.tbz|*.tar.bz2) CMD='$TAR -cvjf "$ARCHIVE" '$(create-list '--exclude=' $EXCLUDE)' $(dir-contents "$DIR")' ;;
 esac
 CMD='rm -vf "$ARCHIVE";'$CMD
 echo "CMD='$CMD'" 1>&2
