b5eef75589849fc64321919a218956d8ffe93450
Roman Senn <roman.l.senn@gmail.com>
2014-11-01 00:56:23 +0100

    ...

--- rxvt.sh
+++ rxvt.sh
@@ -1,6 +1,19 @@
-#!/bin/bash
+#!/bin/sh
 
 FN="-misc-fixed-medium-r-semicondensed--13-120-75-75-c-60-iso8859-15"
 FB="$FN"
+IFS="
+"
+ARGS="$*"
 
-exec /usr/bin/rxvt +sb -rv -fn "$FN" -fb "$FB" -title Terminal -ls -bg gray
+for RXVT in `file {/usr,}/*bin/*rxvt* |grep ':.*executable' |grep -v ':.*script' | cut -d: -f1 | sort -r`; do
+	if "$RXVT" -help 2>&1 | grep -i -q "Usage:"; then
+		break
+	fi
+done
+
+set -- +sb -rv -fn "$FN" -fb "$FB" -title Terminal -ls -bg gray
+
+[ "$ARGS" ] && set -- "$@" -e $ARGS
+
+exec "$RXVT" "$@"
