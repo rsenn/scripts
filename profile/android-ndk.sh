7b3086b7ea974eaa7aba6bedec45c6d4beaf7cb7
Roman Senn <roman.l.senn@gmail.com>
2014-11-01 00:56:59 +0100

    ...

---
+++ profile/android-ndk.sh
@@ -0,0 +1,14 @@
+ANDROID_NDK_PATH=/opt/android-ndk-r10b
+ANDROID_NDK_ROOT=$ANDROID_NDK_PATH
+#PATH="$PATH:$ANDROID_NDK_ROOT"
+ANDROID_SYSROOT=$ANDROID_NDK_ROOT/platforms/android-17/arch-arm/
+JAVA_HOME=/usr/lib/jvm/java-openjdk
+
+for DIR in $ANDROID_NDK_ROOT/toolchains/{arm,x86,mipsel}*-4.8/prebuilt/linux-`uname -i`/bin; do
+  test -d "$DIR" || continue
+	if ! echo ${PATH} | /bin/grep -q "$DIR"; then
+		 PATH="$PATH:$DIR"
+	fi
+done
+
+export ANDROID_NDK_ROOT ANDROID_NDK_PATH ANDROID_NDK_SYSROOT PATH
---
+++ profile/qt.sh
@@ -0,0 +1,51 @@
+# Qt initialization script (sh)
+
+# In multilib environments there is a preferred architecture, 64 bit over 32 bit in x86_64,
+# ppc64. When a conflict is found between two packages corresponding with different arches,
+# the installed file is the one from the preferred arch. This is very common for executables
+# in /usr/bin, for example. If the file /usr/bin/foo is found  in an x86_64 package and in
+# an i386 package, the executable from x86_64 will be installe
+
+#if [ -z "${QTDIR}" ]; then
+
+  QTCLANG=true
+
+	case `uname -m` in
+		 x86_64 | ia64 | s390x | ppc64 | ppc64le)
+			 QT_PREFIXES="$( dirname /opt/Qt*/[0-9]*/*/bin | sort -r -V | grep 64 )
+/usr/lib64/qt-3.3
+/usr/lib/qt-3.3" ;;
+		 * )
+				QT_PREFIXES="$( dirname /opt/Qt*/[0-9]*/*/bin | sort -r -V | grep -v 64 )
+/usr/lib/qt-3.3
+/usr/lib64/qt-3.3" ;;
+	esac
+
+	if [ "$QTCLANG" = true ]; then
+		QT_PREFIXES=$(grep clang <<<"$QT_PREFIXES" )
+	fi
+
+	for QTDIR in ${QT_PREFIXES} ; do
+		test -d "${QTDIR}" && break
+	done
+	unset QT_PREFIXES
+
+	if ! echo ${PATH} | /bin/grep -q $QTDIR/bin ; then
+		 PATH=$QTDIR/bin:${PATH}
+	fi
+
+	QTCREATORDIR=${QTDIR%%/[0-9].*}
+	[ "$QTCREATORDIR" != "$QTDIR" ] && QTCREATORDIR="$QTCREATORDIR/Tools/QtCreator"
+  if [ -d "$QTCREATORDIR" ]; then
+		if ! echo ${PATH} | /bin/grep -q $QTCREATORDIR/bin ; then
+			 PATH=$QTCREATORDIR/bin:${PATH}
+		fi
+	fi
+	unset QTCREATORDIR
+
+	QTINC="$QTDIR/include"
+	QTLIB="$QTDIR/lib"
+
+	export QTDIR QTINC QTLIB PATH
+
+#fi
