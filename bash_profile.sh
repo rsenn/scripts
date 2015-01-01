20beb69b270ee5a8905a9bf029857ef9ab659cbf
Roman Senn <roman.l.senn@gmail.com>
2015-01-01 04:11:08 +0100

    ...

--- bash_profile.sh
+++ bash_profile.sh
@@ -323,11 +323,11 @@ esac
 #[ -d /sbin ] && pathmunge /sbin
 #[ -d /usr/sbin ] && pathmunge /usr/sbin
 
-pathremove /bin && pathmunge /bin
-pathremove /sbin && pathmunge /sbin
-pathremove /usr/bin && pathmunge /usr/bin
-pathremove /usr/sbin && pathmunge /usr/sbin
+pathremove /bin; pathmunge /bin
+pathremove /sbin; pathmunge /sbin
+pathremove /usr/bin; pathmunge /usr/bin
+pathremove /usr/sbin; pathmunge /usr/sbin
 
-pathremove /usr/local/bin && pathmunge /usr/local/bin 
-pathremove /usr/local/sbin && pathmunge /usr/local/sbin
+pathremove /usr/local/bin; pathmunge /usr/local/bin 
+pathremove /usr/local/sbin; pathmunge /usr/local/sbin
 
