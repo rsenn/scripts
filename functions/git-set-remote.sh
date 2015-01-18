b6f175b16c26845978e1d2463be6c14e33395d16
Roman Senn <roman.l.senn@gmail.com>
2015-01-18 12:57:29 +0100

    ...

--- functions/git-set-remote.sh
+++ functions/git-set-remote.sh
@@ -2,6 +2,13 @@ git-set-remote()
 {
   ( IFS="
 "
+  while :; do
+    case "$1" in
+      -f | --force) FORCE=true; shift ;;
+      *) break ;;
+    esac
+  done
+
   gsr-arg() {
    (unset DIR NAME REMOTE
     ARG="$*"
@@ -16,18 +23,22 @@ git-set-remote()
       *) NAME="$ARG";  shift ;;
     esac
       [ -n "$DIR" ] && echo "Setting git remote '$NAME' in '$DIR' to '$REMOTE'" 1>&2
-     eval "${PRECMD}git remote rm \"\$NAME\"" >&/dev/null
+      
+     eval "${PRECMD}git remote rm \"\$NAME\"" #2>/dev/null
+     true
 
      if [ -n "$REMOTE" ]; then
-     eval "${PRECMD}git remote add \"\$NAME\" \"\$REMOTE\""
-   fi
+       eval "${PRECMD}git remote add \"\$NAME\" \"\$REMOTE\""
+     fi
    )
      #   for NAME in $(git-get-remote | awkp ); do :; done
 
   }
   CMD='gsr-arg $R'
+  CMD="$CMD; R=\$?; [ \"\$FORCE\" = true -o \"\$R\" = 0 ] || exit \$R"
+
   if [ $# -le 0 ]; then
-    CMD='while read -r R; do '$CMD' || exit $? ; done'
+    CMD='while read -r R; do '$CMD'; done'
   else
     CMD='while [ $# -gt 0 ]; do 
       case "$1|$2|$3" in
@@ -37,7 +48,7 @@ git-set-remote()
         ?*\|?*\|*)   R="$1 $2"; S=2 ;;
         *\|*\|*)   R="$1"; S=2 ;;
       esac
-      '$CMD' || exit $?
+      '$CMD'
       echo "Shifting by $S" 1>&2
       [ "$S" -gt "$#" ]  && S=$#
       shift ${S:-1}
