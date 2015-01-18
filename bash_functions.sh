041ad575f020a395c3a0b914599037a8d16129ae
Roman Senn <roman.l.senn@gmail.com>
2015-01-18 20:48:23 +0000

    ...

--- bash_functions.sh
+++ bash_functions.sh
@@ -1868,7 +1868,7 @@ index-dir()
         echo "Indexing directory $PWD ..." 1>&2;
         TEMP=`mktemp "$PWD/XXXXXX.list"`;
         trap 'rm -f "$TEMP"; unset TEMP' EXIT;
-        ( type list-r 2>/dev/null >/dev/null && list-r 2>/dev/null || list-recursive ) > "$TEMP";
+        ( if type list-r 2>/dev/null >/dev/null; then  list-r 2>/dev/null; else list-recursive; fi) > "$TEMP";
         ( install -m 644 "$TEMP" "$PWD/files.list" && rm -f "$TEMP" ) || mv -f "$TEMP" "$PWD/files.list";
         wc -l "$PWD/files.list" 1>&2 );
     done )
