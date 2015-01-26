a64903034add082f55d082c61e6726f4cc0c4e04
Roman Senn <roman.l.senn@gmail.com>
2015-01-26 15:40:13 +0100

    ...

--- bin-dep.sh
+++ bin-dep.sh
@@ -2,10 +2,25 @@
 
 IFS="
 "
+get_libdirs() {
+	LIBDIRS=""
+	add_libdir() {
+		for ARG; do test -d "$ARG" &&
+		LIBDIRS="${LIBDIRS:+$LIBDIRS
+}${ARG%/}"
+    done
+  }
+	case "$1" in
+		*/usr/*) add_libdir ${1%/usr/*}{/usr/local,/usr,}/lib{64,,32}{,/*linux*/} ;;
+		*/*bin/*) add_libdir ${1%/*bin/*}{/usr/local,/usr,}/lib{64,,32}{,/*linux*/} ;;
+  esac
+	echo "$LIBDIRS"
+}
 
 get_deps() {
 
 	F=$(file "$1")
+	unset D
 
 	case "$F" in
 		*:\ *ELF*) D=`readelf -a "$1" | sed -n 's,.*Shared library:\s\+\[\(.*\)\].*,\1,p'`; test -n "$D" || D=`strings "$1" |grep '^lib.*\.so'`  ;;
@@ -16,7 +31,23 @@ get_deps() {
   if [ -z "$D" ]; then
     output "No library deps" 2
   else
-	  output "$D"
+
+		LIBDIRS=`get_libdirs "$1"` 
+
+	  for F in $D; do
+			case "$F" in
+				*/*) ;;
+					*)
+					for LIBDIR in $LIBDIRS; do 
+						if [ -e "$LIBDIR/$F" ]; then
+							O="$LIBDIR/$F"
+							break
+						fi
+					done
+			  ;;
+		  esac
+			output "${O:-$F}"
+		done
   fi
 }
 
