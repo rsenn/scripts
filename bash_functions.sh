288d0edebf06459cfaa408c8ca71a79e67ebef04
Roman Senn <roman.l.senn@gmail.com>
2015-01-27 01:03:15 +0100

    ...

--- bash_functions.sh
+++ bash_functions.sh
@@ -1335,6 +1335,14 @@ get-dotfiles()
     done )
 }
 
+get-frags() {
+  EXPR="'s/.*Average frag.*:\\s\\+\\([0-9]\\+\\)\\s\\+.*/\\1/p'"
+  [ $# -gt 1 ] && EXPR="$EXPR | sed \"s|^|\$ARG: |\""
+  eval "for ARG; do
+    contig -a \"\$ARG\" | sed -n $EXPR
+  done"
+}
+
 get-property()
 {
     sed -n "/$1=/ {
