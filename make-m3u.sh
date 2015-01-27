597433e370f8e5c9bd166c81bf862d9835bc5b4d
Roman Senn <roman.l.senn@gmail.com>
2015-01-27 10:42:25 +0100

    ...

--- make-m3u.sh
+++ make-m3u.sh
@@ -105,10 +105,12 @@ bitrate()
 
     test -n "$KBPS" && echo "$KBPS" || (
     R=0
-    set -- $(mminfo "$ARG" | sed -n "/Bit rate=/ { s,\s*Kbps\$,, ; s,\.[0-9]*\$,, ; s|^|$ARG:|; p }")
+    set -- $(mminfo "$ARG"  |sed -n "/[Oo]verall [bB]it [Rr]ate\s*=/ { s,\s*Kbps\$,, ;  s|\([0-9]\)\s\+\([0-9]\)|\1\2|g ; s,\.[0-9]*\$,, ; s,^[^=]*=,,; s|^|$ARG:|; p }")
+		#echo "BR: $*" 1>&2
    #echo "$*" 1>&2
-    for I; do R=` expr $R + ${I##*=}` ; done 2>/dev/null
-    [ "$N" -gt 1 ] && R="$ARG:$R"
+   # for I; do R=` expr $R + ${I##*=}` ; done 2>/dev/null
+	 R=${*##*:}
+   [ "$N" -gt 1 ] && R="$ARG:$R"
       echo "$R"
       )
   done )
