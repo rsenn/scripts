8c33ed1eedcb29f89e90dd0bc3239ea2a242a227
Roman Senn <roman.l.senn@gmail.com>
2015-01-27 10:26:19 +0100

    ...

--- make-m3u.sh
+++ make-m3u.sh
@@ -85,8 +85,8 @@ minfo()
 }
 
 resolution()
-{
- (minfo "$@"|sed -n "/Width: / { N; /Height:/ { s,Width:,, ; s,[^:\n0-9]\+: \+\([^:]*\)\$,\1,g; s,\n[^\n]*:,x,g; s|^\s*||; s|[^0-9]\+|x|g; p } }")
+{  
+  (minfo "$@"|sed -n "/Width\s*: / { N; /Height\s*:/ { s,Width\s*:,, ; s,[^:\n0-9]\+: \+\([^:]*\)\$,\1,g; s|^\s*||; s|\([0-9]\)\s\+\([0-9]\)|\1\2|g; s|\s*pixels||g;  s|\n|x|g; p } }")
 }
 
 bitrate()
