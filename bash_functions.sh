e96ced7e40e9d904264aff19db06981b5c97728c
Roman Senn <roman.l.senn@gmail.com>
2015-02-10 19:18:07 +0100

    ...

--- bash_functions.sh
+++ bash_functions.sh
@@ -2601,9 +2601,19 @@ ls-l()
 
 lsof-win()
 {
-  (for PID in $(ps -aW | sed 1d |awkp 1); do
-    handle -p "$PID" |sed "1d;2d;3d;4d;5d; s|^|$PID\\t|"
-  done)
+#  (for PID in $(ps -aW | sed 1d |awkp 1); do
+#    handle -p "$PID" |sed "1d;2d;3d;4d;5d; s|^|$PID\\t|"
+#  done)
+ (handle -a 2>&1 | { 
+  TAB="	"
+  CR=""
+  IFS="$CR"
+  while read -r LINE; do
+    case "$LINE" in
+      *"pid: "*) LSOF_PID=${LINE##*"pid: "}; LSOF_PID=${LSOF_PID%%" "*} ;;
+      *) printf "%d\t%s\n" "$LSOF_PID" "$LINE" ;;
+    esac
+  done; })
 }
 
 make-slackpkg()
