8df3493d0a4063f4585e7a2a953ba92336f9ce25
Roman Senn <roman.l.senn@gmail.com>
2015-02-15 06:06:46 +0100

    ...

--- bash_functions.sh
+++ bash_functions.sh
@@ -552,6 +552,34 @@ debug()
     msg "DEBUG: $@"
 }
 
+decompress-7z() {
+ (while :; do 
+    case "$1" in
+      -*) OPTS="${OPTS:+$OPTS${IFS:0:1}}$1"; shift ;;
+      *) break ;;
+    esac
+  done
+  NARG=$#
+
+  output() {
+    [ "$NARG" -gt 1 ] && echo "$ARCHIVE: $*" || echo "$*"
+  }
+  
+  [ $# -le 0 ] && set -- -
+
+  while [ $# -gt 0 ]; do
+   (case "$1" in 
+      *://*) INPUT="curl -s \"\$1\"" ;;
+      *) ARCHIVE=$1  ;;
+      -) OPTS="${OPTS:+$OPTS }-si" ;;
+    esac
+    OPTS=${OPTS:+$OPTS }"-so"
+    CMD="7z x \$OPTS ${ARCHIVE+\"\$ARCHIVE\"}"
+    eval "$CMD" )
+    shift
+  done)
+}
+
 decompress()
 {
     local mime="$(file -bi "$1")";
@@ -2306,25 +2334,35 @@ list-7z() {
   }
 
   while [ $# -gt 0 ]; do
-   (case "$1" in 
+   (B=${1##*/}
+    case "$1" in 
       *://*) INPUT="curl -s \"\$1\"" ;;
-
-    *) ARCHIVE=$1  ;;
+      *) ARCHIVE=$1  ;;
     esac
-    CMD="7z l -slt \$OPTS ${ARCHIVE+\"\$ARCHIVE\"}"
+    case "$1" in 
+      *.t?z | *.tbz2)
+        T=${1%.t?z}
+        T=${T%.tbz2} 
+        T=$T.tar
+        INPUT="${INPUT:+$INPUT | }7z x -so${ARCHIVE+ \"$ARCHIVE\"}"; OPTS="${OPTS:+$OPTS }-si${T}";  CMD="7z l -slt $OPTS"
+        ;;
+      *.tar.*) INPUT="${INPUT:+$INPUT | }7z x -so${ARCHIVE+ \"$ARCHIVE\"}"; OPTS="${OPTS:+$OPTS }-si${B%.*}";  CMD="7z l -slt $OPTS" ;;
+      *) CMD="7z l -slt $OPTS ${ARCHIVE+\"$ARCHIVE\"}" ;;
+    esac
     if [ -n "$INPUT" ]; then
       CMD="${INPUT+$INPUT | }$CMD"
       OPTS="$OPTS${IFS:0:1}-si${1##*/}"
     fi 
-    eval "$CMD" | 
+    ( #echo "CMD: $CMD" 1>&2 
+     eval "($CMD) 2>/dev/null" ) | 
     { IFS=" "; unset PREV; while read -r NAME EQ VALUE; do
         case "$NAME" in
           Path) F=${VALUE//"\\"/"/"} ;;
           Folder) [ "$VALUE" = + ] && FP="/"       ;;
           *) ;;
         esac
-        test -z "$NAME" && unset F FP
-        if test -n "$F$FP" -a "$F$FP" != "$PREV"; then
+        [ -z "$NAME" ] && unset F FP
+        if [ -n "$F$FP" -a "$F$FP" != "$PREV" ]; then
           output "$F$FP"
           PREV="$F$FP"
         fi
--- functions/list-7z.sh
+++ functions/list-7z.sh
@@ -12,25 +12,35 @@ list-7z() {
   }
 
   while [ $# -gt 0 ]; do
-   (case "$1" in 
+   (B=${1##*/}
+    case "$1" in 
       *://*) INPUT="curl -s \"\$1\"" ;;
-
-    *) ARCHIVE=$1  ;;
+      *) ARCHIVE=$1  ;;
     esac
-    CMD="7z l -slt \$OPTS ${ARCHIVE+\"\$ARCHIVE\"}"
+    case "$1" in 
+      *.t?z | *.tbz2)
+        T=${1%.t?z}
+        T=${T%.tbz2} 
+        T=$T.tar
+        INPUT="${INPUT:+$INPUT | }7z x -so${ARCHIVE+ \"$ARCHIVE\"}"; OPTS="${OPTS:+$OPTS }-si${T}";  CMD="7z l -slt $OPTS"
+        ;;
+      *.tar.*) INPUT="${INPUT:+$INPUT | }7z x -so${ARCHIVE+ \"$ARCHIVE\"}"; OPTS="${OPTS:+$OPTS }-si${B%.*}";  CMD="7z l -slt $OPTS" ;;
+      *) CMD="7z l -slt $OPTS ${ARCHIVE+\"$ARCHIVE\"}" ;;
+    esac
     if [ -n "$INPUT" ]; then
       CMD="${INPUT+$INPUT | }$CMD"
       OPTS="$OPTS${IFS:0:1}-si${1##*/}"
     fi 
-    eval "$CMD" | 
+    ( #echo "CMD: $CMD" 1>&2 
+     eval "($CMD) 2>/dev/null" ) | 
     { IFS=" "; unset PREV; while read -r NAME EQ VALUE; do
         case "$NAME" in
           Path) F=${VALUE//"\\"/"/"} ;;
           Folder) [ "$VALUE" = + ] && FP="/"       ;;
           *) ;;
         esac
-        test -z "$NAME" && unset F FP
-        if test -n "$F$FP" -a "$F$FP" != "$PREV"; then
+        [ -z "$NAME" ] && unset F FP
+        if [ -n "$F$FP" -a "$F$FP" != "$PREV" ]; then
           output "$F$FP"
           PREV="$F$FP"
         fi
