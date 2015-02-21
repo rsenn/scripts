a8f2d4d9b51e4e7ca13a494c41ee20a6334b3710
Roman Senn <rls@crowdguard.org>
2015-02-21 09:43:39 +0000

    ...

--- functions/get-frags.sh
+++ functions/get-frags.sh
@@ -1,7 +1,23 @@
 get-frags() {
-  EXPR="'s/.*Average frag.*:\\s\\+\\([0-9]\\+\\)\\s\\+.*/\\1/p'"
-  [ $# -gt 1 ] && EXPR="$EXPR | sed \"s|^|\$ARG: |\""
+ (while :; do
+    case "$1" in
+      -l | --left) LEFT=true; shift ;;
+      -x | --debug) DEBUG=true; shift ;;
+      *) break ;;
+    esac
+  done
+  FS="/" BS="\\"
+  EXPR="s/.*Average frag.*:\s\+\([0-9]\+\)\s\+.*/\1/"
+  if [ $# -gt 1 ]; then
+    "${LEFT:-false}" &&
+    EXPR="$EXPR ;; s/\$/ \${ARG//\$FS/\$BS\$FS}/" ||
+    EXPR="$EXPR ;; s/^/\${ARG//\$FS/\$BS\$FS}${SEP:-: }/"
+  fi
+  EXPR="/Average frag/ { $EXPR; p }"
+  CMD='contig -a "$ARG" | sed -n "'$EXPR'"'
+  CMD="($CMD) || return \$?"
+  "${DEBUG:-false}" && echo 1>&2 "CMD='$CMD'"
   eval "for ARG; do
-    (contig -a \"\$ARG\" | sed -n $EXPR) || return $?
-  done"
+   $CMD
+  done")
 }
