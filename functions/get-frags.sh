f2acedf1f98501cbf9ecf2886746e40761fa2559
Roman Senn <rls@crowdguard.org>
2015-02-21 09:44:21 +0000

    ...

--- functions/get-frags.sh
+++ functions/get-frags.sh
@@ -10,7 +10,7 @@ get-frags() {
   EXPR="s/.*Average frag.*:\s\+\([0-9]\+\)\s\+.*/\1/"
   if [ $# -gt 1 ]; then
     "${LEFT:-false}" &&
-    EXPR="$EXPR ;; s/\$/ \${ARG//\$FS/\$BS\$FS}/" ||
+    EXPR="$EXPR ;; s/\$/${SEP:- }\${ARG//\$FS/\$BS\$FS}/" ||
     EXPR="$EXPR ;; s/^/\${ARG//\$FS/\$BS\$FS}${SEP:-: }/"
   fi
   EXPR="/Average frag/ { $EXPR; p }"
