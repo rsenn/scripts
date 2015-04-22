53e74c9e22ae4d0d8463828ab1c677d3b236561a
Roman Senn <roman.l.senn@gmail.com>
2015-04-22 05:27:20 +0200

    ...

--- functions/bpm.sh
+++ functions/bpm.sh
@@ -1,16 +1,35 @@
-bpm()
-{
-    if ! type id3v2 2>/dev/null 1>/dev/null; then
-      get-bpm "$@"
-      return $?
-    fi
-    ( unset NAME;
-    if [ $# -gt 1 ]; then
-        NAME=":";
-    fi;
-    for ARG in "$@";
-    do
-        BPM=` id3v2 -l "$ARG" |sed -n 's,TBPM[^:]*:\s*,,p' `;
-        echo "${NAME+$ARG: }${BPM%.*}";
-    done )
+bpm() {
+  id3v2  -l "$@"|sed -n "/^id3v2 tag info for / {
+		:lp     
+		N
+		/\n[[:upper:][:digit:]]\+ ([^\n]*$/ {
+			/\nTBPM[^\n]*$/! {
+				s|\n[^\n]*$||
+				b lp
+			}
+			s|TBPM (.*): ||g
+			b ok
+		}
+		/:\s*$/! {
+			s|\n| |g
+			b lp
+		}
+		:ok     
+		s|\n[^\n]*:\s*$||
+		s|^id3v2 tag info for \([^\n]*\) *: *\n *|\1: |
+		p
+  }"
 }
+
+#bpm()
+#{
+#    ( unset NAME;
+#    if [ $# -gt 1 ]; then
+#        NAME=":";
+#    fi;
+#    for ARG in "$@";
+#    do
+#        BPM=` id3v2 -l "$ARG" |sed -n 's,TBPM[^:]*:\s*,,p' `;
+#        echo "${NAME+$ARG: }${BPM%.*}";
+#    done )
+#}
