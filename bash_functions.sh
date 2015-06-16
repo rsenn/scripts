838795c2e0a3408044a58039d0ad5de7acf1d25d
Roman Senn <roman.l.senn@gmail.com>
2015-06-16 23:40:44 +0200

    ...

--- bash_functions.sh
+++ bash_functions.sh
@@ -1497,6 +1497,15 @@ get-dotfiles()
     done )
 }
 
+get-exports() {
+ (N=$#
+  [ "$N" -gt 1 ] && PREFIX='$ARG: ' || PREFIX=''
+  CMD='dumpbin -exports "$ARG" |sed -n "/^\\s*ordinal\\s\\+name/ { n; :lp; n; s|^\\s*[0-9]*\\s\\+\\(.*\\)|'$PREFIX'\\1|p; /^\\s*\$/! b lp; }"'
+  for ARG; do
+    eval "$CMD"
+  done)
+}
+
 get-frags() {
  (while :; do
     case "$1" in
