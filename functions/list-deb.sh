4709e6ed17b4c0a39388c8246e66e4490192c038
Roman Senn <roman.l.senn@gmail.com>
2015-08-11 00:31:20 +0200

    ...

--- functions/list-deb.sh
+++ functions/list-deb.sh
@@ -1,14 +1,24 @@
-list-deb() 
-{ 
-  (for ARG in "$@";
-  do
-    (trap 'rm -rf "$TMPDIR"' EXIT QUIT TERM INT;
-    TMPDIR=$(mktemp -d);
-    mkdir -p "$TMPDIR";
-    ABSPATH=$(realpath "$ARG");
-    cd "$TMPDIR";
-    ar x "$ABSPATH";
-    set -- data.*;
-    tar -tf "$1" | removeprefix ./ | /bin/grep --color=auto --line-buffered -v '^\s*$');
-  done)
+list-deb() { 
+  NARG=$#
+  output() {
+    if [ -n "$*" -a "$#" -gt 0 ]; then
+      [ "$NARG" -gt 1 ] && echo "$ARCHIVE: $*" || echo "$*"
+    fi
+  }
+	for ARG in "$@"; do
+   (trap 'rm -rf "$TMPDIR"' EXIT QUIT TERM INT
+    TMPDIR=$(mktemp -d)
+    mkdir -p "$TMPDIR"
+    ABSPATH=$(realpath "$ARG")
+    cd "$TMPDIR"
+    ar x "$ABSPATH"
+    set -- data.*
+    tar -tf "$1" | while read -r LINE; do
+      LINE=${LINE#./}
+      case "$LINE" in
+        "") continue ;;
+        *) output "$LINE" ;;
+      esac
+		done)
+  done
 }
