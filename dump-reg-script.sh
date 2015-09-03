07a5ccad045d077ec615fca68886f42902323f04
Roman Senn <roman.l.senn@gmail.com>
2015-09-04 01:52:29 +0200

    ...

--- dump-reg-script.sh
+++ dump-reg-script.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
-MYNAME=`basename "${0%.sh}"`
+MYKEY=`basename "${0%.sh}"`
 NL="
 "
 BS="\\"
   
-#UNICODE_CHARSET="UTF-16LE"
-UNICODE_CHARSET="WINDOWS-1252"
-#BYTE_CHARSET="UTF-8"
-BYTE_CHARSET="CP850"
+UNICODE_CHARSET="UTF-16LE"
+#UNICODE_CHARSET="WINDOWS-1252"
+BYTE_CHARSET="UTF-8"
+#BYTE_CHARSET="CP850"
+
+#OUTPUT_CHARSET="WINDOWS-1252"
   
 push() {
   eval 'shift;'$1'="${'$1':+$'$1'
@@ -28,7 +30,12 @@ esac
 }
 
 reg_key_exists() {
- (reg query "$1" >&/dev/null
+ (case "$#":"$2" in
+		1:*) set reg query "$1" ;;
+		2:"") set reg query "$1" /ve ;;
+		2:*) set reg query "$1" /v "$2" ;;
+	esac
+  "$@" >&/dev/null
   R=$?
   exit $R)
 }
@@ -36,20 +43,24 @@ reg_key_exists() {
 clear_state() {
   if [ "$HEADER_DONE" != "true" ]; then
     echo "$BODY"
-  elif [ -n "$BODY" ] && match_name "$NAME"; then
+  elif [ -n "$BODY" ] && match_KEY "$KEY"; then
+   
     if [ "$LIST" = "true" ]; then
-      echo "$NAME" 
+      echo "$KEY" 
     else
-      echo "
-$BODY"
+      case "$BODY" in
+        *"$NL"*) ;;
+        *) BODY= ;;
+      esac
+      [ -n "$BODY" ] && echo "${BODY:+
+$BODY}"
     fi
   fi
 
-  NAME=
-  START_LINE=
-  BODY=
-  KEY=
-  EXCLUDE_KEY=false
+  KEY= VALUE= START_LINE=  BODY=
+  
+  EXCLUDE_KEY="false"
+  EXCLUDE_VALUE="false"
 }
 
 msg() {
@@ -80,7 +91,7 @@ for_each_line() {
 			fi
 			HEADER_DONE="true"
 			
-			NAME=${LINE#"["}; NAME=${NAME%"]"} 
+			KEY=${LINE#"["}; KEY=${KEY%"]"} 
 			START_LINE="$LINENO"
 
 			;;
@@ -93,11 +104,12 @@ for_each_line() {
 							
 			if exclude_keys "$KEY"; then
 				EXCLUDE_KEY="true"
-			elif ([ "$EXISTING" = "exclude" ] && reg_key_exists "$KEY") ||
-					 ([ "$EXISTING" = "include" ] && ! reg_key_exists "$KEY") ; then
+			elif [ "$CHECK_VALUES" != "true" ] &&
+			    (([ "$EXISTING" = "exclude" ] && reg_key_exists "$KEY") ||
+					 ([ "$EXISTING" = "include" ] && ! reg_key_exists "$KEY")) ; then
 				EXCLUDE_KEY="true"
 			else				
-				EXCLUDE_KEY=false	
+				EXCLUDE_KEY="false"	
 			fi
 				
 			if [ "$EXCLUDE_KEY" = "true" ]; then
@@ -108,9 +120,34 @@ for_each_line() {
 #          set -- $LINE; shift; push KEYS "$KEY"  
 #        fi
 		;;
-	esac
+  esac
+  
+  if [ -n "$KEY" ]; then
+		case "$LINE" in
+			\"*\"=*)
+				VALUE=${LINE%%=*}
+				VALUE=${VALUE#'"'}
+				VALUE=${VALUE%'"'}
+				
+				if [ "$CHECK_VALUES" = "true" ]; then
+				
+				  if ([ "$EXISTING" = "exclude" ] && reg_key_exists "$KEY" "$VALUE") ||
+					   ([ "$EXISTING" = "include" ] && ! reg_key_exists "$KEY" "$VALUE"); then
+						EXCLUDE_VALUE="true"
+					else				
+						EXCLUDE_VALUE="false"
+					fi
+					
+									
+					if [ "$EXCLUDE_VALUE" = "true" ]; then
+						msg "Excluding value $KEY \"$VALUE\""
+					fi
+			  fi
+			;;
+		esac
+  fi
 	
-	if  [ "${EXCLUDE_KEY:-false}" = "true" ]; then
+	if  [ "${EXCLUDE_KEY:-false}" = "true" -o "${EXCLUDE_VALUE:-false}" = "true" ]; then
 		continue
 	fi
 	
@@ -119,34 +156,52 @@ for_each_line() {
 
 
 main() {
+  NPARAM=0
   while :; do
     case "$1" in
       -x | --debug) DEBUG="true"; shift ;;
+      -o | --output) OUTPUT="$2"; shift 2 ;; -o=* | --output=*) OUTPUT="${1#*=}"; shift ;; -o*) OUTPUT="${1#-?}"; shift ;;
+      -i | --input) INPUT="$2"; shift 2 ;; -i=* | --input=*) INPUT="${1#*=}"; shift ;; -i*) INPUT="${1#-?}"; shift ;;
       -l | --list) LIST="true"; shift ;;
-      -e | --include-exist*) EXISTING="include"; shift ;;
+      -v | --values) CHECK_VALUES="true"; shift ;;
+      -I | --include-exist*) EXISTING="include"; shift ;;
       -E | --exclude-exist*) EXISTING="exclude"; shift ;;
       -[xX]=* | --exclude-layer=*) push EXCLUDE_KEYS="${1#*=}"; shift ;;
       -[xX] | --exclude-layer) push EXCLUDE_KEYS="$2"; shift 2 ;;
       -[xX]*) push EXCLUDE_KEYS "${1#-?}"; shift ;;
-      *) break ;;
+      *) 
+         case "$NPARAM" in
+           0) INPUT="$1"; : $((NPARAM++)); shift ;;
+           1) OUTPUT="$1"; : $((NPARAM++)); shift ;;
+           *) break ;;
+         esac
+         ;;
     esac
+     [ $# -lt 1 ] && break
   done
 
-  if [ -f "$1" ]; then
-    exec <"$1"
-    shift
+  if [ -n "$INPUT" ]; then
+    exec <"$INPUT"
+    msg "Reading from '$INPUT' ..."
   fi
 
-  create_match_function "match_name" "$@"
+  if [ -n "$OUTPUT" ]; then
+    exec >"$OUTPUT"
+    msg "Writing to '$OUTPUT' ..."
+  fi
+
+  create_match_function "match_KEY" "$@"
   create_match_function "exclude_keys" ${EXCLUDE_KEYS:-"----"}
 
-  HEADER_DONE=false
+  HEADER_DONE="false"
   LINENO=0
 
   
   PROCESS_CMD="while getline LINE; do for_each_line; done"
   PROCESS_CMD="iconv -f $UNICODE_CHARSET -t $BYTE_CHARSET | ${PROCESS_CMD}"
-  PROCESS_CMD="${PROCESS_CMD} | iconv -f $BYTE_CHARSET -t $UNICODE_CHARSET"
+# PROCESS_CMD="${PROCESS_CMD} | iconv -f $BYTE_CHARSET -t $UNICODE_CHARSET"
+  PROCESS_CMD="${PROCESS_CMD} | sed -u '1 { s/.*Windows Registry Editor.*/REGEDIT4/  ;; }'"
+  
   
   eval "${PROCESS_CMD}"
 }
