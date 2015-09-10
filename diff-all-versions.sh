e8e8fdbbf7fa9c29d88dbeeb988f963576d80a90
Roman Senn <roman.l.senn@gmail.com>
2015-09-11 00:11:02 +0200

    ...

--- diff-all-versions.sh
+++ diff-all-versions.sh
@@ -9,18 +9,29 @@ get_name() {
   echo "${1%%[-_][0-9]*.*}"
 }
 
-NAME=`get_name "$1"`
-OLD_VERSION=`get_version "$1"`
+main() {
+  while :; do
+    case "$1" in
+      -p | --print*) PRINT_ONLY=true; shift ;;
+      *) break ;;
+    esac
+  done
+  
+	NAME=`get_name "$1"`
+	OLD_VERSION=`get_version "$1"`
 
-while [ $# -gt 1 ]; do
-  PREV="$1"
-  shift
-  THIS="$1"
-  NEW_VERSION=`get_version "$1"`
-  cmd="udiff.sh -ru '$PREV' '$THIS'"
-  
-  cmd="$cmd >'$NAME-${OLD_VERSION}-${NEW_VERSION}.diff'"
-  
-  echo "cmd: $cmd" 1>&2
-  OLD_VERSION=$NEW_VERSION
-done
+	while [ $# -gt 1 ]; do
+		PREV="$1"
+		shift
+		THIS="$1"
+		NEW_VERSION=`get_version "$1"`
+		cmd="udiff.sh -ru '$PREV' '$THIS'"
+		
+		cmd="$cmd >'$NAME-${OLD_VERSION}-to-${NEW_VERSION}.diff'"
+		
+		echo "cmd: $cmd" 1>&2
+		[ "$PRINT_ONLY" = true ] || eval "$cmd"
+		
+		OLD_VERSION=$NEW_VERSION
+	done
+}
