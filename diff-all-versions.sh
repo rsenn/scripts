34a1a52122268ddd8353b63b376652cd2628dc3c
Roman Senn <roman.l.senn@gmail.com>
2015-09-11 00:30:27 +0200

    ...

--- diff-all-versions.sh
+++ diff-all-versions.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 
+SQ="'"
+DQ='"'
+BS="\\"
+FS="/"
+NL="
+"
+
 get_version() {
  (nover=${1%%[-_][0-9]*.*}
   echo "${1#$nover[-_]}")
@@ -9,10 +16,34 @@ get_name() {
   echo "${1%%[-_][0-9]*.*}"
 }
 
+need_quoting() {
+  case "$*" in
+    *" "* | *"\t"* | *'"'* | *"'"*) return 0 ;;
+    *) return 1 ;;
+  esac
+}
+
+make_filename() {
+ (NAME="${1}"
+  case "$NAME" in
+    */) ;;
+    *) test -d "$NAME" && NAME="$NAME/" ;;
+  esac
+  if need_quoting "$NAME"; then
+    NAME="'${NAME//"$SQ"/"$SQ$BS$SQ$SQ"}'"
+  fi
+  echo "$NAME")
+}
+
+
 main() {
+  : ${DIFF:="udiff.sh"}
+  : ${DIFFOPTS:="-ru -x{'.git*','.cvs*','.svn*','*#*','*.rej','*.orig','*~'}"}
+
   while :; do
     case "$1" in
       -p | --print*) PRINT_ONLY=true; shift ;;
+      -x | --debug*) DEBUG=true; shift ;;
       *) break ;;
     esac
   done
@@ -25,13 +56,19 @@ main() {
 		shift
 		THIS="$1"
 		NEW_VERSION=`get_version "$1"`
-		cmd="udiff.sh -ru '$PREV' '$THIS'"
+		CMD="$DIFF${DIFFOPTS:+ $DIFFOPTS} "$(make_filename "$PREV")" "$(make_filename "$THIS")
 		
-		cmd="$cmd >'$NAME-${OLD_VERSION}-to-${NEW_VERSION}.diff'"
+		CMD="$CMD >"$(make_filename "$NAME-${OLD_VERSION}-to-${NEW_VERSION}.diff")
 		
-		echo "cmd: $cmd" 1>&2
-		[ "$PRINT_ONLY" = true ] || eval "$cmd"
+		[ "$DEBUG" = true ] && echo "CMD: $CMD" 1>&2
+		if [ "$PRINT_ONLY" = true ]; then
+		  echo "$CMD"
+		else
+		  eval "$CMD"
+		fi
 		
 		OLD_VERSION=$NEW_VERSION
 	done
 }
+
+main "$@"
