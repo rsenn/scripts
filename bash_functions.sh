dff5fbcd8afca0daa163fe10a9e581e546a8b922
Roman Senn <roman.l.senn@gmail.com>
2015-09-18 23:24:05 +0200

    ...

--- bash_functions.sh
+++ bash_functions.sh
@@ -592,9 +592,22 @@ cut-lsof() {
   done)
 }
 
-cut-num()
-{
-  sed 's,^\s*[0-9]\+\s*,,' "$@"
+cut-num() {
+ (while :; do
+    case "$1" in
+			-n | --num) N="$2"; shift 2 ;;
+			-n=* | --num=*) N="${1##*=}"; shift ;;
+			-n) N="${1#-n}"; shift ;;
+			*) break ;;
+		esac
+  done
+  : ${N=1}
+  EXPR=
+  while [ $((N)) -gt 0 ]; do
+    EXPR="$EXPR[0-9]\\+\\s*"
+    : $((N--))
+  done
+  sed "s|^${EXPR:+\\s*$EXPR}||" "$@")
 }
 
 cut-pkgver()
@@ -4841,12 +4854,12 @@ uuid_hexnums()
 vs2vc() {
  (for ARG; do
    case "$ARG" in
-     8 | 8.0) echo 2005 ;;
-     9 | 9.0) echo 2008 ;;
-     10 | 10.0) echo 2010 ;;
-     11 | 11.0) echo 2012 ;;
-     12 | 12.0) echo 2013 ;;
-     14 | 14.0) echo 2015 ;;
+     8 | 8.0 | 8.00) echo 2005 ;;
+     9 | 9.0 | 9.00) echo 2008 ;;
+     10 | 10.0 | 10.00) echo 2010 ;;
+     11 | 11.0 | 11.00) echo 2012 ;;
+     12 | 12.0 | 12.00) echo 2013 ;;
+     14 | 14.0 | 14.00) echo 2015 ;;
      *) echo "No such Visual Studio version: $ARG" 1>&2; exit 1 ;;
    esac
   done)
@@ -4932,14 +4945,26 @@ fi)
 }
 
 vs2vc() {
- (for ARG; do
+ (NUL=0
+  while :; do
+    case "$1" in
+      -0 | -nul | --nul) : $((NUL++)); shift ;;
+      *) break ;;
+    esac
+  done
+  N=
+  while [ $((NUL)) -gt 0 ]; do
+    N="${N}0"
+    : $((NUL--))
+  done
+  for ARG; do
    case "$ARG" in
-     2005) echo 8.0 ;; 
-     2008) echo 9.0 ;; 
-     2010) echo 10.0 ;; 
-     2012) echo 11.0 ;; 
-     2013) echo 12.0 ;; 
-     2015) echo 14.0 ;; 
+     2005) echo 8${N:+.$N} ;; 
+     2008) echo 9${N:+.$N} ;; 
+     2010) echo 10${N:+.$N} ;; 
+     2012) echo 11${N:+.$N} ;; 
+     2013) echo 12${N:+.$N} ;; 
+     2015) echo 14${N:+.$N} ;; 
      *) echo "No such Visual Studio version: $ARG" 1>&2; exit 1 ;;
    esac
   done)
