2847d21b19b7976cfc46445f3086cfa6598432f9
Roman Senn <roman.l.senn@gmail.com>
2015-10-01 23:17:00 +0200

    ...

--- bash_functions.sh
+++ bash_functions.sh
@@ -5518,6 +5518,20 @@ submatch()
     done
 }
 
+ subst-build-cmd() {
+  : ${vs=2013} ${vc=12}
+  	
+	s() {
+	  sed 's,20[01][0-9],'$vs',g ;; s, [8-9] , '$vc' ,g  ;; s, 1[0124] , '$vc' ,g' "$@"
+	}
+	for x in ${@:-build/vs2008-*/build.cmd}; do
+		y=$(s <<<"$x")
+		mkdir -p "$(dirname "$y")"
+		s < "$x" >"$y"
+		echo "$y"
+	done
+}
+
 subst_script()
 {
     local var script value IFS="$obj_s";
