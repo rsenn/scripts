6ecc435e82b046cc0302d7e3bcd983cd292af5d9
Roman Senn <roman.l.senn@gmail.com>
2015-10-05 02:52:25 +0200

    ...

--- bash_functions.sh
+++ bash_functions.sh
@@ -2117,15 +2117,19 @@ grep-e()
 }
 
  grep-in-index() {
-  (CMD='index-dir -u $DIRS | xargs grep "[^/]\$" -H | sed "s|/files.list:|/|" -u | xargs grep -H -E "($EXPRS)" '
-   case "$PATHTOOL" in
-     cygpath*) PATHTOOL="xargs $PATHTOOL" ;;
-   esac
+  (CMD='index-dir -u $DIRS | xargs grep "[^/]\$" -H | sed "s|^$PWD/files.list:|| ; s|/files.list:|/|" -u | xargs grep $OPTS -H -E "($EXPRS)" '
+#   case "$PATHTOOL" in
+#     cygpath*) PATHTOOL="xargs $PATHTOOL" ;;
+#   esac
    while :; do
      case "$1" in
-       -w | -m) CMD="$CMD | ${PATHTOOL:-xargs cygpath} $1"; shift ;;
+#       -w | -m) CMD="$CMD | ${PATHTOOL:-xargs cygpath} $1"; shift ;;
        -A|-B|-C|-D|-E|-F|-G|-H|-I|-L|-NUM|-P|-R|-T|-U|-V|-Z|-a|-b|-c|-d|-e|-f|-h|-i|-l|-m|-n|-o|-q|-r|-s|-u|-v|-w|-x|-z|\
-       --basic-regexp|--binary|--byte-offset|--count|--dereference-recursive|--extended-regexp|--files-with-matches|--files-without-match|--fixed-strings|--help|--ignore-case|--initial-tab|--invert-match|--line-buffered|--line-number|--line-regexp|--no-filename|--no-messages|--null|--null-data|--only-matching|--perl-regexp|--quiet|--recursive|--silent|--text|--unix-byte-offsets|--version|--with-filename|--word-regexp) GREP_ARGS="${GREP_ARGS:+$GREP_ARGS }$1"; shift ;;
+       --color|--basic-regexp|--binary|--byte-offset|--count|--dereference-recursive|--extended-regexp|--files-with-matches|--files-without-match|--fixed-strings|--help|--ignore-case|--initial-tab|--invert-match|--line-buffered|--line-number|--line-regexp|--no-filename|--no-messages|--null|--null-data|--only-matching|--perl-regexp|--quiet|--recursive|--silent|--text|--unix-byte-offsets|--version|--with-filename|--word-regexp) OPTS="${OPTS:+$OPTS
+}$1"; shift ;;
+       --*=*) OPTS="${OPTS:+$OPTS
+}$1
+$2"; shift 2 ;;
        *) break ;;
      esac
     done
