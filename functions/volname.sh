8fb682324b17cef687798d0dad5f865d21f11f47
Roman Senn <roman.l.senn@gmail.com>
2015-10-15 04:55:18 +0200

    ...

--- functions/volname.sh
+++ functions/volname.sh
@@ -14,12 +14,12 @@ volname() {
     for ARG in "$@"; do
         drive="$ARG"
         case "$drive" in
-          ?) drive="$drive:/" ;;
-          ?:) drive="$drive/" ;;
+          ?) drive="$drive:" ;;
+          ?:*) drive=${drive%%:*}: ;;
           *) drive=$(${PATHTOOL:-echo} "$drive") ;;
         esac  
-        drive=$(${PATHTOOL:-echo} "$drive")
-        NAME=$(cmd /c "vol ${drive%%/*}" | sed -n '/Volume in drive/ s,.* is ,,p')
+        #drive=$(${PATHTOOL:-echo} "$drive")
+        NAME=$(cmd /c "vol ${drive%%[\\/]*}" | sed -n '/Volume in drive/ s,.* is ,,p')
         eval "$ECHO"
     done
   fi)
