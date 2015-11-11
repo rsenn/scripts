26e771919b925c61491d10a7c9144b8236b7c379
Roman Senn <roman.l.senn@gmail.com>
2015-11-11 01:26:57 +0100

    ...

--- bash_functions.sh
+++ bash_functions.sh
@@ -5384,6 +5384,24 @@ readshortcut
 '
 }
 
+sln-version()
+{ 
+    ( [ $# -gt 1 ] && O='""$ARG": $FVER" $VSVER' || O='"$FVER" $VSVER';
+    for ARG in "$@";
+    do
+        ( exec < "$ARG";
+        read -r LINE;
+        FVER=${LINE#*"Version "};
+        read -r LINE;
+        case "$LINE" in 
+            *Visual*Studio*)
+                VSVER=${LINE#*Visual*"Studio "}
+            ;;
+        esac;
+        eval "echo $O" );
+    done )
+}
+
 some()
 {
     eval "while shift
