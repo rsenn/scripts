5c4c8ea049ac4e8f0cb2bf6ea06ddeb93fea6780
Roman Senn <rls@crowdguard.org>
2015-11-23 08:49:39 +0100

    ...

--- bash_functions.sh
+++ bash_functions.sh
@@ -4796,26 +4796,8 @@ path-executables()
 
 pathmunge () { while :; do case "$1" in -s) PATHSEP="$2"; shift 2 ;; -v) PATHVAR="$2"; shift 2 ;; -e) EXPORT="export "; shift ;; -f) FORCE=true; shift ;; -a) AFTER=true; shift ;; *) break ;; esac; done; local IFS=":"; : ${OS=`uname -o | head -n1`}; case "$OS:$1" in [Mm]sys:*[:\\]*) tmp="$1"; shift; set -- `${PATHTOOL:-msyspath} "$tmp"` "$@" ;; esac; IFS=" "; if ! eval "echo \"\\\${\${PATHVAR-PATH}}\"" | ${GREP-grep} -E -q "(^|${PATHSEP-:})$1($|${PATHSEP-:})"; then if [ "$2" = after -o "$AFTER" = true ]; then CMD="${EXPORT}${PATHVAR-PATH}=\"\\\${\${PATHVAR-PATH}:+\\\$\${PATHVAR-PATH}${PATHSEP-:}}\$1\""; else CMD="${EXPORT}${PATHVAR-PATH}=\"\$1\\\${\${PATHVAR-PATH}:+${PATHSEP-:}\\\$\${PATHVAR-PATH}}\""; fi; fi; [ "$FORCE" = true ] && CMD="pathremove \"$1\"; $CMD"; [ "$DEBUG" = true ] && eval "echo \"+ $CMD\" 1>&2"; eval "$CMD"; unset PATHVAR; }
 
-pathremove() {
-  old_IFS="$IFS"
-  IFS=":"
-  RET=1
-  unset NEWPATH
-
-  for DIR in $PATH; do
-    for ARG; do
-      case "$DIR" in
-        $ARG) RET=0; continue 2 ;;
-      esac
-    done
-    NEWPATH="${NEWPATH+$NEWPATH:}$DIR"
-  done
-
-  PATH="$NEWPATH"
-  IFS="$old_IFS"
-  unset NEWPATH old_IFS
-  return $RET
-}
+pathremove() { old_IFS="$IFS"; IFS=":"; RET=1; unset NEWPATH; for DIR in $PATH; do for ARG in "$@"; do case "$DIR" in $ARG) RET=0; continue 2 ;; esac; done; NEWPATH="${NEWPATH+$NEWPATH:}$DIR"; done; PATH="$NEWPATH"; IFS="$old_IFS"; unset NEWPATH old_IFS; return $RET; }
+pathmunge () { while :; do case "$1" in -s) PATHSEP="$2"; shift 2 ;; -v) PATHVAR="$2"; shift 2 ;; -e) EXPORT="export "; shift ;; -f) FORCE=true; shift ;; -a) AFTER=true; shift ;; *) break ;; esac; done; local IFS=":"; : ${OS=`uname -o | head -n1`}; case "$OS:$1" in [Mm]sys:*[:\\]*) tmp="$1"; shift; set -- `${PATHTOOL:-msyspath} "$tmp"` "$@" ;; esac; IFS=" "; if ! eval "echo \"\\\${\${PATHVAR-PATH}}\"" | ${GREP-grep} -E -q "(^|${PATHSEP-:})$1($|${PATHSEP-:})"; then if [ "$2" = after -o "$AFTER" = true ]; then CMD="${EXPORT}${PATHVAR-PATH}=\"\\\${\${PATHVAR-PATH}:+\\\$\${PATHVAR-PATH}${PATHSEP-:}}\$1\""; else CMD="${EXPORT}${PATHVAR-PATH}=\"\$1\\\${\${PATHVAR-PATH}:+${PATHSEP-:}\\\$\${PATHVAR-PATH}}\""; fi; fi; [ "$FORCE" = true ] && CMD="pathremove \"$1\"; $CMD"; [ "$DEBUG" = true ] && eval "echo \"+ $CMD\" 1>&2"; eval "$CMD"; unset PATHVAR; }
 
 pathtool() {
  (EXPR= F=
