8d8aff7ddba664d78fbe04416145b9e410d4314e
Roman Senn <roman.l.senn@gmail.com>
2016-02-03 03:55:19 +0100

    ...

--- git-pull.sh
+++ git-pull.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 MYDIR=`dirname "$0"` 
+NL="
+"
 
 git_get_remote () 
 { 
     ( while :; do
         case "$1" in 
+
             -l | --list)
                 LIST=true;
                 shift
@@ -113,35 +116,77 @@ git_get_remote ()
 git_get_branch () 
 { 
     git branch -a | sed -n 's,^\* ,,p'
+} 
+exec_bin()
+{
+  (IFS=" $IFS"; CMD="$*"
+  exec 10>&2
+  [ "$VERBOSE" = true ] &&  echo -n "+ $CMD" 1>&10
+  TMP="msg$$.tmp"
+  trap 'rm -f "$TMP"' EXIT
+  exec >"$TMP"
+  if [ "$DEBUG" = true ]; then
+  :
+  else 
+:
+  fi
+  
+  "$@" 2>&1
+  
+  R=$?
+  O=$(<"$TMP")
+  rm -f "$TMP"; trap '' EXIT
+  if [ "$R" = 0 ] ; then
+    ERR=" OK" 
+  else
+    ERR=" (ERROR: $R)"
+  fi
+    [ "$VERBOSE" = true ] &&  echo "${ERR:+$ERR}" 1>&10
+    
+  [ "$R" != 0 ] &&    log_msg "$O")
+  
+}
+log_msg() {
+	echo "${NL}$*${NL}" 1>&2
 }
 
-if [ $# -gt 0 ]; then
-  set -- $(find "$@"  -type d -name ".git"|removesuffix /.git)
-else
-  set -- $(git_get_remote "$MYDIR"/*/|grep -E ' (/var/lib/git|github\.com/|ssh://.*crowdguard.org)'|cut -d: -f1|removesuffix / )
-fi
 
-for DIR ; do 
-
-
-  echo "
-Entering directory $DIR ...
-" 1>&2
-  
-  (
-  cd "$DIR"
-  REMOTES=$(git_get_remote .|awkp)
-    git commit -m ... -a
-  for R in $REMOTES; do
-    git pull "$R" $(git_get_branch)
-    git push "$R" $(git_get_branch)
+main() {
+  while :; do
+        case "$1" in
+          -x | --debug) DEBUG="true"; shift ;;
+          -v | --verbose) VERBOSE="true"; shift ;;
+          -p | --print) EVALCMD="echo"; shift ;;
+          *) break ;;
+        esac
   done
-  ) >/dev/null
-  echo "
-Leaving directory $DIR ...
-" 1>&2
-  echo 1>&2
+
+
+  if ! [ $# -gt 0 ]; then
+	set -- $(git_get_remote "$MYDIR"/*/|grep -E ' (/var/lib/git|github\.com/|ssh://.*crowdguard.org)'|cut -d: -f1|removesuffix / )
+#  else
+#	set -- $(find "$@"  -type d -name ".git"|removesuffix /.git)
+  fi
+  [ "$DEBUG" = true ] && echo "$# Arguments" 1>&2
+  for DIR ; do 
+
+
+	log_msg "Entering directory $DIR ..."
+	
+	(
+	cd "$DIR"
+	REMOTES=$(git_get_remote .|awkp)
+	 exec_bin git commit -m ... -a
+	for R in $REMOTES; do
+	 exec_bin git pull "$R" $(git_get_branch)
+	exec_bin  git push "$R" $(git_get_branch)
+	done
+	) >/dev/null
+	log_msg "Leaving directory $DIR ..."
 
 
 
-done
+  done
+}
+
+main "$@"
