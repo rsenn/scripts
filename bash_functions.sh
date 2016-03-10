9e0988b6ffd8b99c9e82f5c5f2fbca877dd9a4f9
Roman Senn <roman.l.senn@gmail.com>
2016-03-10 18:10:30 +0100

    ...

--- bash_functions.sh
+++ bash_functions.sh
@@ -6440,7 +6440,8 @@ yaourt-cutver() {
 }
 
 yaourt-joinlines() {
-(EXPR="\\|^[^/]\\+/[^/]\\+ .*(| { :lp; N; /\\n\\s[^\\n]*$/ { s|\\n\\s\\+| - |; b lp }; s,\\n\\s\\+, - ,g; :lp2; /\\n/ { P; D; b lp2; }; b lp }" "$@")
+(EXPR="\\|^[^/ ]\\+/[^/ ]\\+\\s| { :lp; N; /\\n\\s[^\\n]*$/ { s|\\n\\s\\+| - |; b lp }; s,\\n\\s\\+, - ,g; :lp2; /\\n/ { P; D; b lp2; }; b lp }"
+ exec sed -e "$EXPR" "$@")
 }
 
 yaourt-pkgnames() {
