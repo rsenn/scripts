a6959de928529ebe20fc9c910d980059c937b512
Roman Senn <roman.l.senn@gmail.com>
2016-03-10 18:15:44 +0100

    ...

--- bash_functions.sh
+++ bash_functions.sh
@@ -6440,9 +6440,18 @@ yaourt-cutver() {
 }
 
 yaourt-joinlines() {
-(EXPR="\\|^[^/ ]\\+/[^/ ]\\+\\s| { :lp; N; /\\n\\s[^\\n]*$/ { s|\\n\\s\\+| - |; b lp }; s,\\n\\s\\+, - ,g; :lp2; /\\n/ { P; D; b lp2; }; b lp }"
- exec sed -e "$EXPR" "$@")
-}
+(while :; do
+   case "$1" in 
+		 -R | --remove-repo*) REMOVE_REPO="s|^[^/ ]\\+/||"; shift ;;
+		 -V | --remove-ver*) REMOVE_VER="s|^\([^/ ]\\+\)/\([^/ ]\\+\) \([^ ]\) |\1 |"; shift ;;
+		 -r | --remove-rat*) REMOVE_RATING="s|)\s\+\(([^)]\+)\)|)|"; shift ;;
+		 *) break ;;
+		esac
+	done
+
+  EXPR="\\|^[^/ ]\\+/[^/ ]\\+\\s| { :lp; N; /\\n\\s[^\\n]*$/ { s|\\n\\s\\+| - |; b lp }; s,\\n\\s\\+, - ,g; ${REMOVE_REPO}; ${REMOVE_VER}; ${REMOVE_RAT}; :lp2; /\\n/ { P; D; b lp2; }; b lp }"
+  exec sed -e "$EXPR" "$@")
+} 
 
 yaourt-pkgnames() {
  (NAME='\([^ \t/]\+\)'
