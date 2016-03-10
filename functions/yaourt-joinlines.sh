f4f67346957b593c42276eaf0dc82d434a035126
Roman Senn <roman.l.senn@gmail.com>
2016-03-10 18:20:24 +0100

    ...

--- functions/yaourt-joinlines.sh
+++ functions/yaourt-joinlines.sh
@@ -2,12 +2,13 @@ yaourt-joinlines() {
 (while :; do
    case "$1" in 
 		 -R | --remove-repo*) REMOVE_REPO="s|^[^/ ]\\+/||"; shift ;;
-		 -V | --remove-ver*) REMOVE_VER="s|^\([^/ ]\\+\)/\([^/ ]\\+\) \([^ ]\\+\) |\1 |"; shift ;;
+		 -V | --remove-ver*) REMOVE_VER="s|^\([^/ ]\\+\)/\([^/ ]\\+\) \([^ ]\\+\) |\\1/\\2 |"; shift ;;
 		 -r | --remove-rat*) REMOVE_RATING="s|)\s\+\(([^)]\+)\)|)|"; shift ;;
+		 -n | --remove-num*) REMOVE_NUM="s|^\([^/ ]\\+\)/\([^/ ]\\+\) \([^ ]\\+\) \(([^)]\+)|\\1/\\2 \\3|"; shift ;;
 		 *) break ;;
 		esac
 	done
 
-  EXPR="\\|^[^/ ]\\+/[^/ ]\\+\\s| { :lp; N; /\\n\\s[^\\n]*$/ { s|\\n\\s\\+| - |; b lp }; s,\\n\\s\\+, - ,g; ${REMOVE_REPO}; ${REMOVE_VER}; ${REMOVE_RAT}; :lp2; /\\n/ { P; D; b lp2; }; b lp }"
+  EXPR="\\|^[^/ ]\\+/[^/ ]\\+\\s| { :lp; N; /\\n\\s[^\\n]*$/ { s|\\n\\s\\+| - |; b lp }; s,\\n\\s\\+, - ,g; ${REMOVE_REPO}; ${REMOVE_VER}; ${REMOVE_RATING}; ${REMOVE_NUM}; :lp2; /\\n/ { P; D; b lp2; }; b lp }"
   exec sed -e "$EXPR" "$@")
 } 
