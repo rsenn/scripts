f2f6980daecc7bd03730d4b51d448cf3c71d2e41
Roman Senn <roman.l.senn@gmail.com>
2016-03-24 21:47:43 +0100

    ...

--- clone-rsenn-git-repos.sh
+++ clone-rsenn-git-repos.sh
@@ -1,21 +1,21 @@
 #!/bin/sh
 
-lf="
+IFS="
   "
 
 for REPO in ${@:-Scripts sw-utils dirlist bsed}
 do
 (BRANCH=
 	case "$REPO" in
-		sw-utils) CMD="cd libswsh${lf}bash setup.sh build${lf}sudo bash setup.sh install --prefix=/usr" ;;
-		Scripts) CMD="sudo make install${lf}bash cp-bash-scripts.bash${lf}sudo bash cp-bash-scripts.bash" ;;
-		dirlist) CMD="make DEBUG=0${lf}sudo make DEBUG=0 install" ;;
-		bsed) BRANCH="inplace"; CMD=" make${lf}sudo make install prefix=/usr" ;;
-		shish) CMD="./autogen.sh${lf}./configure --prefix=/usr${lf}make${lf}sudo make install" ;;
+		sw-utils) CMD="cd libswsh${IFS}bash setup.sh build${IFS}sudo bash setup.sh install --prefix=/usr" ;;
+		Scripts) CMD="sudo make install${IFS}bash cp-bash-scripts.bash${IFS}sudo bash cp-bash-scripts.bash" ;;
+		dirlist) CMD="make DEBUG=0${IFS}sudo make DEBUG=0 install" ;;
+		bsed) BRANCH="inplace"; CMD=" make${IFS}sudo make install prefix=/usr" ;;
+		shish) CMD="./autogen.sh${IFS}./configure --prefix=/usr${IFS}make${IFS}sudo make install" ;;
 	esac
-		CMD="git fetch --all${lf}$CMD"
-		CMD="cd $REPO${lf}$CMD"
-		CMD="git clone ${BRANCH:+-b $BRANCH} --depth=1 https://github.com/rsenn/${REPO}.git${lf}$CMD"
+		CMD="git fetch --all${IFS}$CMD"
+		CMD="cd $REPO${IFS}$CMD"
+		CMD="git clone ${BRANCH:+-b $BRANCH} --depth=1 https://github.com/rsenn/${REPO}.git${IFS}$CMD"
 
 		trap 'echo " (Result: $?)" 1>&2' EXIT
 		echo -n "Executing: $CMD " 1>&2
