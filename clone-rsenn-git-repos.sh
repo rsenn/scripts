464a71898e4dc38ce5aab673ab6b8fd4aec728b2
Roman Senn <roman.l.senn@gmail.com>
2016-03-24 21:44:11 +0100

    ...

--- clone-rsenn-git-repos.sh
+++ clone-rsenn-git-repos.sh
@@ -10,12 +10,12 @@ do
 		sw-utils) CMD="cd libswsh${NL}bash setup.sh build${NL}sudo bash setup.sh install --prefix=/usr" ;;
 		Scripts) CMD="sudo make install${NL}bash cp-bash-scripts.bash${NL}sudo bash cp-bash-scripts.bash" ;;
 		dirlist) CMD="make DEBUG=0${NL}sudo make DEBUG=0 install" ;;
-		bsed) BRANCH="mod-gnustyle"${NL}CMD=" make${NL}sudo make install prefix=/usr" ;;
+		bsed) BRANCH="inplace"; CMD=" make${NL}sudo make install prefix=/usr" ;;
 		shish) CMD="./autogen.sh${NL}./configure --prefix=/usr${NL}make${NL}sudo make install" ;;
 	esac
 		CMD="git fetch --all${NL}$CMD"
-		CMD="cd \"$REPO\"${NL}$CMD"
-		CMD="git clone ${BRANCH:+-b \"$BRANCH\"} --depth=1 https://github.com/rsenn/${REPO}.git${NL}$CMD"
+		CMD="cd $REPO${NL}$CMD"
+		CMD="git clone ${BRANCH:+-b $BRANCH} --depth=1 https://github.com/rsenn/${REPO}.git${NL}$CMD"
 
 		trap 'echo " (Result: $?)" 1>&2' EXIT
 		echo -n "Executing: $CMD " 1>&2
