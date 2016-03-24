2d13d9772bcc7f6832740756d786d3f390b04023
Roman Senn <roman.l.senn@gmail.com>
2016-03-24 23:11:10 +0100

    ...

--- cp-bash-scripts.sh
+++ cp-bash-scripts.sh
@@ -1,18 +1,32 @@
 #!/bin/sh
 
-MYDIR=` dirname "$0" ` 
+MYDIR=`dirname "$0" ` 
+SQ="'"
+DQ='"'
+
+cd "$MYDIR"
 
 [ $# -ge 1 ] ||set --  "$HOME"
 
-for x in "$MYDIR"/bash/bash_*.bash; do
-  b=`basename "$x" .bash` 
- for y; do 
-   case "$b" in
-     *_profile) CMD="grep -vE \"(^\\s*#[^ !]|^\\s*#.*[\\\"'(){}]|^\\s*#.*esac|^\\s*#.*done|^\\s*#.*unset|^\\s*#\s*for\s|^\\s*#.*;;)\" <\"$x\" >\"$y/.${b%.bash}\"" ;;
-     *) CMD="cp -f \"$x\" \"$y/.${b%.bash}\"" ;;
+type make 2>/dev/null >/dev/null && MAKE="make" || MAKE=":"
+
+for DIR; do 
+ (CMD=
+	for FILE in bash/bash_*.*sh; do
+  BASE=`basename "$FILE"` 
+	BASE=${BASE%.*}
+   case "$BASE" in
+     *_profile) CMD="grep -vE ${DQ}(^\\s*#[^ !]|^\\s*#.*[\\${DQ}'(){}]|^\\s*#.*esac|^\\s*#.*done|^\\s*#.*unset|^\\s*#\s*for\s|^\\s*#.*;;)$DQ <$DQ$FILE$DQ >$DQ$DIR/.${BASE}$DQ${CMD:+; $CMD}" ;;
+     *) CMD="cp -f $DQ$FILE$DQ ${DQ}\$DIR/.${BASE}$DQ${CMD:+; $CMD}" ;;
    esac
    CMD="$CMD || exit \$?"
-   
-   eval "$CMD" && echo "Wrote '$y/.${b%.bash}'." #1>&2
+
+	 if [ "$MAKE" != : ]; then
+		 CMD="make $FILE${CMD:+; $CMD}"
+	 fi
+	 CMD="$CMD && echo ${DQ}Wrote \$SQ\$DIR/.${BASE}\$SQ$DQ 1>&2"
   done
+	[ -n "$DEBUG"  ] && echo "+ $CMD" 1>&2
+   eval "$CMD" 
+	 )
 done
