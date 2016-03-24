f6f4e25d561e0cb5b7b8ded93fce5661c40c8808
Roman Senn <roman.l.senn@gmail.com>
2016-03-24 21:32:18 +0100

    ...

--- bash_functions.sh
+++ bash_functions.sh
@@ -6473,14 +6473,14 @@ yaourt-joinlines() {
 		 -R | --remove-repo*) REMOVE_REPO="s|^[^/ ]\\+/||"; shift ;;
 		 -V | --remove-ver*) REMOVE_VER="s|^\([^/ ]\\+\)/\([^/ ]\\+\) \([^ ]\\+\) |\\1/\\2 |"; shift ;;
 		 -I | --no*inst*) NO_INSTALLED="/\\[installed/!"; shift ;;
-		 -r | --remove-rat*) REMOVE_RATING="s|)\s\+\(([^)]\+)\)|)|"; shift ;;
+		 -r | --remove-rat*) REMOVE_RATING="s|)\s\+\(([^)]\+)\)|) |"; shift ;;
 		 -n | --remove-num*) REMOVE_NUM="s|^\([^/ ]\\+\)/\([^/ ]\\+\) \([^ ]\\+\) \(([^)]\+)\)|\\1/\\2 \\3|"; shift ;;
 		 -s | --*sep*) COLSEP="$2";  shift 2 ;;
 		 *) break ;;
 		esac
 	done
 
-  EXPR="\\|^[^/ ]\\+/[^/ ]\\+\\s| { :lp; ${REMOVE_RATING}; ${REMOVE_NUM}; ${REMOVE_VER}; ${REMOVE_REPO}; N; /\\n\\s[^\\n]*$/ { s,\\n\\s\\+,${COLSEP- - },; b lp }; s,\\n\\s\\+, - ,g; :lp2; /\\n/ { ${NO_INSTALLED} P; D; b lp2; }; b lp }"
+  EXPR="\\|^[^/ ]\\+/[^/ ]\\+\\s| { :lp; ${REMOVE_RATING}; ${REMOVE_NUM}; ${REMOVE_VER}; ${REMOVE_REPO}; N; /\\n\\s[^\\n]*$/ { s,\\n\\s\\+,${COLSEP- - },; b lp }; s,\\n\\s\\+, - ,g; :lp2; /\\n/ { s,[^[:print:]\\n],,g; ${NO_INSTALLED} P; D; b  lp2; }; b lp }"
   exec sed -e "$EXPR" "$@")
 } 
 
---
+++ clone-rsenn-git-repos.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+NL="
+  "
+
+for REPO in ${@:-Scripts sw-utils dirlist bsed}
+do
+(BRANCH=
+	case "$REPO" in
+		sw-utils) CMD="cd libswsh${NL}bash setup.sh build${NL}sudo bash setup.sh install --prefix=/usr" ;;
+		Scripts) CMD="sudo make install${NL}bash cp-bash-scripts.bash${NL}sudo bash cp-bash-scripts.bash" ;;
+		dirlist) CMD="make DEBUG=0${NL}sudo make DEBUG=0 install" ;;
+		bsed) BRANCH="mod-gnustyle"${NL}CMD=" make${NL}sudo make install prefix=/usr" ;;
+		shish) CMD="./autogen.sh${NL}./configure --prefix=/usr${NL}make${NL}sudo make install" ;;
+	esac
+		CMD="git fetch --all${NL}$CMD"
+		CMD="cd \"$REPO\"${NL}$CMD"
+		CMD="git clone ${BRANCH:+-b \"$BRANCH\"} --depth=1 https://github.com/rsenn/${REPO}.git${NL}$CMD"
+
+		trap 'echo " (Result: $?)" 1>&2' EXIT
+		echo -n "Executing: $CMD " 1>&2
+		eval "(set -e; $CMD)" >"$REPO.log" 2>&1 
+		)
+done
