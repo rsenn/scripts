97daf8e7a3f86361fc46109504df786ee2a14aad
Roman Senn <roman.l.senn@gmail.com>
2016-03-24 22:51:41 +0100

    ...

--- sh/functions/yaourt-search.sh
+++ sh/functions/yaourt-search.sh
@@ -1,14 +1,20 @@
 yaourt-search() { 
 (NPAD=32; VPAD=24; : ${COLS=$(tput cols)}; while :; do
    case "$1" in
+		 -D | --no*desc*) EVARS='$N $V'; shift ;;
+		 -N | --name-only) EVARS='$N'; shift ;;
 		-*) pushv OPTS "$1"; shift ;;
 		*) break ;;
 		esac
 	done
  
- CMD="yaourt-search-cmd | yaourt-search-output"
- eval "$CMD"
- )
+ CMD="yaourt-search-cmd \"\$@\" | yaourt-search-output"
+ if is-a-tty ; then
+	 CMD="$CMD | grep -E --color=yes \"$(grep-e-expr "$@")\""
+	else
+		 NPAD= VPAD=
+ fi
+ eval "$CMD")
 }
 	yaourt-search-cmd() {
   for Q in "$@"; do
@@ -17,23 +23,24 @@ yaourt-search() {
    command grep -a --line-buffered --colour=auto -i -E "($*)")
  done; }
  yaourt-search-output() {
-	 IFS=" "; while read -r NAME VERSION_DESC; do
+	: ${EVARS='$N $V $DESC'}
+	IFS=" "; while read -r NAME VERSION_DESC; do
 	 DESC=${VERSION_DESC##*"|"}
 	 VERSION=${VERSION_DESC%%"|"*}
 	 NUM="(${VERSION#*"("}"
 	 VERSION=${VERSION%"$NUM"}
 	 VERSION=${VERSION%" "}
 (
-	N=$(printf "%-${NPAD}s" "${NAME}"  )
-	[ ${#N} -gt $((NPAD)) ] && VPAD=$((VPAD - (  ${#N}  -    $((NPAD)) ) ))
+	N=$(printf "%${NPAD+-$NPAD}s" "${NAME}"  )
+	[ $((NPAD)) -gt 0 -a ${#N} -gt $((NPAD)) ] && VPAD=$((VPAD - (  ${#N}  -    $((NPAD)) ) ))
 
-	V=$(printf "%-${VPAD}s"  "${VERSION}" )
+	V=$(printf "%${VPAD+-$VPAD}s"  "${VERSION}" )
  #MAXDESC=$(( COLS - (NPAD + 1 + VPAD + 1) )) 
  MAXDESC=$(( COLS - ${#N} - 1 - ${#V} - 1 ))
 	 if [ ${#DESC} -gt $(( COLS - ${#N} - 1 - ${#V} - 1))  ]; then
 		 DESC=${DESC:1:$((MAXDESC-3))}...
 	fi
-	echo "$N $V $DESC"
+	eval "echo \"$EVARS\""
 	)
  done
 }
