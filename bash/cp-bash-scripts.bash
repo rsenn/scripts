3e3b4aa5210f50a800172e5c766791d794daf143
Roman Senn <roman.l.senn@gmail.com>
2016-03-24 22:14:24 +0100

    ...

--- bash/cp-bash-scripts.bash
+++ bash/cp-bash-scripts.bash
@@ -2,15 +2,15 @@ MYDIR=` dirname "$0" `
 
 [ $# -ge 1 ] ||set --  "$HOME"
 
-for x in "$MYDIR"/bash_*.sh; do
-  b=`basename "$x" .sh` 
+for x in "$MYDIR"/bash_*.bash; do
+  b=`basename "$x" .bash` 
  for y; do 
    case "$b" in
-     *_profile) CMD="grep -vE \"(^\\s*#[^ !]|^\\s*#.*[\\\"'(){}]|^\\s*#.*esac|^\\s*#.*done|^\\s*#.*unset|^\\s*#\s*for\s|^\\s*#.*;;)\" <\"$x\" >\"$y/.${b%.sh}\"" ;;
-     *) CMD="cp -f \"$x\" \"$y/.${b%.sh}\"" ;;
+     *_profile) CMD="grep -vE \"(^\\s*#[^ !]|^\\s*#.*[\\\"'(){}]|^\\s*#.*esac|^\\s*#.*done|^\\s*#.*unset|^\\s*#\s*for\s|^\\s*#.*;;)\" <\"$x\" >\"$y/.${b%.bash}\"" ;;
+     *) CMD="cp -f \"$x\" \"$y/.${b%.bash}\"" ;;
    esac
    CMD="$CMD || exit \$?"
    
-   eval "$CMD" && echo "$y/.${b%.sh}" #1>&2
+   eval "$CMD" && echo "Wrote '$y/.${b%.bash}'." #1>&2
   done
 done
