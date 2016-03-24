448944768367d935d08e3c0ef784999cc364f410
Roman Senn <roman.l.senn@gmail.com>
2016-03-24 22:43:36 +0100

    ...

--- sh/functions/yaourt-search.sh
+++ sh/functions/yaourt-search.sh
@@ -1,5 +1,5 @@
 yaourt-search() { 
-(while :; do
+(NPAD=32; VPAD=24; : ${COLS=$(tput cols)}; while :; do
    case "$1" in
 		-*) pushv OPTS "$1"; shift ;;
 		*) break ;;
@@ -9,5 +9,24 @@ yaourt-search() {
 	 (IFS="| $IFS"; set -- $Q
    command yaourt -Ss $@ | yaourt-joinlines -s "|" $OPTS | 
    command grep -a --line-buffered --colour=auto -i -E "($*)")
-  done)
+ done | { IFS=" "; while read -r NAME VERSION_DESC; do
+   DESC=${VERSION_DESC##*"|"}
+	 VERSION=${VERSION_DESC%%"|"*}
+	 NUM="(${VERSION#*"("}"
+	 VERSION=${VERSION%"$NUM"}
+	 VERSION=${VERSION%" "}
+(
+	N=$(printf "%-${NPAD}s" "${NAME}"  )
+	[ ${#N} -gt $((NPAD)) ] && VPAD=$((VPAD - (  ${#N}  -    $((NPAD)) ) ))
+
+	V=$(printf "%-${VPAD}s"  "${VERSION}" )
+ #MAXDESC=$(( COLS - (NPAD + 1 + VPAD + 1) )) 
+ MAXDESC=$(( COLS - ${#N} - 1 - ${#V} - 1 ))
+	 if [ ${#DESC} -gt $(( COLS - ${#N} - 1 - ${#V} - 1))  ]; then
+		 DESC=${DESC:1:$((MAXDESC-3))}...
+	fi
+	echo "$N $V $DESC"
+	)
+ done
+ })
 }
