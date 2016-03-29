ff70d576e0cf29751d911863e0e89caba030b855
Roman Senn <roman.l.senn@gmail.com>
2016-03-29 22:49:31 +0200

    ...

--- sh/isodate.sh
+++ sh/isodate.sh
@@ -4,6 +4,7 @@ FMT="%Y%m%d"
 
 while :; do
   case "$1" in
+    --debug | -x) DEBUG="true"; shift ;;
     -t | --time) FMT="$FMT %H:%M"; shift ;;
     --ref*=*) DFILE="${1#*=}"; shift ;;
     -r | --ref*) DFILE="$2"; shift 2 ;;
@@ -14,4 +15,5 @@ done
 if [ -e "$DFILE" ]; then
   UT=$(ls -n -l -d --time-style="+%s" "$DFILE" | awk '{ print $6 }')
 fi
+[ "$DEBUG" = true ] && set -x
 exec date ${UT:+--date="@${UT}"} +"$FMT" 
--- sh/isotime.sh
+++ sh/isotime.sh
@@ -4,6 +4,7 @@ FMT="%Y%m%d-%H%M"
 
 while :; do
   case "$1" in
+    --debug | -x) DEBUG="true"; shift ;;
     --ref*=*) DFILE="${1#*=}"; shift ;;
     -r | --ref*) DFILE="$2"; shift 2 ;;
     *) break ;;
@@ -13,4 +14,5 @@ done
 if [ -e "$DFILE" ]; then
   UT=$(ls -n -l -d --time-style="+%s" "$DFILE" | awk '{ print $6 }')
 fi
+[ "$DEBUG" = true ] && set -x
 exec date ${UT:+--date="@${UT}"} +"$FMT" 
