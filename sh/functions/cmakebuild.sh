731f7bb3550e74b2717f50a4c876cc7462d84caf
Roman Senn <roman.l.senn@gmail.com>
2016-04-07 00:53:03 +0200

    ...

---
+++ sh/functions/cmakebuild.sh
@@ -0,0 +1,43 @@
+cmakebuild() 
+{ 
+is_interactive || set -e
+    builddir=build/cmake
+    destdir=${PWD}-linux
+    : ${pkgdir=~/Downloads}
+    : ${python_config=/usr/bin/python2.7-config}
+    (set -x
+     rm -rf $builddir/ $destdir/
+     mkdir -p $builddir/)
+
+    (set -x 
+     pushd $builddir 
+     find_libpython() {
+         ( set -- $(echo $($python_config --ldflags --libs )  |sed -n 's,.*-L\([^ ]*\) .*-lpython\([^ ]*\) .*,\1/libpython\2.a,p'; for ext in 'a' 'so.*' ; do (set -x; find $($python_config --exec-prefix)/lib*/ -maxdepth 3 -and -not -type d -and -name "libpython*$ext"); done); test -n "$1" -a -f "$1" && echo "$1")
+     }
+    cmake \
+        -DCMAKE_VERBOSE_MAKEFILE=TRUE \
+        -DCONFIG=Release \
+        -DBUILD_SHARED_LIBS=ON \
+        -DCMAKE_{C,CXX}_FLAGS="-fPIC" \
+        -DCMAKE_CXX_COMPILER="${CXX:-`which g++`}" \
+        -DCMAKE_C_COMPILER="${CC:-`which gcc`}" \
+        -DCMAKE_INSTALL_PREFIX="$($python_config --exec-prefix)" \
+        -DPYTHON_EXECUTABLE="$($python_config --exec-prefix)/bin/python2" \
+        -DPYTHON_INCLUDE_DIR="$($python_config  --includes|sed 's,^-I\([^ ]*\) .*,\1,p' -n)" \
+        -DPYTHON_LIBRARY="${PYTHON_LIBRARY:-`find_libpython`}" \
+    "$@" \
+    ../.. 2>&1) >cmake.log
+
+
+    (set -x
+    make -C $builddir/ 2>&1 ) >make.log || { ERR=$?; grep '(Stop|failed|error:)' -E  -C3 make.log; exit $?; }
+    (set -x
+    trap '${SUEXEC:-command} rm -rf "$destdir"' EXIT
+    (${SUEXEC:-command} make DESTDIR="$destdir" -C $builddir/ install -i 2>&1 ) >install.log
+    mkdir -p $pkgdir
+    pushd "$destdir"
+    make-archive.sh -d $pkgdir -t txz -9 -r -D)
+    set +e
+
+
+}
