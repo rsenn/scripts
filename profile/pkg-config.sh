971e732c52ad039040fccc3b172179f616bc69fa
Roman Senn <roman.l.senn@gmail.com>
2016-04-07 21:28:16 +0200

    ...

--- profile/pkg-config.sh
+++ profile/pkg-config.sh
@@ -1,9 +1,11 @@
-PKG_CONFIG_PATH=$(
-  old_IFS="$IFS"; IFS=":"; set -- $PATH; IFS="
-";  isin() {  (N="$1"; while [ "$#" -gt 1 ]; do shift; test "$N" = "$1" && exit 0; done; exit 1); }
-  P="${*%%/bin*}"; set --; for D in $P; do
-    DIR="$D/lib/pkgconfig"; if [ -d "$DIR" ] && ! isin "$DIR" "$@"; then set -- "$@" "$DIR"; fi
-  done; IFS=";"; echo "$*"
-)
+if [ "${PKG_CONFIG_PATH+set}" != set ]; then
+  old_IFS=$IFS IFS=:; set -- $PATH; IFS=";
+";isin() { N=$1; while [ "$#" -gt 1 ]; do shift; [ "$N" = "$1" ] && return 0; done; return 1
+  }; P="${*%%/bin*}"; set --; for D in $P; do D=$D/lib/pkgconfig; if [ -d "$D" ]; then
+	 type cygpath 2>/dev/null >/dev/null && IFS=: && D=`cygpath -a "$D"`
+	 ! isin "$D" "$@" && set -- "$@" "$D"
+  fi; done; PKG_CONFIG_PATH=$*; IFS=$old_IFS
+fi
+
 export PKG_CONFIG_PATH
 
