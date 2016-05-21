4846c89ae53c1d15db4ca7315dbbdcd746d17ab8
Roman Senn <roman.l.senn@gmail.com>
2016-05-21 08:25:49 +0200

    ...

--- bash/bash_functions.bash
+++ bash/bash_functions.bash
@@ -422,6 +422,18 @@ choco-joinlines() {
   done)
 }
 
+choco-search()
+{ 
+    ( IFS=" $IFS${nl}";
+    while [ $# -gt 0 ]; do
+        ( set -x;
+        choco search -f -v $1 ) | choco-joinlines | ( set -- ${GREP:-grep
+--color=yes} -i $1 --;
+        eval "$*" );
+        shift;
+    done )
+}
+
 choices-list()
 {
     local n=$1 count=0 choices='';
