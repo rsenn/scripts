c0a3f09296ed924e2dc765cb02e6df8151b4b210
Roman Senn <roman.l.senn@gmail.com>
2016-05-28 23:59:19 +0200

    ...

--- sh/functions/for_each.sh
+++ sh/functions/for_each.sh
@@ -1,14 +1,14 @@
 for_each() {
-  ABORT_COND=' || return $?'
+  ABORT_COND=' return $?'
   while :; do 
     case "$1" in
       -c | --cd | --ch*dir*) CHANGE_DIR=true; shift ;;
-      -f | --force) ABORT_COND=; shift ;;
+      -f | --force) ABORT_COND=' :'; shift ;;
       -x | --debug) DEBUG=true; shift ;;
       *) break ;;
     esac
   done
-  ABORT_COND='{ unset CMD CHANGE_DIR ABORT_COND DEBUG;  [ "$PD" != "$PWD" ] && cd "$PD" >/dev/null; }; '$ABORT_COND
+  ABORT_COND=' { unset CMD CHANGE_DIR ABORT_COND DEBUG;  [ "$PD" != "$PWD" ] && cd "$PD" >/dev/null; '$ABORT_COND'; }'
   PD=$PWD
   CMD=$1
   if [ "$(type -t "$CMD")" = function ]; then
@@ -18,9 +18,9 @@ for_each() {
   [ "$CHANGE_DIR" = true ] &&  CMD='D=$1; cd "$D" >/dev/null;'$CMD';cd - >/dev/null'  || CMD='D=;'$CMD
   	
   if [ $# -gt 1 ]; then
-    CMD='while shift; [ "$#" -gt 0 ]; do { '$CMD'; }'$ABORT_COND'; done'
+    CMD='while shift; [ "$#" -gt 0 ]; do { '$CMD'; } ||'$ABORT_COND'; done'
   else
-    CMD='while read -r LINE; do set -- $LINE; { '$CMD'; }'$ABORT_COND'; done'
+    CMD='while read -r LINE; do set -- $LINE; { '$CMD'; } ||'$ABORT_COND'; done'
   fi
 #	[ "$DEBUG" = true ] && echo "+ $CMD" 1>&2
   eval "$CMD; $ABORT_COND"
