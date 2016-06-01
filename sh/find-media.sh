1d72bc66b9b05fc690a8ca7e192f3c8fcca5ac7b
Roman Senn <roman.l.senn@gmail.com>
2016-06-02 00:27:03 +0200

    ...

--- sh/find-media.sh
+++ sh/find-media.sh
@@ -263,10 +263,7 @@ esac
 #*Linux*|*linux*) : ${MEDIAPATH="/m*/*/"} ;;
 #esac
 #
-#case "$(command ${GREP-grep
--a
---line-buffered
---color=auto} --help 2>&1)" in
+#case "$(command ${GREP-grep -a --line-buffered --color=auto} --help 2>&1)" in
 #  *--color*) GREP_ARGS="$GREP_ARGS --color=auto" ;;
 #esac
 #
