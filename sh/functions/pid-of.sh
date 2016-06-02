2f40ff43e808563603fabc3f1b41096903108ba4
Roman Senn <roman.l.senn@gmail.com>
2016-06-02 02:47:13 +0200

    ...

--- sh/functions/pid-of.sh
+++ sh/functions/pid-of.sh
@@ -1,15 +1,15 @@
 pid-of() {
-   (if ps --help 2>&1 |${GREP-grep
+ (if ps --help 2>&1 | ${GREP-grep
 -a
 --line-buffered
 --color=auto} -q '\-W'; then
-       PGREP_CMD='ps -aW |${GREP-grep -a --line-buffered --color=auto} -i "$ARG" | awkp'
-    elif type pgrep 2>/dev/null >/dev/null; then
-       PGREP_CMD='pgrep -f "$ARG"'
-    else
-       PGREP_CMD='ps -ax | ${GREP-grep -a --line-buffered --color=auto} -i "$ARG" | awkp'
-    fi
-    for ARG in "$@"; do
-      eval "$PGREP_CMD"
-    done | ${SED-sed} -n 's/^\([0-9]\+\)\r\?$/\1/p')
+     PGREP_CMD='ps -aW |${GREP-grep -a --line-buffered --color=auto} -i "$ARG" | awkp'
+  elif type pgrep 2>/dev/null >/dev/null; then
+     PGREP_CMD='pgrep -f "$ARG"'
+  else
+     PGREP_CMD='ps -ax | ${GREP-grep -a --line-buffered --color=auto} -i "$ARG" | awkp'
+  fi
+  for ARG in "$@"; do
+    eval "$PGREP_CMD"
+  done | ${SED-sed} -n 's/^\([0-9]\+\)\r\?$/\1/p')
 }
