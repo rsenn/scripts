054faefaeaf481d2f187aa804fc9c3153099c4ec
Roman Senn <roman.l.senn@gmail.com>
2016-06-09 23:08:53 +0200

    ...

--- profile/android-ndk.sh
+++ profile/android-ndk.sh
@@ -1,10 +1,10 @@
-ANDROID_NDK_PATH=/opt/android-ndk-r10b
+ANDROID_NDK_PATH=/opt/android-ndk-r11c
 ANDROID_NDK_ROOT=$ANDROID_NDK_PATH
 #PATH="$PATH:$ANDROID_NDK_ROOT"
 ANDROID_SYSROOT=$ANDROID_NDK_ROOT/platforms/android-17/arch-arm/
 JAVA_HOME=/usr/lib/jvm/java-openjdk
 
-for DIR in $ANDROID_NDK_ROOT/toolchains/{arm,x86,mipsel}*-4.8/prebuilt/linux-`uname -i`/bin; do
+for DIR in $ANDROID_NDK_ROOT/toolchains/{arm,x86,mipsel}*-4.9/prebuilt/linux-`uname -i`/bin; do
   test -d "$DIR" || continue
 	if ! echo ${PATH} | grep -q "$DIR"; then
 		 PATH="$PATH:$DIR"
