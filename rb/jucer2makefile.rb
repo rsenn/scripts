dda8cdcd589e494530fb280fa9cd6ea6c4353d0b
Roman Senn <roman.l.senn@gmail.com>
2016-06-11 07:14:42 +0200

    ...

--- rb/jucer2makefile.rb
+++ rb/jucer2makefile.rb
@@ -11,6 +11,7 @@ require 'pp'
 require_relative 'lib/hash_array.rb'
 require_relative 'lib/enum.rb'
 require_relative 'lib/makefile.rb'
+require_relative 'lib/jucerfile.rb'
 
 class Array
   def push_unique(*args)
@@ -32,215 +33,54 @@ class String
 end
 
 
-""" JucerFile -------------------------------------------------------------- """
-""" ------------------------------------------------------------------------ """
-class JucerFile < BuildFile
-  attr_accessor :file
-
-  """ initialize(filename) """
-#  def initialize
-#    super.initialize
-#    if filename != nil then
-#      read filename
-#    end
-#  end
-
-  def read(filename)
-    @file = REXML::Document.new File.new(filename)
+def main
+  settings = CLI.new do
+      switch :debug, :short  => :d, :description => 'debug messages'
+      argument :input_file,   :short => :i, :description => 'input file' #, :default => "/mnt/tmpdata/Sources/ctrlr/Builds/VST/Ctrlr_Plugin_VST.jucer"
+      argument :output_file, :short => :o, :required => false, :description => 'output file'
+  end.parse! do |settings|
+    fail "No such file '#{settings.input_file}'" unless File.exist? String(settings.input_file)
   end
 
-  def write(o=$stdout)
-    @file.write(o)
-    $stdout << "\n"
+
+  dir = File.dirname(settings.input_file)
+  name = File.basename(settings.input_file, ".jucer")
+  infile = File.basename(settings.input_file.to_s)
+
+  if settings.output_file then
+    outfile = settings.output_file.gsub(/#name/, name)
+  else
+    outfile = "Makefile.#{name}"
   end
 
-  def targets
-    @file.elements.to_a("/JUCERPROJECT").map { |e| e.attribute("name").to_s }
+  $stderr.puts "Entering directory #{dir} ..."
+
+  Dir.chdir dir
+
+  $stderr.puts "Reading '#{infile}' ..."
+
+  myfile = JucerFile.new
+  myfile.read infile 
+
+  $stderr.puts "Writing '#{outfile}' ..."
+
+  of = MakeFile.clone myfile
+  of.write(File.new(dir+"/"+outfile, "r+", 0644))
+
+  $stderr.puts "To run: make -C #{dir} -f #{outfile}"
+
+
+  if settings.debug then
+    $stdout.puts "sources: "+ myfile.sources.flatten.join(" ")
+    $stdout.puts "targets: "+ myfile.targets.join(" ")
+    $stdout.puts "project type: " +myfile.project_type.to_s
+    $stdout.puts "defines: "+ myfile.defines("Release", "VS*")
+    $stdout.puts "compile flags: "+myfile.compile_flags("*LIN*")
+    $stdout.puts "link flags: "+myfile.link_flags("*LIN*")
+    $stdout.puts "configurations: "+myfile.configurations.join(", ")
+
+    #myfile.write($stdout)
   end
-
-  def project_type
-    ProjectType.new @file.elements.to_a("/JUCERPROJECT")[0].attributes["projectType"]
-  end
-
-    """ Returns a list of source files """
-  def sources
-    h = Hash.new
-    h[targets[0]] = files "@compile=1" # and @resource=0"
-    return h
-  end
-
-    """ Returns a list of ressource files """
-  def resources
-    files "@resource=0"
-  end
-
-    """ Returns compile flags for all the exporters which match the given expression """
-  def compile_flags(exporter = "*", sep = " ")
-    split_and_concat_uniq attribute("extraCompilerFlags", exporter).values, sep
-  end
-
-    """ Returns link flags for all the exporters which match the given expression """
-  def link_flags(exporter = "*", sep = " ")
-    f = linker(exporter, sep).map do |arg|
-      if not arg.match(/^[-\/][Ll]/) then
-        arg
-      end
-    end
-    f.join(" ")
-  end
-
-    """ Returns libraries for all the exporters which match the given expression """
-  def libs(exporter = "*", sep = " ")
-    f = linker(exporter, sep).map do |arg|
-      if arg.match(/^[-\/][Ll]/) then
-        arg
-      end
-    end
-    f.join(" ")
-  end
-
-    """ Returns link flags for all the exporters which match the given expression """
-  def defines(configuration = "*", exporter = "*", sep = " ", prefix = "-D")
-    r = configuration_attribute("defines", exporter).select do |k,v| 
-      configuration == "*" or k.match(configuration) or k == configuration
-    end.values.join("\n")
-
-    #r = split_and_concat_uniq(r.values, "\n")
-
-    r.split(/\s+/).map { |v| prefix + v }.join(sep)
-  end
-
-    """ Returns all configuration names """
-  def configurations(exporter = "*")
-    r = Array.new
-    export_formats(exporter).each do |f|
-      REXML::XPath.each(f, "//CONFIGURATION") do |c|
-        r.push_unique c.attributes["name"]
-      end
-    end
-    r
-  end
-
-  private
-    """ Returns link flags for all the exporters which match the given expression """
-  def linker(exporter = "*", sep = " ")
-    
-    f = Array.new
-
-
-    configuration_attribute("libraryPath", exporter).values.each do |libpath|
-      libpath.split(/\n/).each do |p|
-        p.strip!
-        p.gsub!(/\/*$/, "")
-
-#        p.gsub!(/^\${([^}]*)}/, "$(\\1)")
-        if p.match(/^\$[\({].*[\)}]$/) then next end
-
-        if p.match('[^A-Za-z0-9\\\\_/\${}\(\)]') then 
-          f.push_unique "-L "+p.doublequote 
-        else
-          f.push_unique "-L#{p}"
-        end
-      end
-    end
-
-    f += attribute("extraLinkerFlags", exporter).values
-
-    f.delete("")
-    f
-  end
-
-    """ Returns a list of files """
-  def files(cond="")
-    r = Array.new
-    REXML::XPath.each(@file, "//FILE" + (cond == "" ? "" : "["+cond+"]")) do |f|
-      r.push f.attributes["file"]
-    end
-    return r
-  end  
-
-  def export_formats(exporter = "*")
-    expr = "//EXPORTFORMATS/*"
-    if exporter != "*" then
-      expr += "[contains(name(),'"+exporter.gsub("*","")+"')]"
-    end
-    REXML::XPath.match(@file, expr)
-  end
-
-  def attribute(name, exporter = "*")
-    r = Hash.new
-    export_formats(exporter).each do |f|
-      n = f.attributes[name]
-      if n != nil then r[f.name] = n end
-    end
-    r
-  end
-
-  def configuration_attribute(name, exporter = "*")
-    r = Hash.new
-    export_formats(exporter).each do |f|
-      REXML::XPath.each(f, "//CONFIGURATION") do |c|
-        cfgname = c.attributes["name"]
-        a = c.attributes[name]
-        if a != nil then
-          if r.has_key?(cfgname) then
-            a = split_and_concat_uniq(r[cfgname]+"\n"+a, "\n")
-          end
-          r[cfgname] = a
-        end
-      end
-    end
-    r
-  end
-
 end
 
-
-settings = CLI.new do
-    switch :debug, :short  => :d, :description => 'debug messages'
-    argument :input_file,   :short => :i, :description => 'input file' #, :default => "/mnt/tmpdata/Sources/ctrlr/Builds/VST/Ctrlr_Plugin_VST.jucer"
-    argument :output_file, :short => :o, :required => false, :description => 'output file'
-end.parse! do |settings|
-  fail "No such file '#{settings.input_file}'" unless File.exist? String(settings.input_file)
-end
-
-
-
-dir = File.dirname(settings.input_file)
-name = File.basename(settings.input_file, ".jucer")
-infile = File.basename(settings.input_file.to_s)
-
-if settings.output_file then
-  outfile = settings.output_file.gsub(/#name/, name)
-else
-  outfile = "Makefile.#{name}"
-end
-
-$stderr.puts "Entering directory #{dir} ..."
-
-Dir.chdir dir
-
-$stderr.puts "Reading '#{infile}' ..."
-
-myfile = JucerFile.new
-myfile.read infile 
-
-$stderr.puts "Writing '#{outfile}' ..."
-
-of = MakeFile.clone myfile
-of.write(File.new(dir+"/"+outfile, "r+", 0644))
-
-$stderr.puts "To run: make -C #{dir} -f #{outfile}"
-
-
-if settings.debug then
-  $stdout.puts "sources: "+ myfile.sources.flatten.join(" ")
-  $stdout.puts "targets: "+ myfile.targets.join(" ")
-  $stdout.puts "project type: " +myfile.project_type.to_s
-  $stdout.puts "defines: "+ myfile.defines("Release", "VS*")
-  $stdout.puts "compile flags: "+myfile.compile_flags("*LIN*")
-  $stdout.puts "link flags: "+myfile.link_flags("*LIN*")
-  $stdout.puts "configurations: "+myfile.configurations.join(", ")
-
-  #myfile.write($stdout)
-end
+main
---
+++ rb/lib/jucerfile.rb
@@ -0,0 +1,163 @@
+
+""" JucerFile -------------------------------------------------------------- """
+""" ------------------------------------------------------------------------ """
+class JucerFile < BuildFile
+  attr_accessor :file
+
+  """ initialize(filename) """
+#  def initialize
+#    super.initialize
+#    if filename != nil then
+#      read filename
+#    end
+#  end
+
+  def read(filename)
+    @file = REXML::Document.new File.new(filename)
+  end
+
+  def write(o=$stdout)
+    @file.write(o)
+    $stdout << "\n"
+  end
+
+  def targets
+    @file.elements.to_a("/JUCERPROJECT").map { |e| e.attribute("name").to_s }
+  end
+
+  def project_type
+    ProjectType.new @file.elements.to_a("/JUCERPROJECT")[0].attributes["projectType"]
+  end
+
+    """ Returns a list of source files """
+  def sources
+    h = Hash.new
+    h[targets[0]] = files "@compile=1" # and @resource=0"
+    return h
+  end
+
+    """ Returns a list of ressource files """
+  def resources
+    files "@resource=0"
+  end
+
+    """ Returns compile flags for all the exporters which match the given expression """
+  def compile_flags(exporter = "*", sep = " ")
+    split_and_concat_uniq attribute("extraCompilerFlags", exporter).values, sep
+  end
+
+    """ Returns link flags for all the exporters which match the given expression """
+  def link_flags(exporter = "*", sep = " ")
+    f = linker(exporter, sep).map do |arg|
+      if not arg.match(/^[-\/][Ll]/) then
+        arg
+      end
+    end
+    f.join(" ")
+  end
+
+    """ Returns libraries for all the exporters which match the given expression """
+  def libs(exporter = "*", sep = " ")
+    f = linker(exporter, sep).map do |arg|
+      if arg.match(/^[-\/][Ll]/) then
+        arg
+      end
+    end
+    f.join(" ")
+  end
+
+    """ Returns link flags for all the exporters which match the given expression """
+  def defines(configuration = "*", exporter = "*", sep = " ", prefix = "-D")
+    r = configuration_attribute("defines", exporter).select do |k,v| 
+      configuration == "*" or k.match(configuration) or k == configuration
+    end.values.join("\n")
+
+    #r = split_and_concat_uniq(r.values, "\n")
+
+    r.split(/\s+/).map { |v| prefix + v }.join(sep)
+  end
+
+    """ Returns all configuration names """
+  def configurations(exporter = "*")
+    r = Array.new
+    export_formats(exporter).each do |f|
+      REXML::XPath.each(f, "//CONFIGURATION") do |c|
+        r.push_unique c.attributes["name"]
+      end
+    end
+    r
+  end
+
+  private
+    """ Returns link flags for all the exporters which match the given expression """
+  def linker(exporter = "*", sep = " ")
+    
+    f = Array.new
+
+
+    configuration_attribute("libraryPath", exporter).values.each do |libpath|
+      libpath.split(/\n/).each do |p|
+        p.strip!
+        p.gsub!(/\/*$/, "")
+
+#        p.gsub!(/^\${([^}]*)}/, "$(\\1)")
+        if p.match(/^\$[\({].*[\)}]$/) then next end
+
+        if p.match('[^A-Za-z0-9\\\\_/\${}\(\)]') then 
+          f.push_unique "-L "+p.doublequote 
+        else
+          f.push_unique "-L#{p}"
+        end
+      end
+    end
+
+    f += attribute("extraLinkerFlags", exporter).values
+
+    f.delete("")
+    f
+  end
+
+    """ Returns a list of files """
+  def files(cond="")
+    r = Array.new
+    REXML::XPath.each(@file, "//FILE" + (cond == "" ? "" : "["+cond+"]")) do |f|
+      r.push f.attributes["file"]
+    end
+    return r
+  end  
+
+  def export_formats(exporter = "*")
+    expr = "//EXPORTFORMATS/*"
+    if exporter != "*" then
+      expr += "[contains(name(),'"+exporter.gsub("*","")+"')]"
+    end
+    REXML::XPath.match(@file, expr)
+  end
+
+  def attribute(name, exporter = "*")
+    r = Hash.new
+    export_formats(exporter).each do |f|
+      n = f.attributes[name]
+      if n != nil then r[f.name] = n end
+    end
+    r
+  end
+
+  def configuration_attribute(name, exporter = "*")
+    r = Hash.new
+    export_formats(exporter).each do |f|
+      REXML::XPath.each(f, "//CONFIGURATION") do |c|
+        cfgname = c.attributes["name"]
+        a = c.attributes[name]
+        if a != nil then
+          if r.has_key?(cfgname) then
+            a = split_and_concat_uniq(r[cfgname]+"\n"+a, "\n")
+          end
+          r[cfgname] = a
+        end
+      end
+    end
+    r
+  end
+
+end
