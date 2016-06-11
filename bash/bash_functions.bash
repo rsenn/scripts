422f8473eae87f86698f5f4fcc32db109eac2016
Roman Senn <roman.l.senn@gmail.com>
2016-06-11 16:02:07 +0200

    ...

--- bash/bash_functions.bash
+++ bash/bash_functions.bash
@@ -5400,16 +5400,13 @@ pid-args()
 }
 
 pid-of() {
- (: ${GREP=grep
--a
---line-buffered
---color=auto}
-if ps --help 2>&1 | $GREP -q '\-W'; then
-     PGREP_CMD='ps -aW | $GREP -i "$ARG" | awkp'
+ (: ${GREP=grep -a --line-buffered --color=auto}
+  if ps --help 2>&1 | $GREP -q '\-W'; then
+     PGREP_CMD="ps -aW | $GREP -i \"\$ARG\" | awkp"
   elif type pgrep 2>/dev/null >/dev/null; then
      PGREP_CMD='pgrep -f "$ARG"'
   else
-     PGREP_CMD='ps -ax | $GREP -i "$ARG" | awkp'
+     PGREP_CMD="ps -ax | $GREP -i \"\$ARG\" | awkp"
   fi
   for ARG in "$@"; do
     eval "$PGREP_CMD"
