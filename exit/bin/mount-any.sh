ca52abed08ac69ae419175895cb5abb5a0384102
Roman Senn <roman.l.senn@gmail.com>
2016-07-12 16:15:08 +0200

    ...

--- exit/bin/mount-any.sh
+++ exit/bin/mount-any.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 : ${MNTDIR:="$HOME/mnt"}
-: ${FUSEISO:=`which fuseiso`}
 
 while :; do
   case "$1" in
@@ -17,17 +16,13 @@ for ARG; do
   NAME=${FILE%.iso}
   MNT="$MNTDIR/$NAME"
 
-  fusermount -u "$MNT" 2>/dev/null
+  umount "$MNT" 2>/dev/null
   
   mkdir -p "$MNT"
 
   if [ ! -b "$ARG" -a ! -c "$ARG" ]; then
 
-    if [ -n "$FUSEISO" -a -f "$FUSEISO" -a -x "$FUSEISO" ]; then
-      fuseiso "$ARG" "$MNT" -o allow_other  || exit $?
-    else
       mount ${TYPE:+-t "$TYPE"} -o loop "$ARG" "$MNT" || exit $?
-    fi
   else
       mount "$ARG" "$MNT" || exit $?
   fi
