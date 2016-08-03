a123754ea622ee3e046c53af868f43efe7c5e7f8
Roman Senn <roman.l.senn@gmail.com>
2016-08-03 20:51:31 +0200

    ...

--- bash/bash_functions.bash
+++ bash/bash_functions.bash
@@ -3660,7 +3660,8 @@ list-mediapath() {
  (unset CMD
   while :; do
     case "$1" in
-      -b|-c|-d|-e|-f|-g|-h|-k|-L|-N|-O|-p|-r|-s|-x) FILTER="${FILTER:+$FILTER | }filter-test $1"; shift ;;
+      -b|-c|-d|-e|-f|-g|-h|-k|-L|-N|-O|-p|-r|-s) FILTER="${FILTER:+$FILTER | }filter-test $1"; shift ;;
+      -x|-debug|--debug) DEBUG=true;  shift ;;
       -m|--mixed|-M|--mode|-u|--unix|-w|--windows|-a|--absolute|-l|--long-name) PATHTOOL_OPTS="${PATHTOOL_OPTS:+PATHTOOL_OPTS }$1"; shift ;;
       -*) OPTS="${OPTS:+$OPTS }$1"; shift ;;
       --) shift; break ;;
@@ -3668,7 +3669,7 @@ list-mediapath() {
       esac
   done
   for ARG; do ARG=${ARG//" "/"\\ "}; ARG=${ARG//"("/"\\("};  ARG=${ARG//")"/"\\)"}; 
-   CMD="${CMD:+$CMD; }ls -1 -d $OPTS -- $MEDIAPATH/${ARG#/} 2>/dev/null"; done
+   CMD="${CMD:+$CMD; }set -- $MEDIAPATH${ARG#/} ; IFS=\$'\\n'; ls -1 -d $OPTS -- \$* 2>/dev/null"; done
 
   [ -n "$PATHTOOL_OPTS" ] && CMD="${PATHTOOL:+$PATHTOOL ${PATHTOOL_OPTS:--m}}${PATHTOOL:-realpath} \$($CMD)"
   #CMD="for ARG; do $CMD; done"
