9b3720241f6e7fc3799a8edb2c367b9ca0a90256
Roman Senn <roman.l.senn@gmail.com>
2016-09-24 21:56:26 +0200

    ...

--- sh/make-m3u.sh
+++ sh/make-m3u.sh
@@ -36,7 +36,17 @@ resolution()
   (minfo "$@"|${SED-sed} -n "/Width\s*: / { N; /Height\s*:/ { s,Width\s*:,, ; s,[^:\n0-9]\+: \+\([^:]*\)\$,\1,g; s|^\s*||; s|\([0-9]\)\s\+\([0-9]\)|\1\2|g; s|\s*pixels||g;  s|\n|x|g; p } }")
 }
 
-duration()
+
+minfo()
+{
+    #timeout ${TIMEOUT:-10} \
+   (CMD='mediainfo "$ARG" 2>&1'
+    [ $# -gt 1 ] && CMD="$CMD | addprefix \"\$ARG:\""
+    CMD="for ARG; do $CMD; done"
+    eval "$CMD")  | ${SED-sed} '#s|\s\+:\s\+|: | ; s|\s\+:\([^:]*\)$|:\1| ; s| pixels$|| ; s|: *\([0-9]\+\) \([0-9]\+\)|: \1\2|g '
+}
+
+Vduration()
 {
     ( IFS=" $IFS";
       CMD='echo "${ARG:+$ARG:}$S"'
@@ -74,31 +84,6 @@ duration()
     done )
 }
 
-mminfo()
-{
-    ( for ARG in "$@";
-    do
-        minfo "$ARG" | ${SED-sed} -n "s,^\([^:]*\):\s*\(.*\),${2:+$ARG:}\1=\2,p";
-    done | ${SED-sed} \
-        's,\s\+=,=,  ;;
-s|\([0-9]\) \([0-9]\)|\1\2|g
-/Duration/ { 
-  s|\([0-9]\) min|\1min|g
-  s|\([0-9]\) \([hdw]\)|\1\2|g
-  s|\([0-9]\) s$|\1s|
-  s|\([0-9]\+\) \([^ ]*b/s\)$|\1\2|
-}')
-}
-
-minfo()
-{
-    #timeout ${TIMEOUT:-10} \
-   (CMD='mediainfo "$ARG" 2>&1'
-    [ $# -gt 1 ] && CMD="$CMD | addprefix \"\$ARG:\""
-    CMD="for ARG; do $CMD; done"
-    eval "$CMD")  | ${SED-sed} '#s|\s\+:\s\+|: | ; s|\s\+:\([^:]*\)$|:\1| ; s| pixels$|| ; s|: *\([0-9]\+\) \([0-9]\+\)|: \1\2|g '
-}
-
 bitrate()
 {
   ( N=$#
@@ -115,14 +100,36 @@ bitrate()
     test -n "$KBPS" && echo "$KBPS" || (
     R=0
     set -- $(mminfo "$ARG"  |${SED-sed} -n "/[Oo]verall [bB]it [Rr]ate\s*=/ { s,\s*[kK]b[p/]s\$,, ;  s|\([0-9]\)\s\+\([0-9]\)|\1\2|g ; s,\.[0-9]*\$,, ; s,^[^=]*=,,; s|^|$ARG:|; p }")
-[ "$DEBUG" = true ] && echo "BR: $*" 1>&2
-   #echo "$*" 1>&2
-   # for I; do R=` expr $R + ${I##*=}` ; done 2>/dev/null
+    [ "$DEBUG" = true ] && echo "BR: $*" 1>&2
    R=${*##*:}
+   case "$R" in
+       *Mb[/p]s | *Mb?s) R=${R%%Mb?s*}; R=${R%" "}; R=$(echo "$R * 2^10"  | bc -l ); : ${R:=$(( ${R%%.*} * 1024  )) } ;; 
+       *Gb[/p]s | *Gb?s) R=${R%%Gb?s*}; R=${R%" "}; R=$(echo "$R * 2^20"  | bc -l ); : ${R:=$(( ${R%%.*} * 1024 * 1024 )) } ;; 
+       *Tb[/p]s | *Tb?s) R=${R%%Tb?s*}; R=${R%" "}; R=$(echo "$R * 2^30"  | bc -l ); : ${R:=$(( ${R%%.*} * 1024 * 1024 * 1024 )) } ;;
+       *Pb[/p]s | *Pb?s) R=${R%%Pb?s*}; R=${R%" "}; R=$(echo "$R * 2^40"  | bc -l ); : ${R:=$(( ${R%%.*} * 1024 * 1024 * 1024 * 1024 )) } ;;
+   esac
+   R=${R%.*}
    [ "$N" -gt 1 ] && R="$ARG:$R"
       echo "$R"
       )
   done )
 }
+
+mminfo()
+{
+    ( for ARG in "$@";
+    do
+        minfo "$ARG" | ${SED-sed} -n "s,^\([^:]*\):\s*\(.*\),${2:+$ARG:}\1=\2,p";
+    done | ${SED-sed} \
+        's,\s\+=,=,  ;;
+s|\([0-9]\) \([0-9]\)|\1\2|g
+/Duration/ { 
+  s|\([0-9]\) min|\1min|g
+  s|\([0-9]\) \([hdw]\)|\1\2|g
+  s|\([0-9]\) \(m\?s\)|\1\2|g
+  s|\([0-9]\+\) \([^ ]*b/s\)$|\1\2|
+}')
+}
+
 
 main "$@"
