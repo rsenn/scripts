a4dc062b4e4f865b7c0d78a6a9d106a8f3de130d
Roman Senn <roman.l.senn@gmail.com>
2016-09-30 11:48:02 +0200

    ...

--- sh/dump-eagle-script.sh
+++ sh/dump-eagle-script.sh
@@ -1,25 +1,26 @@
 #!/bin/bash
+
 MYNAME=`basename "${0%.sh}"`
 NL="
 "
+CR=$'\r'
 BS="\\"
-
 push() {
   eval 'shift;'$1'="${'$1':+$'$1'
 }$*"'
 }
 
 create_match_function() {
-	FN="$1() { case \"\$1\" in"
-	shift
-	for ARG in "${@:-"*"}"; do
-		push FN "$ARG) return 0 ;;"
-	done
-	push FN "*) return 1 ;;
+  FN="$1() { case \"\$1\" in"
+  shift
+  for ARG in "${@:-"*"}"; do
+    push FN "$ARG) return 0 ;;"
+  done
+  push FN "*) return 1 ;;
 esac
 }"
-	msg "FN: $FN"
-	eval "$FN"
+  msg "FN: $FN"
+  eval "$FN"
 }
 
 clear_state() {
@@ -33,7 +34,6 @@ clear_state() {
 $BODY"
     fi
   fi
-
   NAME=
   START_LINE=
   BODY=
@@ -49,9 +49,15 @@ count() {
   echo $#
 }
 
+dbg() {
+          [ "$DEBUG" = true ] && echo "$@" 1>&2
+}
+
+
 main() {
   while :; do
     case "$1" in
+      -x | --debug) DEBUG=true; shift ;;
       -l | --list) LIST="true"; shift ;;
       -[xX]=* | --exclude-layer=*) push EXCLUDE_LAYERS="${1#*=}"; shift ;;
       -[xX] | --exclude-layer) push EXCLUDE_LAYERS="$2"; shift 2 ;;
@@ -59,14 +65,12 @@ main() {
       *) break ;;
     esac
   done
-
+  
   if [ -f "$1" ]; then
     exec <"$1"
     shift
   fi
-
-
-
+  
   create_match_function "match_name" "$@"
   create_match_function "exclude_layers" ${EXCLUDE_LAYERS:-"----"}
   
@@ -84,58 +88,73 @@ main() {
 # eval "$MATCH_NAME"
 
   HEADER_DONE=false
-  LINENO=0
-
+  LINENUM=0
+  
   getline() {
-    : $((LINENO++))
-    read -r "$1"
+    : $((LINENUM++))
+    read -r "$1"; R=$?
+    eval "$1=\${$1%\"$CR\"}"
+    return $R
   }
-
+  
   while getline LINE; do
     while [ "$LINE" != "${LINE%"$BS"}" ]; do
       getline NEXTLINE 
       LINE="${LINE%"$BS"} ${NEXTLINE}"
     done
-       
+    
+    
     case "$LINE" in
       "") clear_state ;;
+      
       "Edit '"*)
+	  
+	  
+	  
         if [ "$LIST" != true -a "$HEADER_DONE" != true ]; then
         #echo "$HEADER"
          msg "$(IFS="$NL"; count $LAYERS) layers"
         fi
-        HEADER_DONE=true
         
+        HEADER_DONE=true
         NAME=${LINE#"Edit '"}; NAME=${NAME%%"'"*} 
-        START_LINE="$LINENO"
+        START_LINE="$LINENUM"
+        
+        if match_name "$NAME"; then
+          MATCH_COND=true
+                  		dbg "EDIT[$LINENUM]: $LINE"
+
+        else
+          MATCH_COND=false
+        fi
+        
 
         ;;
-    esac
-    
-    case "$LINE" in
+#    esac
+#    
+#    case "$LINE" in
       "Layer "*) 
-				set -- ${LINE%%";"*}; shift
-				
-				LAYER="$*"
-								
-				if exclude_layers "$LAYER"; then
-				  EXCLUDE_LAYER=true
-				else
-				  EXCLUDE_LAYER=false	
-				fi
-          
+        set -- ${LINE%%";"*}; shift
+        LAYER="$*"
+        #	  dbg "LAYER[$LINENUM]: $LAYER"
+
+        
+        if exclude_layers "$LAYER"; then
+          EXCLUDE_LAYER=true
+        else
+          EXCLUDE_LAYER=false  
+        fi
         if [ "$HEADER_DONE" != true ]; then
           set -- $LINE; shift; push LAYERS "$LAYER"  
         fi
       ;;
     esac
     
+    
     if  [ "${EXCLUDE_LAYER:-false}" = true ]; then
       continue
     fi
-    
     push BODY "$LINE"  
   done 
 }
-    
 main "$@"
