621cd8fd97b5f2d8c085ed7fcfc424289fc6a772
Roman Senn <roman.l.senn@gmail.com>
2016-10-26 06:53:07 +0000

    ...

---
+++ rb/mediathek-filme.rb
@@ -0,0 +1,46 @@
+#!/usr/bin/env ruby
+
+#require 'json/stream'
+#stream = File.open('D:/Programs/MediathekView_11_2015.09.15/Einstellungen/.mediathek3/filme.json')
+#obj = JSON::Stream::Parser.parse(stream)
+
+#def post_init
+#  @parser = JSON::Stream::Parser.new do
+#    start_document { puts "start document" }
+#    end_document   { puts "end document" }
+#    start_object   { puts "start object" }
+#    end_object     { puts "end object" }
+#    start_array    { puts "start array" }
+#    end_array      { puts "end array" }
+#    key            {|k| puts "key: #{k}" }
+#    value          {|v| puts "value: #{v}" }
+#  end
+#end
+
+#def receive_data(data)
+#  begin
+#    @parser << data
+#  rescue JSON::Stream::ParserError => e
+#    close_connection
+#  end
+#end
+
+#$LOAD_PATH.unshift File.expand_path(File.dirname(__FILE__) + '/../..')
+#$LOAD_PATH.unshift File.expand_path(File.dirname(__FILE__) + '/../../lib')
+
+file = ARGV[0] ? ARGV[0] : "D:/Programs/MediathekView_11_2015.09.15/Einstellungen/.mediathek3/filme.json"
+
+require 'yajl'
+
+unless file
+  puts "\nUsage: ruby examples/from_file.rb benchmark/subjects/item.json\n\n"
+  exit(0)
+end
+
+file_stream = File.new(file, 'r')
+
+json = Yajl::Parser.parse(file_stream)
+json.each do |hash|
+  puts hash.inspect
+end
+file_stream.close
