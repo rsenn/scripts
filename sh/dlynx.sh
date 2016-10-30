e909a1e06de7c5646072c74d86beb59bac0168c3
Roman Senn <roman.l.senn@gmail.com>
2016-10-30 22:12:23 +0100

    ...

--- sh/dlynx.sh
+++ sh/dlynx.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+push() { 
+ eval "shift;$1=\"\${$1+\"\$$1 \"}\$*\""
+}
 usage() {
  echo "Usage: $(basename "$0" .sh) [options] <urls...>
  
@@ -23,16 +26,18 @@ while :; do
 
 #: ${USER_AGENT="Mozilla/5.0 (X11; Linux x86_64; rv:25.0) Gecko/20100101 Firefox/25.0"}
 : ${USER_AGENT="Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.82 Safari/537.36"}
+  CMD="lynx -accept_all_cookies${USER_AGENT:+ -useragent=\"\$USER_AGENT\"}${COOKIES:+ -cookie_file=\"\$COOKIES\"} \$OPTS \"\$URL\" 2>/dev/null"
 
-[ "$SOURCE" = true ] || DUMP=true
+[ "$SOURCE" = true ] || { [ "$DUMP" != true ] && { DUMP=true; WIDTH=16384; }; }
 
-[ "$DUMP" = true ] || OPTS="-listonly"
-[ "$SOURCE" = true ] && OPTS="-source" DUMP="false" 
+[ "$DUMP" = true ] && push OPTS -dump -nonumbers || push OPTS -listonly
+[ -n "$WIDTH" -a "$WIDTH" -gt 0 ] && push OPTS -width="$WIDTH"
+[ "$SOURCE" = true ] && { push OPTS -source; DUMP="false" ; }
+
 #[ -n "$COOKIES" ] || OPTS="-cookie_file=\"\$COOKIES\""
 
 : ${OPTS:="-dump -nonumbers -hiddenlinks=merge"}
 
-  CMD="lynx -accept_all_cookies${USER_AGENT:+ -useragent=\"\$USER_AGENT\"}${COOKIES:+ -cookie_file=\"\$COOKIES\"} $OPTS \"\$URL\" 2>/dev/null"
   [ "$NO_PARENT" = true ] && CMD="$CMD | grep \"\${URL%/}/[^?]\""
 
 	[ "$DEBUG" = true ] && CMD="(set -x; $CMD)"
