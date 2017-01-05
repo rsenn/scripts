63032da834b08c33b61c6551af6dcd86c0b4326a
Roman Senn <roman.l.senn@gmail.com>
2017-01-05 15:41:30 +0100

    ...

--- bash/bash_functions.bash
+++ bash/bash_functions.bash
@@ -2360,12 +2360,14 @@ get-volume-list() {
 
 get-volume-path() {
   (
-  RF="[^ ]\+\s\+"
-  for ARG; do
-  df "$(  get-volume-list |sed -n "\\|\\s$ARG\$| { s|\s.*||; p }")" |sed "1d; s|^${RF}${RF}${RF}${RF}${RF}||"
-
-
-   done)
+  get-volume-list |sed "\\|\s${1}\$| { s|\s${1}\$||; \\|/|! { s|\$|/| }; p }" -n 
+#  RF="[^ ]\+\s\+"
+#  for ARG; do
+#  df "$(  get-volume-list |sed -n "\\|\\s$ARG\$| { s|\s.*||; p }")" |sed "1d; s|^${RF}${RF}${RF}${RF}${RF}||"
+#
+#
+#   done
+)
 }
 
 getuuid()
