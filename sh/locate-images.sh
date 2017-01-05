8af39ae09af295c7bae4659f6efa196cc881caf2
Roman Senn <roman.l.senn@gmail.com>
2017-01-05 07:14:39 +0100

    ...

--- sh/locate-images.sh
+++ sh/locate-images.sh
@@ -11,8 +11,13 @@ locate_images()
             done
     EXTS="bmp cin cod dcx djvu emf fig gif ico im1 im24 im8 jin jpeg jpg lss miff opc pbm pcx pgm pgx png pnm ppm psd rle rmp sgi shx svg tga tif tiff wim xcf xpm xwd"
  [ $# -le 0 ] && set -- ".*"
- for ARG; do ([ "$DEBUG" = true ] && set -x; locate -i -r "$ARG"); done |([ "$DEBUG" = true ] && set -x; grep -iE "\.($(IFS='| '; set -- $EXTS; echo "$*"))\$")
+ for ARG; do ([ "$DEBUG" = true ] && set -x; locate -i -r "$ARG"); done |(
+     [ "$DEBUG" = true ] && set -x
+     
+     grep -iE "\.($(IFS='| '; set -- $EXTS; echo "$*"))\$"
+     
      )
+  )
 }
 
 locate_images "$@"
