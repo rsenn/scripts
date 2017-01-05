1815710ac03f41df271cf762171eb7e1a33a109c
Roman Senn <roman.l.senn@gmail.com>
2017-01-05 07:13:15 +0100

    ...

--- sh/locate-images.sh
+++ sh/locate-images.sh
@@ -3,9 +3,15 @@
 locate_images()
 {
     (IFS="
- "; EXTS="bmp cin cod dcx djvu emf fig gif ico im1 im24 im8 jin jpeg jpg lss miff opc pbm pcx pgm pgx png pnm ppm psd rle rmp sgi shx svg tga tif tiff wim xcf xpm xwd"
+ "; while :; do
+      case "$1" in
+            -x | --debug) DEBUG=true; shift ;;
+            *) break ;;
+            esac
+            done
+    EXTS="bmp cin cod dcx djvu emf fig gif ico im1 im24 im8 jin jpeg jpg lss miff opc pbm pcx pgm pgx png pnm ppm psd rle rmp sgi shx svg tga tif tiff wim xcf xpm xwd"
  [ $# -le 0 ] && set -- ".*"
- for ARG; do locate -i -r "$ARG"; done |grep -iE "\.($(IFS='| '; set -- $EXTS; echo "$*"))\$"
+ for ARG; do ([ "$DEBUG" = true ] && set -x; locate -i -r "$ARG"); done |grep -iE "\.($(IFS='| '; set -- $EXTS; echo "$*"))\$"
  )
 }
 
