62f9bc0d58a467e2b9fca95bac9b5d6a271cabee
Roman Senn <roman.l.senn@gmail.com>
2017-06-17 03:05:42 +0200

    ...

--- sh/make-m3u.sh
+++ sh/make-m3u.sh
@@ -5,131 +5,133 @@ IFS="
 main()
 {
   while :; do
-    case "$1" in
-      --) shift; break ;;
-      -h | --help) usage; exit 0 ;;
-      -x | --debug) DEBUG=true ;;
-      *) break ;;
-    esac
-    shift
+  case "$1" in
+    --) shift; break ;;
+    -h | --help) usage; exit 0 ;;
+    -x | --debug) DEBUG=true ;;
+    *) break ;;
+  esac
+  shift
   done
   set -f
 
   echo "#EXTM3U"
   for ARG; do
-      D=$(duration "$ARG")
-      TITLE=${ARG##*/}
-      TITLE=${TITLE%.*}
-      TITLE=${TITLE//"_"/" "}
-      TITLE=${TITLE//" - "/"-"}
-      TITLE=$(echo "$TITLE" | ${SED-sed} 's|[^[:alnum:]][0-9]\+p[^[:alnum:]]| |g ;; s|\[| |g ;;  s|\]| |g ;; s|[ _]\+| |g ;')
-      RESOLUTION=$(resolution "$ARG")
-      BITRATE=$(bitrate "$ARG")
-      echo "#EXTINF:$D,${TITLE}${RESOLUTION:+ [$RESOLUTION]}${BITRATE:+ ${BITRATE}kbps}"
-      echo "$ARG"
+    duration "$ARG"
+    TITLE=${ARG##*/}
+    TITLE=${TITLE%.*}
+    TITLE=${TITLE//"_"/" "}
+    TITLE=${TITLE//" - "/"-"}
+    TITLE=$(echo "$TITLE" | ${SED-sed} 's|[^[:alnum:]][0-9]\+p[^[:alnum:]]| |g ;; s|\[| |g ;;  s|\]| |g ;; s|[ _]\+| |g ;')
+    resolution "$ARG"
+    bitrate "$ARG"
+    echo "#EXTINF:$DURATION,${TITLE}${RESOLUTION:+ [$RESOLUTION]}${BITRATE:+ ${BITRATE}kbps}"
+    echo "$ARG"
   done
 }
 
 
-resolution()
-{  
-  (minfo "$@"|${SED-sed} -n "/Width\s*: / { N; /Height\s*:/ { s,Width\s*:,, ; s,[^:\n0-9]\+: \+\([^:]*\)\$,\1,g; s|^\s*||; s|\([0-9]\)\s\+\([0-9]\)|\1\2|g; s|\s*pixels||g;  s|\n|x|g; p } }")
+resolution() {  
+  minfo "$1"
+  
+  RESOLUTION=$(echo "$LASTOUT" | ${SED-sed} -n "/Width\s*: / { N; /Height\s*:/ { s,Width\s*:,, ; s,[^:\n0-9]\+: \+\([^:]*\)\$,\1,g; s|^\s*||; s|\([0-9]\)\s\+\([0-9]\)|\1\2|g; s|\s*pixels||g;  s|\n|x|g; p } }")
 }
 
 
-minfo()
-{
-    #timeout ${TIMEOUT:-10} \
-   (CMD='mediainfo "$ARG" 2>&1'
+minfo() {
+#[ "$DEBUG" = true ] && echo "+ minfo $(format_args "$@")" 1>&2
+  minfo_filter() { ${SED-sed} '#s|\s\+:\s\+|: | ; s|\s\+:\([^:]*\)$|:\1| ; s| pixels$|| ; s|: *\([0-9]\+\) \([0-9]\+\)|: \1\2|g'; }
+  
+ # echo "LASTFILE: $LASTFILE ($1)" 1>&2
+  if [ -n "$LASTFILE" -a "$LASTFILE" = "$1" ]; then
+    return 0
+  fi
+  
+  ARG="$1"
+  if [ -n "$ARG" -a -f "$ARG" ]; then
+    LASTFILE="$ARG"
+    LASTOUT=$(CMD='mediainfo "$ARG" 2>&1'
+    E=${CMD//"'"/"\\'"}
+    E=${E//"\""/"'"}
+  #  [ "$DEBUG" = true ] && CMD='echo "+ '${E}'" 1>&2; '$CMD
+  
     [ $# -gt 1 ] && CMD="$CMD | addprefix \"\$ARG:\""
-    CMD="for ARG; do $CMD; done"
-    eval "$CMD")  | ${SED-sed} '#s|\s\+:\s\+|: | ; s|\s\+:\([^:]*\)$|:\1| ; s| pixels$|| ; s|: *\([0-9]\+\) \([0-9]\+\)|: \1\2|g '
+    CMD="for ARG; do $CMD; done | minfo_filter"
+    
+    eval "$CMD")  
+  fi
 }
 
 duration()
 {
-    ( IFS=" $IFS";
-      CMD='echo "${ARG:+$ARG:}$S"'
-    while :; do
-       case "$1" in
-         -m | --minute*) CMD='echo "${ARG:+$ARG:}$((S / 60))"' ; shift ;;
-       *) break ;;
-     esac
-   done
-    N="$#";
-    for ARG in "$@"
-    do
-        D=$(mminfo "$ARG" |${SED-sed} -n 's,Duration=,,p' | head -n1);
-        set -- $D;
-        S=0;
-        for PART in "$@";
-        do
-            case $PART in
-                *ms)
-                    S=$(( (S * 1000 + ${PART%ms}) / 1000))
-                ;;
-                *mn|*m | *min)
-                    PART=${PART%%[!0-9]*};
-                    S=$((S + $PART * 60))
-                ;;
-                *h)
-                    S=$((S + ${PART%h} * 3600))
-                ;;
-                *s)
-                    S=$((S + ${PART%s}))
-                ;;
-            esac;
-        done;
-        [ "$N" -gt 1 ] && eval "$CMD" || ARG= eval "$CMD"
-    done )
+  IFS=" $IFS";
+  CMD='DURATION="${ARG:+$ARG:}$S"'
+  while :; do
+    case "$1" in
+      -m | --minute*) CMD='echo "${ARG:+$ARG:}$((S / 60))"' ; shift ;;
+      *) break ;;
+    esac
+  done
+  N="$#"
+  for ARG; do
+    mminfo "$ARG"
+    D=$(echo "$MMOUT" |${SED-sed} -n 's,Duration=,,p' | head -n1)
+    set -- $D
+    S=0
+    for PART; do
+      case $PART in
+        *ms) S=$(( (S * 1000 + ${PART%ms}) / 1000)) ;;
+        *mn|*m | *min)    PART=${PART%%[!0-9]*};  S=$((S + $PART * 60)) ;;
+        *h) S=$((S + ${PART%h} * 3600)) ;;
+        *s) S=$((S + ${PART%s})) ;;
+      esac
+    done
+    [ "$N" -gt 1 ] && eval "$CMD" || ARG= eval "$CMD"
+  done
 }
 
-bitrate()
-{
-  ( N=$#
-  for ARG in "$@";
-  do
+bitrate()  {
+  N=$#
+  for ARG; do
     #EXPR="\\s[^:]*\\s\\+\\([0-9]\\+\\)\\s*kb[p/]s.*"
     EXPR=":\\s.*\s\\([0-9]\\+\\)\\s*kb[/p]s.*,"
     test $N -le 1 && EXPR=".*$EXPR" || EXPR="$EXPR:"
     EXPR="s,$EXPR\\1,p"
 
     KBPS=$(file "$ARG" |${SED-sed} -n "$EXPR")
-    #echo "EXPR='$EXPR'" 1>&2
 
-    test -n "$KBPS" && echo "$KBPS" || (
-    R=0
-    set -- $(mminfo "$ARG"  |${SED-sed} -n "/[Oo]verall [bB]it [Rr]ate\s*=/ { s,\s*[kK]b[p/]s\$,, ;  s|\([0-9]\)\s\+\([0-9]\)|\1\2|g ; s,\.[0-9]*\$,, ; s,^[^=]*=,,; s|^|$ARG:|; p }")
-    [ "$DEBUG" = true ] && echo "BR: $*" 1>&2
-   R=${*##*:}
-   case "$R" in
-       *Mb[/p]s | *Mb?s) R=${R%%Mb?s*}; R=${R%" "}; R=$(echo "$R * 2^10"  | bc -l ); : ${R:=$(( ${R%%.*} * 1024  )) } ;; 
-       *Gb[/p]s | *Gb?s) R=${R%%Gb?s*}; R=${R%" "}; R=$(echo "$R * 2^20"  | bc -l ); : ${R:=$(( ${R%%.*} * 1024 * 1024 )) } ;; 
-       *Tb[/p]s | *Tb?s) R=${R%%Tb?s*}; R=${R%" "}; R=$(echo "$R * 2^30"  | bc -l ); : ${R:=$(( ${R%%.*} * 1024 * 1024 * 1024 )) } ;;
-       *Pb[/p]s | *Pb?s) R=${R%%Pb?s*}; R=${R%" "}; R=$(echo "$R * 2^40"  | bc -l ); : ${R:=$(( ${R%%.*} * 1024 * 1024 * 1024 * 1024 )) } ;;
-   esac
-   R=${R%.*}
-   [ "$N" -gt 1 ] && R="$ARG:$R"
-      echo "$R"
-      )
-  done )
+    test -n "$KBPS" && echo "$KBPS" || {
+      R=0
+      mminfo "$ARG"
+      set -- $(echo "$MMOUT" | ${SED-sed} -n "/[Oo]verall [bB]it [Rr]ate\s*=/ { s,\s*[kK]b[p/]s\$,, ;  s|\([0-9]\)\s\+\([0-9]\)|\1\2|g ; s,\.[0-9]*\$,, ; s,^[^=]*=,,; s|^|$ARG:|; p }")
+      [ "$DEBUG" = true ] && echo "BR: $*" 1>&2
+      R=${*##*:}
+      case "$R" in
+        *Mb[/p]s | *Mb?s) R=${R%%Mb?s*}; R=${R%" "}; R=$(echo "$R * 2^10"  | bc -l ); : ${R:=$(( ${R%%.*} * 1024  )) } ;; 
+        *Gb[/p]s | *Gb?s) R=${R%%Gb?s*}; R=${R%" "}; R=$(echo "$R * 2^20"  | bc -l ); : ${R:=$(( ${R%%.*} * 1024 * 1024 )) } ;; 
+        *Tb[/p]s | *Tb?s) R=${R%%Tb?s*}; R=${R%" "}; R=$(echo "$R * 2^30"  | bc -l ); : ${R:=$(( ${R%%.*} * 1024 * 1024 * 1024 )) } ;;
+        *Pb[/p]s | *Pb?s) R=${R%%Pb?s*}; R=${R%" "}; R=$(echo "$R * 2^40"  | bc -l ); : ${R:=$(( ${R%%.*} * 1024 * 1024 * 1024 * 1024 )) } ;;
+      esac
+      R=${R%.*}
+      [ "$N" -gt 1 ] && R="$ARG:$R"
+      BITRATE="$R"
+    }      
+  done
 }
 
-mminfo()
-{
-    ( for ARG in "$@";
-    do
-        minfo "$ARG" | ${SED-sed} -n "s,^\([^:]*\):\s*\(.*\),${2:+$ARG:}\1=\2,p";
-    done | ${SED-sed} \
-        's,\s\+=,=,  ;;
-s|\([0-9]\) \([0-9]\)|\1\2|g
-/Duration/ { 
-  s|\([0-9]\) min|\1min|g
-  s|\([0-9]\) \([hdw]\)|\1\2|g
-  s|\([0-9]\) \(m\?s\)|\1\2|g
-  s|\([0-9]\+\) \([^ ]*b/s\)$|\1\2|
-}')
+format_args() {
+  F=""
+  for ARG; do F="${F:+$F }'${ARG//"'"/"\\'"}'"; done
+  echo "$F"
 }
-
+  
+mminfo_filter() { ${SED-sed} "s,\s\+=,=,  ;; s|\([0-9]\) \([0-9]\)|\1\2|g ;; /Duration/ {  s|\([0-9]\) min|\1min|g ;;  s|\([0-9]\) \([hdw]\)|\1\2|g ;; s|\([0-9]\) \(m\?s\)|\1\2|g ;; s|\([0-9]\+\) \([^ ]*b/s\)\$|\1\2| ;; }"; }
+mminfo() {
+ # [ "$DEBUG" = true ] && echo "+ mminfo $(format_args "$@")" 1>&2
+  #DEBUG=false
+  minfo "$1"
+ MMOUT=$( echo "$LASTOUT" | ${SED-sed} -n "s,^\([^:]*\):\s*\(.*\),${2:+${1}:}\1=\2,p" | ${SED-sed} "s,\s\+=,=,  ;; s|\([0-9]\) \([0-9]\)|\1\2|g ;; /Duration/ {  s|\([0-9]\) min|\1min|g ;;  s|\([0-9]\) \([hdw]\)|\1\2|g ;; s|\([0-9]\) \(m\?s\)|\1\2|g ;; s|\([0-9]\+\) \([^ ]*b/s\)\$|\1\2| ;; }")
+ 
+}
 
 main "$@"
