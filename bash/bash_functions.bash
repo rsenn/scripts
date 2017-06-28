4158058003e28fedb947159f2c4ed704f71c3c09
Roman Senn <roman.l.senn@gmail.com>
2017-06-29 00:51:43 +0200

    ...

--- bash/bash_functions.bash
+++ bash/bash_functions.bash
@@ -871,23 +871,78 @@ cpan-search()
     done )
 }
 
+chr2dec() {
+    echo "set ascii [scan \"$1\" \"%c\"]; puts -nonewline [format \"%d\" \${ascii}]" | tclsh
+}
+
+create-win-kbd-shortcut() {
+  N=0; for A; do case "$A" in
+      "ALT" | "alt") N=$((N | 0x00140000)) ;;
+      "CTRL" | "ctrl" | "CONTROL" | "control") N=$((N | 0x00120000)) ;;
+      "SHIFT" | "shift") N=$((N | 0x00110000)) ;;
+      ?) N=$(( N | $(chr2dec "$A") )) ;;
+    esac; done; printf "%d" "$N"
+    ([ "$DEBUG" = true ] && echo + create-win-kbd-shortcut "$@" "$(printf "(=0x%02x/%d)\n" "$N" "$N"))" 1>&2
+ 
+ )
+}
+
 create-qttabbar-applications() { 
- (echo "REGEDIT4"
+ ( : ${SPACE=" "}
+   : ${BS="\\"}
+   : ${NL="
+"}; kbdcode=65
+    tmpfile=$(mktemp)
+   trap 'rm -f "$tmpfile"' EXIT
+echo "REGEDIT4"
   echo
   echo '[HKEY_CURRENT_USER\Software\Quizo\QTTabBar\AppLauncher]'
-  for x; do
-    fn=${x##*/}
-    case "$x" in
-      *.lnk)  y=$(cygpath -aw "$(readshortcut "$x")");name=$(basename "$x" .lnk) ;;
-      *) y=$(cygpath -aw "$x"); name=$(basename "${x%.*}"); name=${name##*Start} ;;
+  for file; do
+    file=$(realpath "$file")
+    fn=${file##*/}
+    case "$file" in
+      *.lnk)  winpath=$(cygpath -aw "$(readshortcut "$file")");name=$(basename "$file" .lnk); workdir=$(readshortcut -g "$file") ;;
+      *) winpath=$(cygpath -aw "$file"); name=$(basename "${file%.*}"); name=${name##*Start};  workdir=${file%/*} ;;
     esac 
-    case "$x" in
+    case "$file" in
       Start?*.exe) arg="%C%" ;;
       *) arg="" ;;
     esac
-   (set -- $(echo -n -e "${y}\x00${arg}\x00${y}\x00\x30\x00\x00\x00" |hexdump -C |
-      sed 's,|.*,, ; s,^\s\+,, ; s,\s\+, ,g; :lp; N; $! { b lp }; s,[[:xdigit:]][[:xdigit:]][[:xdigit:]][[:xdigit:]][[:xdigit:]][[:xdigit:]][[:xdigit:]][[:xdigit:]]\+\s*,,g; s,|[^\n]*\n,,g; s,\s\+,\n,g; p' -n)
-    echo "\"$name\"=hex(7):$(implode , <<<"$*")" )
+     #workdir=$(cygpath -aw "$workdir")
+     case "$workdir" in 
+       *:) workdir="$workdir/" ;;
+     esac
+     workdir=${workdir//"/"/"\\\\"}
+     winkbd=$(list SHIFT ALT $(asc2chr "$kbdcode"))
+     if [ -n "$kbdcode" ]; then
+       [ "$kbdcode" -eq 57 ] && kbdcode=
+       kbdcode=$((kbdcode+1))
+       [ "$kbdcode" -gt 90 ] && kbdcode=49
+       key="$(create-win-kbd-shortcut $winkbd)"
+     else
+       key="0"
+     fi
+     
+     [ "$DEBUG" = true ] && echo "file='$file' name='$name' arg='$arg' winpath='$winpath' arg='$arg' workdir='$workdir' winkbd='${winkbd//$NL/$SPACE}' key='$key'" 1>&2
+     
+     echo -n -e "${winpath//$BS/$BS$BS}\x00${arg//$BS/$BS$BS}\x00${workdir//$BS/$BS$BS}\x00${key}\x00\x00\x00" >"$tmpfile"
+     [ "$DEBUG" = true ] && sed "s,\\\\,\\\\\\\\,g; s,\\x00,|,g ; s|.*|regstr='&'\n|" "$tmpfile" 1>&2
+     #hexdump -C <"$tmpfile" 1>&2
+   (set -- $( hexdump -C <"$tmpfile" |
+      sed '
+        s,|.*,,
+        s,^\s\+,,
+        s,\s\+, ,g
+        :lp
+        N
+        $! { b lp }
+        s,[[:xdigit:]][[:xdigit:]][[:xdigit:]][[:xdigit:]][[:xdigit:]][[:xdigit:]][[:xdigit:]][[:xdigit:]]\+\s*,,g
+        s,|[^\n]*\n,,g
+        s,\s\+,\n,g
+        p
+      ' -n)
+    echo "\"$name\"=hex(7):$(implode , <<<"$*")" 
+   )
   done) | unix2dos
 }
 
@@ -1050,9 +1105,8 @@ debug()
     [ "$DEBUG" = true ] && echo "DEBUG: $@" 1>&2
 }
 
-dec-to-hex()
-{
-    printf "%08x\n" "$1"
+dec-to-hex() { 
+  (for N; do printf "${D2XPFX}%08x\n" "$N"; done)
 }
 
 dec2bin() {
