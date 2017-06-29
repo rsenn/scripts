964c97e47893793f76b33d95b27f3c06bf80e57f
Roman Senn <roman.l.senn@gmail.com>
2017-06-29 06:11:27 +0200

    ...

--- sh/functions/quote.sh
+++ sh/functions/quote.sh
@@ -1,7 +1,7 @@
 
 quote() {  (unset O; SQ="'"; DQ='"'; BS="\\"
   for A; do case "$A" in
-      *\ *) O="${O+$O }'${A//"$SQ"/"$SQ$BS$SQ$SQ"}'" ;;
+      *\ * | *[|()]*) O="${O+$O }'${A//"$SQ"/"$SQ$BS$SQ$SQ"}'" ;;
       *)  O="${O+$O }${A//"$SQ"/"$BS$SQ"}" ;;
 
     esac; done; echo "$O")
