9ec860e37f7e48ff806627063be26c5cd698579a
Roman Senn <roman.l.senn@gmail.com>
2017-06-29 06:18:07 +0200

    ...

--- bash/bash_functions.bash
+++ bash/bash_functions.bash
@@ -6036,6 +6036,14 @@ quiet()
     "$@" 2> /dev/null
 }
 
+quote() {  (unset O; SQ="'"; DQ='"'; BS="\\"
+  for A; do case "$A" in
+      *\ * | *[\|\(\)]*) O="${O+$O }'${A//"$SQ"/"$SQ$BS$SQ$SQ"}'" ;;
+      *)  A=${A//"$SQ"/"$BS$SQ"}; A=${A//"$DQ"/"$BS$DQ"}; O="${O+$O } $A" ;;
+
+    esac; done; echo "$O")
+  }
+
 rand()
 {
     local rot=$(( ${random_seed:-0xdeadbeef} & 0x1f ));
