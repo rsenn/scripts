1e525f16a8d0f41ada8ba73d440ee30b301d3232
Roman Senn <roman.l.senn@gmail.com>
2017-09-18 05:04:19 +0200

    ...

--- bash/bash_functions.bash
+++ bash/bash_functions.bash
@@ -4933,14 +4933,7 @@ min()
     echo "$i" )
 }
 
-minfo()
-{
-    #timeout ${TIMEOUT:-10} \
-   (CMD='mediainfo "$ARG" 2>&1'
-    [ $# -gt 1 ] && CMD="$CMD | addprefix \"\$ARG:\""
-    CMD="for ARG; do $CMD; done"
-    eval "$CMD")  | ${SED-sed} '#s|\s\+:\s\+|: | ; s|\s\+:\([^:]*\)$|:\1| ; s| pixels$|| ; s|: *\([0-9]\+\) \([0-9]\+\)|: \1\2|g '
-}
+
 
 #!/bin/bash
  
