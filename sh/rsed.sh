a66e0fdcf8d7a97773331cb6d88372bd397ce5d2
Roman Senn <roman.l.senn@gmail.com>
2017-09-26 05:53:10 +0200

    ...

--- sh/rsed.sh
+++ sh/rsed.sh
@@ -5,9 +5,40 @@
 # $Id: rsed.sh 575 2008-08-26 12:07:20Z enki $
 # ---------------------------------------------------------------------------
 
-pushv () 
-{ 
+addprefix() {
+ (PREFIX=$1; shift
+  CMD='echo "$PREFIX$LINE"'
+  [ $# -gt 0 ] && CMD="for LINE; do $CMD; done" || CMD="while read -r LINE; do $CMD; done"
+  eval "$CMD")
+}
+debug()
+{
+    [ "$DEBUG" = true ] && echo "DEBUG: $@" 1>&2
+}
+map()
+{
+    from=$1 to=$2;
+    shift;
+    while shift && [ "$#" -gt 0 ]; do
+        if var_isset "$from$1"; then
+            var_set "$to$1" "`var_get "$from$1"`";
+        fi;
+    done;
+    unset -v from to
+}
+pushv()
+{
     eval "shift;$1=\"\${$1+\"\$$1\${IFS%\"\${IFS#?}\"}\"}\$*\""
+}
+some()
+{
+    eval "while shift
+  do
+  case \"\`str_tolower \"\$1\"\`\" in
+    $(str_tolower "$1") ) return 0 ;;
+  esac
+  done
+  return 1"
 }
 
 # rsed [options] [files...]
@@ -22,7 +53,7 @@ rsed()
   
   while [ "$#" -gt 0 ]; do
     case "$1" in
-						-x | --debug) DEBUG=true; ;;
+            -x | --debug) DEBUG=true  ;;
       -e) 
         pushv EXPRESSIONS "$2"
         shift
@@ -48,9 +79,15 @@ rsed()
   if some 'test -d "$1"' "$@"; then
     set -- `map 'fs_recurse -f "$1"' "$@"`
   fi
-	[ "$DEBUG" = true ] && set -x
 
-  ${SED-sed} $OPTIONS `addprefix "-e$IFS" $EXPRESSIONS` "$@"
+  set -- ${SED-sed} $OPTIONS `addprefix "-e$IFS" $EXPRESSIONS` "$@"
+  [ "$DEBUG" = true ] && set -x
+
+  "$@"
   )
 }
 
+
+if [ "${0##*/}" = rsed.sh ]; then
+				rsed "$@" 
+fi
