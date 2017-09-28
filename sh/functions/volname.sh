2edb14229da6691199d7a2a1cfb56fa4ad349bf7
Roman Senn <roman.l.senn@gmail.com>
2017-09-28 14:33:11 +0200

    ...

--- sh/functions/volname.sh
+++ sh/functions/volname.sh
@@ -1,19 +1,14 @@
 volname() { 
- ( case "$(uname -o)" in
-    *Linux*) CMD='VARS=$(blkid "$ARG"); VARS="${VARS#*:}"; (eval "$VARS"; echo "$LABEL")' ;;
-   
-    *) CMD=' drive="$ARG"
+ ([ $# -gt 1 ] && ECHO='echo "$drive $NAME"' || ECHO='echo "$NAME"'
+  for ARG in "$@"; do
+      drive="$ARG"
       case "$drive" in
         ?) drive="$drive:/" ;;
         ?:) drive="$drive/" ;;
         *) drive=$(cygpath -m "$drive") ;;
       esac  
       drive=$(cygpath -m "$drive")
-      NAME=$(cmd /c "vol ${drive%%/*}" | sed -n "/Volume in drive/ s,.* is ,,p")
-      eval "$ECHO"' ;;
-      esac
- [ $# -gt 1 ] && ECHO='echo "$drive $NAME"' || ECHO='echo "$NAME"'
-  CMD='for ARG in "$@"; do '$CMD'; done'
-  eval "$CMD"
-)    
+			NAME=$("${COMSPEC//"\\"/"/"}" /c "vol ${drive%%/*}" | sed -n ' s,\x84,, ;  s,\r$,, ; s,.*\sist\?\s,,p')
+      eval "$ECHO"
+  done)
 }
