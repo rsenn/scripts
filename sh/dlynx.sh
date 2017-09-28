f198280d796182583d33cd2c5e8170f3e2e0fa5a
Roman Senn <roman.l.senn@gmail.com>
2017-09-28 14:02:07 +0200

    ...

--- sh/dlynx.sh
+++ sh/dlynx.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 push() { 
  eval "shift;$1=\"\${$1+\"\$$1 \"}\$*\""
@@ -11,10 +11,11 @@ usage() {
  }
 
 if [ -s ~/cookies.txt ]; then
-				echo "Found ~/cookies.txt" 1>&2
+[ "$DEBUG" = true ] && 				echo "Found ~/cookies.txt" 1>&2
 				COOKIES=~/cookies.txt
 fi
 
+[ "$DEBUG" = true ] && echo "DUMP=${DUMP:-false}" 1>&2
 while :; do
   case "$1" in
     -np | -no*parent | --no*parent)  NO_PARENT=true; shift ;; 
@@ -28,15 +29,19 @@ while :; do
     *) break ;;
  esac
  done
+[ "$DEBUG" = true ] && echo "DUMP=${DUMP:-false}" 1>&2
 
 #: ${USER_AGENT="Mozilla/5.0 (X11; Linux x86_64; rv:25.0) Gecko/20100101 Firefox/25.0"}
 : ${USER_AGENT="Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.82 Safari/537.36"}
   CMD="lynx -accept_all_cookies${USER_AGENT:+ -useragent=\"\$USER_AGENT\"}${COOKIES:+ -cookie_file=\"\$COOKIES\"} \$OPTS \"\$URL\" 2>/dev/null"
 
-[ "$SOURCE" = true ] || { [ "$DUMP" != true ] && { DUMP=true; WIDTH=16384; }; }
+[ "$SOURCE" = true ] || { [ "$DUMP" != true ] && { : DUMP=true; WIDTH=16384; }; }
+
+[ "$DEBUG" = true ] && echo "SOURCE=${SOURCE:-false}" 1>&2
+[ "$DEBUG" = true ] && echo "DUMP=${DUMP:-false}" 1>&2
 
 if [ "$SOURCE" != true -a "$DUMP" != true ]; then
-	CMD="$CMD | grep '^[^ \\t]*://'"
+	CMD="$CMD | grep \"^[^\s]*://\""
 fi
 
 [ "$DUMP" = true ] && { WIDTH=16384; push OPTS -dump -nonumbers; } || push OPTS -listonly
@@ -47,8 +52,24 @@ fi
 
 : ${OPTS:="-dump -nonumbers -hiddenlinks=merge"}
 
-  [ "$NO_PARENT" = true ] && CMD="$CMD | grep \"\${URL%/}/[^?]\""
+case "$OPTS" in
+  *-dump*) ;;
+  *) OPTS="$OPTS -dump" ;;
+esac
+case "$OPTS" in
+  *-nonumber*) ;;
+  *-dump*) OPTS="$OPTS -nonumbers" ;;
+  *) ;;
+esac
 
-	[ "$DEBUG" = true ] && CMD="(set -x; $CMD)"
-  CMD="for URL; do $CMD; done"
-  eval "$CMD"
+[ "$NO_PARENT" = true ] && CMD="$CMD | grep \"\${URL%/}/[^?]\""
+[ "$DEBUG" = true ] && echo "CMD='$CMD'" 1>&2
+
+Q='"'
+BSQ='\"'
+BSBSQ='\\\"'
+
+[ "$DEBUG" = true ] && CMD="eval \"echo CMD='${CMD//$Q/$BSQ}'\" 1>&2; $CMD"
+#[ "$DEBUG" = true ] && CMD="(set -x; $CMD)"
+CMD="for URL; do $CMD; done"
+eval "$CMD"
