a3476d9697ae4cb2c473616eca23894331a1bb4f
Roman Senn <you@example.com>
2017-10-14 02:07:56 +0200

    ...

--- sh/functions/config-enable.sh
+++ sh/functions/config-enable.sh
@@ -3,9 +3,9 @@ config-enable() {
   shift
   trap 'rm -f "$TMP"' EXIT
   TMP=`mktemp`
-  for ENTRY; do
-	  echo "\\|^$ENTRY=| s|.*|$ENTRY=y|"
-	  echo "\\|# $ENTRY is not set| s|.*|$ENTRY=y|"
+  for ENTRY in "${@%%[ =]*}"; do
+	  echo "\\|^${ENTRY#"# "}=| s|.*|${ENTRY#"# "}=y|"
+	  echo "\\|# ${ENTRY#"# "} is not set| s|.*|${ENTRY#"# "}=y|"
   done >$TMP
   sed -i -f "$TMP" "$CFG")
 }
--- sh/functions/config-module.sh
+++ sh/functions/config-module.sh
@@ -1,10 +1,11 @@
-config-disable() {
+config-enable() {
  (CFG="$1"
   shift
   trap 'rm -f "$TMP"' EXIT
   TMP=`mktemp`
   for ENTRY in "${@%%[ =]*}"; do
-	  echo "\\|^${ENTRY#"# "}=| s|.*|# ${ENTRY#"# "} is not set|"
+	  echo "\\|^${ENTRY#"# "}=| s|.*|${ENTRY#"# "}=m|"
+	  echo "\\|# ${ENTRY#"# "} is not set| s|.*|${ENTRY#"# "}=m|"
   done >$TMP
   sed -i -f "$TMP" "$CFG")
 }
