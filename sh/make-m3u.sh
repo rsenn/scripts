53d4eb37ebeafb504eb5ea1d086deb899311d3aa
Roman Senn <you@example.com>
2017-10-20 10:18:14 +0200

    ...

--- sh/make-m3u.sh
+++ sh/make-m3u.sh
@@ -83,35 +83,6 @@ duration()
   done
 }
 
-bitrate()  {
-  N=$#
-  for ARG; do
-    #EXPR="\\s[^:]*\\s\\+\\([0-9]\\+\\)\\s*kb[p/]s.*"
-    EXPR=":\\s.*\s\\([0-9]\\+\\)\\s*kb[/p]s.*,"
-    test $N -le 1 && EXPR=".*$EXPR" || EXPR="$EXPR:"
-    EXPR="s,$EXPR\\1,p"
-
-    KBPS=$(file "$ARG" |${SED-sed} -n "$EXPR")
-
-    test -n "$KBPS" && echo "$KBPS" || {
-      R=0
-      mminfo "$ARG"
-      set -- $(echo "$MMOUT" | ${SED-sed} -n "/[Oo]verall [bB]it [Rr]ate\s*=/ { s,\s*[kK]b[p/]s\$,, ;  s|\([0-9]\)\s\+\([0-9]\)|\1\2|g ; s,\.[0-9]*\$,, ; s,^[^=]*=,,; s|^|$ARG:|; p }")
-      [ "$DEBUG" = true ] && echo "BR: $*" 1>&2
-      R=${*##*:}
-      case "$R" in
-        *Mb[/p]s | *Mb?s) R=${R%%Mb?s*}; R=${R%" "}; R=$(echo "$R * 2^10"  | bc -l ); : ${R:=$(( ${R%%.*} * 1024  )) } ;; 
-        *Gb[/p]s | *Gb?s) R=${R%%Gb?s*}; R=${R%" "}; R=$(echo "$R * 2^20"  | bc -l ); : ${R:=$(( ${R%%.*} * 1024 * 1024 )) } ;; 
-        *Tb[/p]s | *Tb?s) R=${R%%Tb?s*}; R=${R%" "}; R=$(echo "$R * 2^30"  | bc -l ); : ${R:=$(( ${R%%.*} * 1024 * 1024 * 1024 )) } ;;
-        *Pb[/p]s | *Pb?s) R=${R%%Pb?s*}; R=${R%" "}; R=$(echo "$R * 2^40"  | bc -l ); : ${R:=$(( ${R%%.*} * 1024 * 1024 * 1024 * 1024 )) } ;;
-      esac
-      R=${R%.*}
-      [ "$N" -gt 1 ] && R="$ARG:$R"
-      BITRATE="$R"
-    }      
-  done
-}
-
 format_args() {
   F=""
   for ARG; do F="${F:+$F }'${ARG//"'"/"\\'"}'"; done
@@ -134,6 +105,37 @@ s|\([0-9]\) \([0-9]\)|\1\2|g
   s|\([0-9]\) \(m\?s\)|\1\2|g
   s|\([0-9]\+\) \([^ ]*b/s\)$|\1\2|
 }')
+}
+
+bitrate()
+{
+  ( N=$#
+  for ARG in "$@";
+  do
+    #EXPR="\\s[^:]*\\s\\+\\([0-9]\\+\\)\\s*kb[p/]s.*"
+    EXPR=":\\s.*\s\\([0-9]\\+\\)\\s*kb[/p]s.*,"
+    test $N -le 1 && EXPR=".*$EXPR" || EXPR="$EXPR:"
+    EXPR="s,$EXPR\\1,p"
+
+    KBPS=$(file "$ARG" |${SED-sed} -n "$EXPR")
+    #echo "EXPR='$EXPR'" 1>&2
+
+    test -n "$KBPS" && echo "$KBPS" || (
+    R=0
+    set -- $(mminfo "$ARG"  |${SED-sed} -n "/[Oo]verall [bB]it [Rr]ate\s*=/ { s,\s*[kK]b[p/]s\$,, ;  s|\([0-9]\)\s\+\([0-9]\)|\1\2|g ; s,\.[0-9]*\$,, ; s,^[^=]*=,,; s|^|$ARG:|; p }")
+    [ "$DEBUG" = true ] && echo "BR: $*" 1>&2
+   R=${*##*:}
+   case "$R" in
+       *Mb[/p]s | *Mb?s) R=${R%%Mb?s*}; R=${R%" "}; R=$(echo "$R * 2^10"  | bc -l ); : ${R:=$(( ${R%%.*} * 1024  )) } ;; 
+       *Gb[/p]s | *Gb?s) R=${R%%Gb?s*}; R=${R%" "}; R=$(echo "$R * 2^20"  | bc -l ); : ${R:=$(( ${R%%.*} * 1024 * 1024 )) } ;; 
+       *Tb[/p]s | *Tb?s) R=${R%%Tb?s*}; R=${R%" "}; R=$(echo "$R * 2^30"  | bc -l ); : ${R:=$(( ${R%%.*} * 1024 * 1024 * 1024 )) } ;;
+       *Pb[/p]s | *Pb?s) R=${R%%Pb?s*}; R=${R%" "}; R=$(echo "$R * 2^40"  | bc -l ); : ${R:=$(( ${R%%.*} * 1024 * 1024 * 1024 * 1024 )) } ;;
+   esac
+   R=${R%.*}
+   [ "$N" -gt 1 ] && R="$ARG:$R"
+      echo "$R"
+      )
+  done )
 }
 
 main "$@"
