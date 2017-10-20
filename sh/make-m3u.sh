9a69859312202cc641087d031105997e070e69e9
Roman Senn <you@example.com>
2017-10-20 10:19:26 +0200

    ...

--- sh/make-m3u.sh
+++ sh/make-m3u.sh
@@ -55,34 +55,6 @@ minfo()
 						s|\r||g; s|\s\+:\([^:]*\)$|:\1| ; s| pixels$|| ; s|: *\([0-9]\+\) \([0-9]\+\)|: \1\2|g '
 }
 
-duration()
-{
-  IFS=" $IFS";
-  CMD='DURATION="${ARG:+$ARG:}$S"'
-  while :; do
-    case "$1" in
-      -m | --minute*) CMD='echo "${ARG:+$ARG:}$((S / 60))"' ; shift ;;
-      *) break ;;
-    esac
-  done
-  N="$#"
-  for ARG; do
-    mminfo "$ARG"
-    D=$(echo "$MMOUT" |${SED-sed} -n 's,Duration=,,p' | head -n1)
-    set -- $D
-    S=0
-    for PART; do
-      case $PART in
-        *ms) S=$(( (S * 1000 + ${PART%ms}) / 1000)) ;;
-        *mn|*m | *min)    PART=${PART%%[!0-9]*};  S=$((S + $PART * 60)) ;;
-        *h) S=$((S + ${PART%h} * 3600)) ;;
-        *s) S=$((S + ${PART%s})) ;;
-      esac
-    done
-    [ "$N" -gt 1 ] && eval "$CMD" || ARG= eval "$CMD"
-  done
-}
-
 format_args() {
   F=""
   for ARG; do F="${F:+$F }'${ARG//"'"/"\\'"}'"; done
@@ -136,6 +108,44 @@ bitrate()
       echo "$R"
       )
   done )
+}
+
+duration()
+{
+    ( IFS=" $IFS";
+      CMD='echo "${ARG:+$ARG:}$S"'
+    while :; do
+       case "$1" in
+         -m | --minute*) CMD='echo "${ARG:+$ARG:}$((S / 60))"' ; shift ;;
+       *) break ;;
+     esac
+   done
+    N="$#";
+    for ARG in "$@"
+    do
+        D=$(mminfo "$ARG" |${SED-sed} -n 's,Duration=,,p' | head -n1);
+        set -- $D;
+        S=0;
+        for PART in "$@";
+        do
+            case $PART in
+                *ms)
+                    S=$(( (S * 1000 + ${PART%ms}) / 1000))
+                ;;
+                *mn|*m | *min)
+                    PART=${PART%%[!0-9]*};
+                    S=$((S + $PART * 60))
+                ;;
+                *h)
+                    S=$((S + ${PART%h} * 3600))
+                ;;
+                *s)
+                    S=$((S + ${PART%s}))
+                ;;
+            esac;
+        done;
+        [ "$N" -gt 1 ] && eval "$CMD" || ARG= eval "$CMD"
+    done )
 }
 
 main "$@"
