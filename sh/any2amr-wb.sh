b2b1631d33861d242bb19a34305afbf55590864a
Roman Senn <you@example.com>
2017-11-11 11:22:30 +0100

    ...

--- sh/any2amr-wb.sh
+++ sh/any2amr-wb.sh
@@ -16,16 +16,15 @@ for ARG; do
         cd "$DIR"
 
   #OUTPUT="${ARG%.*}.amr-wb"
-  OUTPUT="$BASE.amr-wb"
+  OUTPUT="$BASE.amr"
 
   WAV="${BASE}-$$.wav"
 
         trap 'rm -vf "$WAV"' EXIT QUIT INT TERM
 
   (set -x; mplayer  -really-quiet -noconsolecontrols -vo null -vc null ${SAMPLERATE+-af resample=$SAMPLERATE} -ao pcm:waveheader:file="$WAV" "$ARG") &&
-					(set -x; sox -S  "$WAV" -t amr-wb "$OUTPUT")
-			#		(set -x; amrwb-encoder -dtx 7 "$WAV" "$OUTPUT")
-	    #(set -x; ${FFMPEG-ffmpeg} -y -strict -2 -i "$WAV" ${FMT+-f "$FMT"} ${ACODEC:+-acodec "$ACODEC"}   ${BITRATE+-ab "${BITRATE}k"} ${SAMPLERATE:+-ar "$SAMPLERATE"} ${CHANNELS:+-ac "$CHANNELS"} "$OUTPUT")
-
+					(set -x; #sox -S  "$WAV" -t awb "$OUTPUT" ||
+                    sox -S "$WAV" -t amr-wb -c 1 -r 16000 -C 4 "$OUTPUT" ||
+                    ${FFMPEG-ffmpeg} -y -i "$WAV"  -acodec libopencore_amrwb  ${SRATE:+-ar "$SRATE"}  ${CHANNELS:+-ac "$CHANNELS"} "$OUTPUT")
   )
 done
