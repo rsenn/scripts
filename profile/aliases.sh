719924bf2ab58467895766480739bd59ca9204ef
Roman Senn <you@example.com>
2018-01-11 09:14:42 +0100

    ...

--- profile/aliases.sh
+++ profile/aliases.sh
@@ -1,4 +1,3 @@
-
 alias s='sudo'
 alias si='sudo -i'
 
@@ -6,5 +5,14 @@ alias schr='sudo chown `id -u`:`id -g`'
 alias schR='sudo chown -R `id -u`:`id -g`'
 
 alias df='df -T -x tmpfs -x rootfs'
+alias astyle='astyle --style=attach --indent=spaces=2 --unpad-paren --pad-oper --keep-one-line-{blocks,statements}'
+
+alias xargs='xargs -d "\\n"'
 alias grepdiff='grepdiff --output-matching=hunk'
 
+alias curl='curl -L -k'
+alias wget='wget --content-disposition --no-check-certificate --no-use-server-timestamps'
+
+alias lynx='lynx -accept_all_cookies -cookies'
+
+alias tar='tar -m --keep-directory-symlink'
