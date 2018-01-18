213c92277c9cab141c815260f7f675d7261334e4
Roman Senn <you@example.com>
2018-01-19 00:24:57 +0100

    ...

--- rb/convert-boot-menu.rb
+++ rb/convert-boot-menu.rb
@@ -1,43 +1,54 @@
+#!/usr/bin/env ruby
+
 require 'pp'
 require_relative 'lib/bootmenu.rb'
 
 
-from_type = nil
+require 'optparse'
 
-if ARGV.length > 0 then
-  file_name = ARGV[0]
+options = Hash.new
+OptionParser.new do |parser|
+  parser.banner = "Usage: convert-boot-menu.rb [options] <file(s)...>"
 
-  if ARGV.length > 1 then
-    from_type = ARGV[1]
-    if ARGV.length > 2 then
-      to_type = ARGV[2]
-    end
+  parser.on("-v", "--[no-]verbose", "Run verbosely") do |v|
+    options[:verbose] = v
   end
-else
-  file_name = '/mnt/PHILIPS-16G/multibootusb/syslinux.cfg'
+  parser.on("-fFROM", "--from=FROM", "From type") do |from|
+     options[:from] = from
+  end
+  parser.on("-fTO", "--to=TO", "To type") do |to|
+     options[:to] = to
+  end
+end.parse!
+
+#p options
+#p ARGV
+
+from_type = options[:from] ? options[:from].to_sym : nil
+to_type = options[:to] ? options[:to].to_sym : nil
+
+ARGV.each do |arg|
+  file_name = arg
+
+  if from_type then
+#	$stderr.puts "BootMenuParser(#{from_type}, #{file_name})"
+	m = BootMenuParser(from_type, file_name)
+  else
+	m =  SyslinuxMenu.new file_name
+  end
+
+  if to_type then
+	to_type = to_type.to_sym
+  else
+	to_type = :grub4dos
+  end
+
+  m.read
+
+  to = m.dup(to_type)
+
+  pp m.class
+  pp to.class
+
+  to.write($stdout)
 end
-
-if from_type then
-  $stderr.puts "BootMenuParser(#{from_type}, #{file_name})"
-  m = BootMenuParser(from_type, file_name)
-else
-  m =  SyslinuxMenu.new file_name
-end
-
-if to_type then
-  to_type = to_type.to_sym
-else
-  to_type = :grub4dos
-end
-
-
-m.read
-#pp m
-
-to = m.dup(to_type)
-
-pp m.class
-pp to.class
-
-to.write($stdout)
-
