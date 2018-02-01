d4d537315bfabdc5497c329536057cc656e5b095
Roman Senn <you@example.com>
2018-02-01 20:19:36 +0100

    ...

--- sh/hless.sh
+++ sh/hless.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
-source-highlight -o /dev/stdout -i Parser.c -f esc --style-file /usr/share/source-highlight/esc.style |
+case "$1" in 
+  *.md) s=todo ;;
+esac
+
+source-highlight -o /dev/stdout ${s+-s "$s"} -i "$1" -f esc --style-file /usr/share/source-highlight/esc.style |
   sed 's,^,\x1b[33m,' | 
   less -R
