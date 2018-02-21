a94979a65de7705405571527cadcadb28f1ac692
Roman Senn <roman.l.senn@gmail.com>
2018-02-21 09:37:31 +0100

    ...

--- profile/xterm-256color.sh
+++ profile/xterm-256color.sh
@@ -1,8 +1,18 @@
 
 case $TERM in
-  rxvt* | xterm* | mrxvt*)
+  rxvt* |  mrxvt*)
+    if test "`TERM=rxvt-256color; tput colors`" = 256; then
+      TERM=rxvt-256color
+    fi
+  ;;
+  screen*)
+    if test "`TERM=screen-256color; tput colors`" = 256; then
+      TERM=screen-256color
+    fi
+  ;;
+  xterm*)
     if test "`TERM=xterm-256color; tput colors`" = 256; then
       TERM=xterm-256color
     fi
- ;;
+  ;;
 esac
