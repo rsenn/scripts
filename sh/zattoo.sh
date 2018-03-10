3a2df4a0ca594e9ba5b7fb9fc1cc4cc85bfce60e
Roman Senn <you@example.com>
2018-03-10 11:22:01 +0100

    ...

--- sh/zattoo.sh
+++ sh/zattoo.sh
@@ -1,7 +1,13 @@
 #!/bin/sh
+#
+# Watch zattoo.com using mplayer or similar
+#
+# Usage: env EMAIL=enkilo@gmx.ch PASSSWORD=lala PLAYER=mplayer zattoo.sh n24_doku 
+
+
 
 : ${EMAIL="enkilo@gmx.ch"}
-: ${PASSWORD="lala"}
+: ${PASSWORD="lalala"}
 : ${PLAYER="mplayer"}
 
 exec streamlink  \
