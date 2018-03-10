096f46f07e3269084f58757aad93bdc2f16189df
Roman Senn <you@example.com>
2018-03-11 00:39:17 +0100

    ...

--- sh/zattoo.sh
+++ sh/zattoo.sh
@@ -5,7 +5,26 @@
 # Usage: env EMAIL=enkilo@gmx.ch PASSSWORD=lala PLAYER=mplayer zattoo.sh n24_doku 
 
 
-
+# German Zatto Channels:
+#
+#    3plus                hr                   prosieben            super-rtl           
+#    3sat                 hse24                puls8                swiss_1             
+#    4plus                hse24_extra          radio-bremen-tv      swr-fernsehen-bw    
+#    anixe                hse24_trend          rbb                  tele-5              
+#    anixe_hd             kabel1_doku          ric                  telezueri           
+#    ard                  kabel-eins           rtl                  tlc                 
+#    br                   kika                 rtl-2                toggo_plus          
+#    br-alpha             kinowelt             rtlnitro             tv24                
+#    channel_55           mdr-sachsen          rtl_plus             tv25                
+#    comedycentral        n24_doku             s1                   vox                 
+#    DE_arte              ndr-niedersachsen    sat1                 wdr-koeln           
+#    de_sixx              nick                 sat1gold             weltderwunder       
+#    disney               orf-1                servus_tv            zdf                 
+#    dmax                 orf-2                sf-2                 zdf-info            
+#    einsextra            phoenix              sf-info              zdfneo              
+#    einsfestival         planet               sr-fernsehen                             
+#    eotv                 pro7maxx             startv                                   
+#   
 : ${EMAIL="enkilo@gmx.ch"}
 : ${PASSWORD="lalala"}
 : ${PLAYER="mplayer"}
