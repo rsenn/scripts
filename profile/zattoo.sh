aedfc28b7e066e1e8da188281b69a466e5db7dd7
Roman Senn <you@example.com>
2018-03-26 23:02:52 +0200

    ...

--- profile/zattoo.sh
+++ profile/zattoo.sh
@@ -1,14 +1,13 @@
 EMAIL="roman.l.senn@gmail.com"
 PASSWORD="lalala"
 
-: ${PLAYER=$'mplayer\n-vf\nscale=960:540'}
-
-export EMAIL PASSWORD PLAYER
 #: ${PLAYER=$'mplayer\n-vf\nscale=640:360'}
 #: ${PLAYER=$'mplayer\n-vf\nscale=720:405'}
 #: ${PLAYER=$'mplayer\n-vf\nscale=800:450'}
 #: ${PLAYER=$'mplayer\n-vf\nscale=864:486'}
 #: ${PLAYER=$'mplayer\n-vf\nscale=960:540'}
 #: ${PLAYER=$'mplayer\n-vf\nscale=1024:576'}
-#: ${PLAYER=$'mplayer\n-vf\nscale=1280:720'}
+ ${PLAYER=$'mplayer\n-vf\nscale=1280:720'}
 #: ${PLAYER=$'mplayer\n-vf\nscale=1600:900'}
+
+export EMAIL PASSWORD PLAYER
