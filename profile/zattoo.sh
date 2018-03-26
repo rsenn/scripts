d9bc4b88e65da5180f612d1ff8118253f8b1ad05
Roman Senn <you@example.com>
2018-03-26 23:03:32 +0200

    ...

--- profile/zattoo.sh
+++ profile/zattoo.sh
@@ -7,7 +7,7 @@ PASSWORD="lalala"
 #: ${PLAYER=$'mplayer\n-vf\nscale=864:486'}
 #: ${PLAYER=$'mplayer\n-vf\nscale=960:540'}
 #: ${PLAYER=$'mplayer\n-vf\nscale=1024:576'}
- ${PLAYER=$'mplayer\n-vf\nscale=1280:720'}
+: ${PLAYER=$'mplayer\n-vf\nscale=1280:720'}
 #: ${PLAYER=$'mplayer\n-vf\nscale=1600:900'}
 
 export EMAIL PASSWORD PLAYER
