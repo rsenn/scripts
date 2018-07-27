049a976c43bd28dbf0955d6c5b7e4ae0a2cd325f
Roman Senn <roman.l.senn@gmail.com>
2018-07-27 23:40:35 +0200

    ...

--- sh/pdfopt.sh
+++ sh/pdfopt.sh
@@ -4,27 +4,75 @@
 
 # This definition is changed on install to match the
 # executable name set in the makefile
-GS_EXECUTABLE=gs
-gs="`dirname $0`/$GS_EXECUTABLE"
-if test ! -x "$gs"; then
-	gs="$GS_EXECUTABLE"
-fi
-GS_EXECUTABLE=gs
 
-OPTIONS="-dSAFER -dDELAYSAFER"
-while true
-do
-	case "$1" in
-	-?*) OPTIONS="$OPTIONS $1" ;;
-	*)  break ;;
-	esac
-	shift
-done
+THISDIR=`dirname "$0"`
 
-if [ $# -ne 2 ]; then
-	echo "Usage: `basename $0` input.pdf output.pdf" 1>&2
-	exit 1
-fi
+filesize() {
+  
+ (IFS=" " ; set -- `ls -lnd  -- "$1"`
+  echo $5)
+}
 
-#exec "$GS_EXECUTABLE" -q -dNODISPLAY $OPTIONS -- "$1" "$2"
-exec "$GS_EXECUTABLE" -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -dPDFSETTINGS=/screen -dNOPAUSE -dQUIET -dBATCH -sOutputFile="$2" "$1"
+pdfopt() {
+
+  show_ratio() {
+    IN_SIZE=`filesize "$IN"`
+    OUT_SIZE=`filesize "$OUT"`
+    RATIO=`echo "$OUT_SIZE * 100 / $IN_SIZE" | bc -l`
+    echo "${NAME:+$NAME: }$RATIO%"
+  }
+
+
+
+  GS_EXECUTABLE=gs
+  gs="`dirname $0`/$GS_EXECUTABLE"
+  if test ! -x "$gs"; then
+    gs="$GS_EXECUTABLE"
+  fi
+  GS_EXECUTABLE=gs
+
+  OPTIONS="-dSAFER -dDELAYSAFER"
+  while true
+  do
+    case "$1" in
+      -?*) OPTIONS="$OPTIONS $1" ;;
+      -i) INPLACE=true ;;
+      *)  break ;;
+    esac
+    shift
+  done
+
+  if [ $# -ne 2 ]; then
+    echo "Usage: `basename $0` [OPTIONS] input.pdf [output.pdf]" 1>&2
+    exit 1
+  fi
+
+  if [ -n "$1" -a -e "$1" ] &&
+    [ -n "$2" -a -e "$2" ]; then
+    for ARG; do
+      pdfopt "$ARG" || break
+    done
+    return $?
+  fi
+
+  CMD='"$GS_EXECUTABLE" -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -dPDFSETTINGS=/screen -dNOPAUSE -dQUIET -dBATCH -sOutputFile="$IN" "$OUT"'
+
+  CMD="$CMD; $SHOW_RATIO"
+  IN="$1"
+  if [ -n "$2" ]; then
+    OUT="$2"
+  else
+    if [ "$INPLACE" = true ]; then
+      OUT=`mktemp`
+      CMD="(set -e; $CMD)"' && mv -f -- "$OUT" "$IN"'
+    else
+      OUT="${IN%.*}.out.${IN##*.}"
+    fi
+  fi
+  a
+
+  #exec "$GS_EXECUTABLE" -q -dNODISPLAY $OPTIONS -- "$1" "$2"
+  eval "$CMD"
+}
+
+pdfopt "$@"
