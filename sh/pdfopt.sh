72889ddd5a2616dcefa5f46756d5363fe9429321
Roman Senn <roman.l.senn@gmail.com>
2018-07-28 00:57:41 +0200

    ...

--- sh/pdfopt.sh
+++ sh/pdfopt.sh
@@ -16,6 +16,9 @@ explode_1() {
  (IFS="$1"; shift; set -- $*; IFS="
 "; echo "$*")
 }
+implode() {
+  (IFS="${1:1:1}"; shift; echo "$@")
+}
 
 pdfopt() {
 
@@ -25,6 +28,11 @@ pdfopt() {
     RATIO=`echo "scale=3; $OUT_SIZE * 100 / $IN_SIZE" |  bc -l`
   }
 
+  method_binary() {
+   (eval "set -- $(method_cmd="$1"); echo \$1")
+   
+  }
+
   method_cmd() {
     case "$1" in
       gs) CMD='"$GS_EXECUTABLE" -q -dNOPAUSE -dBATCH -dSAFER -sDEVICE=pdfwrite -dCompatibilityLevel=1.3 -dPDFSETTINGS=/screen -dEmbedAllFonts=true -dSubsetFonts=true -dColorImageDownsampleType=/Bicubic -dColorImageResolution=72 -dGrayImageDownsampleType=/Bicubic -dGrayImageResolution=72 -dMonoImageDownsampleType=/Bicubic -dMonoImageResolution=72 -sOutputFile="$OUT" "$IN"' ;;
@@ -32,9 +40,13 @@ pdfopt() {
       ps2pdf) CMD='ps2pdf -dPDFSETTINGS=/ebook "$IN" "$OUT"' ;;
       *magick*) CMD='convert -density 200x200 -quality 60 -compress jpeg "$IN" "$OUT"' ;;
       qpdf) CMD='qpdf --stream-data=compress "$IN" "$OUT"' ;;
-      k2pdfopt) CMD='k2pdfopt "$IN" -o "$OUT" -ocr -ocrlang eng -dev kpw -bp -f2p -1' ;;
+      k2pdfopt) CMD='k2pdfopt "$IN" -o "$OUT" -ocr -ocrlang eng -dev kpw -bp -x' ;;
     esac
   }
+
+  ALL_METHODS=$(declare -f method_cmd | sed -n "1d; /)/ { s|^\\s*||; s|).*||; s|\\*||g; p }" )
+  [ "$DEBUG" = true ] && echo "ALL_METHODS" = $ALL_METHODS 1>&2
+
 
   GS_EXECUTABLE=gs
   gs="`dirname $0`/$GS_EXECUTABLE"
@@ -58,6 +70,8 @@ pdfopt() {
     esac
     shift
   done
+
+  [ -z "$METHODS" ] && METHODS=$ALL_METHODS
 
   [ "$DEBUG" = true ] && echo "METHODS" = $METHODS 1>&2
   N_METHODS=$(set -- $(explode_1 , $METHODS); echo $#)
