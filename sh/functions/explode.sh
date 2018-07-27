23ef44deca93ef9a45a9608d8bababfc65b186ec
Roman Senn <roman.l.senn@gmail.com>
2018-07-28 00:38:16 +0200

    ...

--- sh/functions/explode.sh
+++ sh/functions/explode.sh
@@ -1,10 +1,8 @@
-explode()
-{
- (S="$1"; shift
-  IFS="
-";
-
-  [ $# -gt 0 ] && exec <<<"$*"
-  ${SED-sed} "s|${S//\"/\\\"}|\n|g"
- )
+explode_1() {
+ (old_IFS="$IFS"; IFS="$1"; shift; set -- $*; IFS="$old_IFS"; echo "$*")
+}
+explode() {
+ [ ${#1} -le 1 ] && explode_1 "$@" || (S="$1"; shift; IFS="
+"; [ $# -gt 0 ] && exec <<<"$*"
+  ${SED-sed} "s|${S//\"/\\\"}|\n|g")
 }
--- sh/pdfopt.sh
+++ sh/pdfopt.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 # $Id: pdfopt 8773 2008-05-25 02:17:14Z alexcher $
 # Convert PDF to "optimized" form.
 
@@ -12,17 +12,27 @@ filesize() {
  (IFS=" " ; set -- `ls -lnd  -- "$1"`
   echo $5)
 }
+explode_1() {
+ (IFS="$1"; shift; set -- $*; IFS="
+"; echo "$*")
+}
 
 pdfopt() {
 
   show_ratio() {
-    IN_SIZE=`filesize "$IN"`
-    OUT_SIZE=`filesize "$OUT"`
-    RATIO=`echo "$OUT_SIZE * 100 / $IN_SIZE" | tee /dev/stderr| bc -l`
-    echo "${NAME:+$NAME: }$RATIO%"
+    : ${IN_SIZE:=`filesize "$IN"`}
+    : ${OUT_SIZE:=`filesize "$OUT"`}
+    RATIO=`echo "scale=3; $OUT_SIZE * 100 / $IN_SIZE" |  bc -l`
   }
 
-
+  method_cmd() {
+    case "$1" in
+      gs) CMD='"$GS_EXECUTABLE" -q -dNOPAUSE -dBATCH -dSAFER -sDEVICE=pdfwrite -dCompatibilityLevel=1.3 -dPDFSETTINGS=/screen -dEmbedAllFonts=true -dSubsetFonts=true -dColorImageDownsampleType=/Bicubic -dColorImageResolution=72 -dGrayImageDownsampleType=/Bicubic -dGrayImageResolution=72 -dMonoImageDownsampleType=/Bicubic -dMonoImageResolution=72 -sOutputFile="$OUT" "$IN"' ;;
+      pdftk) CMD='pdftk "$IN" output "$OUT" compress' ;;
+      ps2pdf) CMD='ps2pdf -dPDFSETTINGS=/ebook "$IN" "$OUT"' ;;
+      *magick*) CMD='convert -density 200x200 -quality 60 -compress jpeg "$IN" "$OUT"' ;;
+    esac
+  }
 
   GS_EXECUTABLE=gs
   gs="`dirname $0`/$GS_EXECUTABLE"
@@ -34,47 +44,73 @@ pdfopt() {
   OPTIONS="-dSAFER -dDELAYSAFER"
   while [ $# -gt 0 ]; do
     case "$1" in
+      --methods=* | -M=* ) METHODS=$(explode_1 , ${1#*=}) ;;
+      --methods | -M ) METHODS=$(explode_1 , ${2}); shift ;;
+      -M*) METHODS=$(explode_1 , ${1#-M}) ;;
+
       -x | -d | --debug) DEBUG=true ;;
       -i) INPLACE=true ;;
+      -r | --show-ratio) SHOW_RATIO=true ;;
       -?*) OPTIONS="$OPTIONS $1" ;;
       *)  break ;;
     esac
     shift
   done
 
+  [ "$DEBUG" = true ] && echo "METHODS" = $METHODS 1>&2
+  N_METHODS=$(set -- $(explode_1 , $METHODS); echo $#)
+  [ "$N_METHODS" -gt 0 ] || METHODS=gs
+
   if [ $# -lt 1 ]; then
     echo "Usage: `basename $0` [OPTIONS] input.pdf [output.pdf]" 1>&2
     exit 1
   fi
 
-  if [ -n "$1" -a -e "$1" ] &&
-    [ -n "$2" -a -e "$2" ]; then
-    for ARG; do
-      pdfopt "$ARG" || break
-    done
-    return $?
+  if [ -z "$LOOP_CMD" ]; then
+      LOOP_CMD='pdfopt "$ARG"; E=$?; R=$((R & E))'
+    [ "$SHOW_RATIO" = true ] && LOOP_CMD='SHOW_RATIO= '$LOOP_CMD
+    [ $N_METHODS -gt 1 ] && LOOP_CMD='for METHOD in $METHODS; do METHOD=$METHOD '$LOOP_CMD'; done; show_ratio'
+    [ "$SHOW_RATIO" = true ] && LOOP_CMD=$LOOP_CMD'; echo "${NAME:+$NAME: }$RATIO"'
+    LOOP_CMD='R=127; '$LOOP_CMD'; [ $R = 0 ] || break'
+    LOOP_CMD='for ARG; do '$LOOP_CMD'; done'
+    echo "LOOP_CMD='$LOOP_CMD'" 1>&2
+      eval "$LOOP_CMD"
+    
+      exit $?
   fi
 
-  CMD='"$GS_EXECUTABLE" -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -dPDFSETTINGS=/screen -dNOPAUSE -dQUIET -dBATCH -sOutputFile="$IN" "$OUT"'
+  CMD=
+  method_cmd "$METHOD"
+  : ${CMD:='"$GS_EXECUTABLE" -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -dPDFSETTINGS=/screen -dNOPAUSE -dQUIET -dBATCH -sOutputFile="$OUT" "$IN"'}
 
-  CMD='([ "$DEBUG" = true ] && set -x; '$CMD')'
+  CMD='([ "$DEBUG" = true ] && set -x; exec '$CMD')'
 
-  CMD="$CMD && show_ratio"
-  IN="$1"
+  [ "$SHOW_RATIO" = true ] && 
+    CMD="$CMD; R=\$?; show_ratio; echo \"\${NAME:+\$NAME: }\$RATIO\"; return \$R" ||
+    CMD="$CMD; R=\$?; show_ratio; return \$R"
+  IN="$1" 
+
   NAME=
   if [ -n "$2" ]; then
     OUT="$2"
   else
     if [ "$INPLACE" = true ]; then
-      OUT=`mktemp`
-      CMD=$CMD' && mv -f -- "$OUT" "$IN"'
+      OUT=`mktemp XXXXXX.pdf`
+      CMD=${CMD%"; return"*}'; [ $R = 0 ] && [ ${RATIO%%.*} -lt 100 ] && mv -f -- "$OUT" "$IN"; return $R'
       NAME="$IN"
     else
       OUT="${IN%.*}.out.${IN##*.}"
     fi
   fi
+
   : ${NAME:="$OUT"}
 
+  IN_SIZE=$(filesize "$IN")
+  OUT_SIZE=
+
+    #[ "$INPLACE" = true ] && CMD=${CMD%"; return"*}'; OUT=$IN; return $R'
+
+  [ "$DEBUG" = true ] && echo "CMD=$CMD" 1>&2
   #exec "$GS_EXECUTABLE" -q -dNODISPLAY $OPTIONS -- "$1" "$2"
 
 
