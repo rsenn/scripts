5424fee5711e09d2d0da7509b22524e2279ea083
Roman Senn <roman.l.senn@gmail.com>
2018-08-17 12:29:38 +0200

    ...

--- sh/mount-cifs.sh
+++ sh/mount-cifs.sh
@@ -1,12 +1,19 @@
+get-shares() {
+  smbclient  -L "$CIFSHOST" --user "$USERNAME" --no-pass 2>/dev/null | 
+  sed '1 { :lp1; /Sharename/! { N; b lp1 }; N; d }; /^\s*Server\s*Comment/ { :lp2; N; $! { b lp2 }; d  }; s,^\s*\([^ ]\+\)\s.*,\1,; /^Anonymous$/d; /\$$/d '
+}
 mount-cifs () 
 {
- : ${USERNAME="madeleine"} 
- : ${PASSWORD="mase11.02"} 
- [ "$#" -le 0 ] && set --  BCPY-170909 BOOT Daten madeleine System-reserviert w10x64
+ : ${USERNAME="roman"} 
+ : ${PASSWORD="r4eHuJ"} 
+ : ${CIFSHOST=192.168.3.195}
+
+
+ [ "$#" -le 0 ] && set --  FATBOOT NewData W7x20ALT manjaro roman
     for x ; do
         d=${MNTBASE:-$HOME/mnt}/${MNTPFX:+$MNTPFX-}"$x";
         mkdir -p "$d";
-        (set -x; sudo mount -t cifs //${CIFSHOST:-192.168.0.52}/"$x" "$d" -o uid=`id -u`,gid=`id -g`${USERNAME:+,username=$USERNAME${PASSWORD+,password=$PASSWORD}} || rmdir "$d"
+        (set -x; sudo mount -t cifs //${CIFSHOST}/"$x" "$d" -o uid=`id -u`,gid=`id -g`${USERNAME:+,username=$USERNAME${PASSWORD+,password=$PASSWORD}} || rmdir "$d"
 )
     done
 }
