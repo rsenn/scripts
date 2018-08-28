aac114ca44de8c9a44d6517b124ebae7865c64cc
Roman Senn <roman.l.senn@gmail.com>
2018-08-28 12:10:07 +0200

    ...

--- sh/functions/cleanup-desktop.sh
+++ sh/functions/cleanup-desktop.sh
@@ -4,14 +4,14 @@ cleanup-desktop() {
   links=$( ls -ltdr --time-style=+%Y%m%d -- *.lnk|${GREP-grep} "$(date +%Y%m%d|removesuffix '[0-9]')"|cut-ls-l )
   set  -- $( ls -td -- $(ls-files|${GREP-grep} -viE '(\.lnk$|\.ini$)'))
   touch "$@"
-  mv -vft "$DOCUMENTS" -- "$@" *" - Shortcut"*
+  mv -vft "$DOCUMENTS" "$@" *" - Shortcut"*
   d=$(ls -d  ../Unused* )
 
   for l in $links; do
     while :; do
       read -r -p "Move ${l##*/} to $d? " ANS
 			case "$ANS" in
-				y*|j*|Y*|J*) mv -vi -t -- "$l" ;;
+                            y*|j*|Y*|J*) mv -vi -t $(cygpath -a "$USERPROFILE")/Unused*Shortcuts*/ "$l" ;;
 				n*|N*) ;;
 				*) continue ;;
 			esac
