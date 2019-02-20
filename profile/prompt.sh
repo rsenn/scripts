3009a171431cb347b7a2b4c2def88af82cd0f795
Roman Senn <roman.l.senn@gmail.com>
2019-02-20 04:20:04 +0100

    ...

--- profile/prompt.sh
+++ profile/prompt.sh
@@ -18,7 +18,7 @@ chr2dec() {
   echo "set ascii [scan \"$1\" \"%c\"]; puts -nonewline [format \"%d\" \${ascii}]" | tclsh
 }
 colors_from_hostname() {
-  S=1; N=${#HOSTNAME}; for I in $(seq 1 $((N - 1)) ); do  S=$((S * $(chr2dec "${HOSTNAME:$I:1}") )); S=$((S + 3)); done; C1="38;5;$(( S % 224 + 16))"; S=$((S / 224));  C2="38;5;$((S % 224 + 16))    "; S=$((S / 224));  C3="38;5;$((S % 224 + 16))"
+  S=1; N=${#HOSTNAME}; for I in $(seq 0 $((N - 1)) ); do  S=$((S * $(chr2dec "${HOSTNAME:$I:1}") )); S=$((S + 3)); done; C1="38;5;$(( S % 224 + 16))"; S=$((S / 224));  C2="38;5;$((S % 224 + 16))    "; S=$((S / 224));  C3="38;5;$((S % 224 + 16))"
 } 
 case "$SYSTEM" in
   msys*) C1="1;35" C2="1;34" ;;
@@ -27,5 +27,4 @@ case "$SYSTEM" in
     colors_from_hostname
     ;;
 esac
-PS1='\[\e[${C1}m\]'${SYSTEM:-$USER}'\[\e[0m\]@\[\e[${C2}m\]'${HOSTNAME%.*}'\[\e[0m\] \[\e[${C3}m\]$(homepath -a "$PWD")\[\e[0m\]
-$ '
+PS1='\[\e[${C1}m\]'${SYSTEM:-$USER}'\[\e[0m\]@\[\e[${C2}m\]'${HOSTNAME%.*}'\[\e[0m\]:(\[\e[${C3}m\]$(homepath -a "$PWD")\[\e[0m\]) \$ '
