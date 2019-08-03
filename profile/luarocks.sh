70cb162ed83dbff198885babc742ebee16d2de24
enki <roman.l.senn@gmail.com>
2019-08-03 02:18:16 +0200

    ...

---
+++ profile/luarocks.sh
@@ -0,0 +1,15 @@
+alias luarocks=_luarocks
+
+_luarocks() {
+ (CMD="$1"
+  FILTER=""
+  case "$CMD" in
+    search) 
+      FILTER="${FILTER:+$FILTER ;; } /^[-=\\s]\+\$/d ;; /^\$/d ;; /^   [^ ]/d ;; /Search results for/d ;; /Rockspecs and source rocks:/d" 
+    ;;
+  esac
+  shift
+  for ARG; do 
+    eval "(echo 'Searching $ARG ...' 1>&2 ; command luarocks \$CMD \$ARG${FILTER:+ | sed -e '$FILTER'}) || exit \$?"
+  done)
+}
