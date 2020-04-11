470ad4e8f81a75f4a905b62bc5ff50053048e354
Roman <roman.l.senn@gmail.com>
2020-04-11 08:01:26 +0200

    ...

--- sh/functions/mount-matching.sh
+++ sh/functions/mount-matching.sh
@@ -1,19 +1,18 @@
-mount-matching()
-{
-    ( MNTDIR="/mnt";
-   [ "$UID" != 0 ] && SUDO=sudo
-	 (list-devices-by || blkid) | grep-e "$@" | {
-        IFS=" ";
-        while read -r DEV PROPERTIES; do
-            DEV=${DEV%:};
-            unset LABEL UUID TYPE;
-            eval "$PROPERTIES";
-            MNT="$MNTDIR/${LABEL:-${DEV##*/}}";
-            if ! is-mounted "$DEV" && ! is-mounted "$MNT"; then
-                $SUDO mkdir -p "$MNT";
-                echo "Mounting $DEV to $MNT ..." 1>&2;
-                $SUDO mount "$DEV" "$MNT" ${MNTOPTS:+-o "$MNTOPTS"}
-            fi;
-        done
-    } )
+mount-matching() {
+ (: ${MNTDIR="/mnt"};
+  [ "$UID" != 0 ] && SUDO=sudo
+ (list-devices-by || blkid) | grep-e "$@" | {
+    IFS=" ";
+    while read -r DEV PROPERTIES; do
+        DEV=${DEV%:};
+        unset LABEL UUID TYPE;
+        eval "$PROPERTIES";
+        MNT="$MNTDIR/${LABEL:-${DEV##*/}}";
+        if ! is-mounted "$DEV" && ! is-mounted "$MNT"; then
+            $SUDO mkdir -p "$MNT";
+            echo "Mounting $DEV to $MNT ..." 1>&2;
+            $SUDO mount "$DEV" "$MNT" ${MNTOPTS:+-o "$MNTOPTS"}
+        fi;
+    done
+  } )
 }
