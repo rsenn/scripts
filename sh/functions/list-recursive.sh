f78ed72a29b3f2b6d638a66f04239f5f15790c39
Roman <roman.l.senn@gmail.com>
2020-05-05 03:33:19 +0200

    ...

--- sh/functions/list-recursive.sh
+++ sh/functions/list-recursive.sh
@@ -1,33 +1,20 @@
-list-recursive()
-{
-    ( NL="
+list-recursive() {
+(NL="
 ";
-    unset ARGS;
-    while :; do
-        case "$1" in
-            -s | -save)
-                SAVE=true;
-                shift
-            ;;
-            -a | -o | -maxdepth | -amin | -atime | -cnewer | -fstype | -group | -iname | -iwholename | -links | -mmin | -name | -path | -wholename | -uid | -user | -fprintf | -fprint | -exec | -ok | -execdir)
-                ARGS="${ARGS:+$ARGS$NL}$1${NL}$2";
-                shift 2
-            ;;
-            -print | -and | -follow | -depth | -mount | --version | -ignore_readdir_race | -N | -false | -nogroup | -readable | -executable | -type | -delete | -print | -prune)
-                ARGS="${ARGS:+$ARGS$NL}$1";
-                shift
-            ;;
-            *)
-                break
-            ;;
-        esac;
-    done;
-    [ $# = 0 ] && set .;
-    for ARG in "$@";
-    do
-        ( cd "$ARG";
-        CMD='find . $ARGS -xdev  | while read -r FILE; do test -d "$FILE" && echo "$FILE/" || echo "$FILE"; done | ${SED-sed} "s|^\.\/||"';
-        [ "$SAVE" = true ] && CMD="$CMD | { tee .${FILENAME:-files}.${TMPEXT:-tmp}; mv -f .${FILENAME:-files}.${TMPEXT:-tmp} ${FILENAME:-files}.${EXT:-list}; echo \"Created \$PWD/${FILENAME:-files}.${EXT:-list}\" 1>&2; }";
-        eval "$CMD" );
-    done )
+  [ -n "$D" ] && set -- "$D"
+  [ $# -le 0 ] && set -- .
+  for ARG; do
+    ARG="${ARG%/}"
+    ARG="${ARG#./}"
+   (for ENTRY in "$ARG"/*; do
+      case "$ENTRY" in
+       */\*) continue ;;
+      esac
+      F="$ENTRY"   
+      F="${F#./}"
+      [ -n "$TEST" ] && ! eval "test ${TEST/\$1/\$F}" && continue
+      [ -d "$F" ] && echo "$F/" || echo  "$F"
+      D="$F"    list-recursive 
+    done)
+  done)
 }
