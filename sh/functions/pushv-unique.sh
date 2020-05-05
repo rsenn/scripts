703f0d604983e2dfffe42595b52237ebc6bd0769
Roman <roman.l.senn@gmail.com>
2020-05-05 09:53:59 +0200

    ...

--- sh/functions/pushv-unique.sh
+++ sh/functions/pushv-unique.sh
@@ -1,13 +1,7 @@
-pushv-unique()
-{
-    local v=$1 s IFS=${IFS%${IFS#?}};
-    shift;
-    for s in "$@";
-    do
-        if eval "! isin \$s \${$v}"; then
-            pushv "$v" "$s";
-        else
-            return 1;
-        fi;
-    done
+pushv-unique() {
+  local __VAR=$1 __ARG IFS=${IFS%${IFS#?}}
+  shift
+  for __ARG; do
+    eval "! isin \$__ARG \${$__VAR}" && pushv "$__VAR" "$__ARG" || return 1
+  done
 }
