1a3cab5ac96d4ccfff38d7004c985dbf81689997
Roman <roman.l.senn@gmail.com>
2020-10-24 01:28:31 +0200

    ...

--- sh/make-wine-wrapper.sh
+++ sh/make-wine-wrapper.sh
@@ -1,28 +1,36 @@
 #!/bin/sh
 
-PROG=`realpath "$1"`
-TYPE=`file "$PROG"`
+process() {
+  PROG=`realpath "$1"`
+  TYPE=`file "$PROG"`
 
-case $TYPE in
-  *:\ PE*executable*) ;;
-  *)
-    echo "$1: Not a PE executable" 1>&2
-    exit 1
-  ;;
-esac
+  case $TYPE in
+    *:\ PE*executable*) ;;
+    *)
+      echo "$1: Not a PE executable" 1>&2
+      exit 1
+    ;;
+  esac
 
-BASE=`basename "$PROG" .exe`
+  BASE=`basename "$PROG" .exe`
 
-case `id -u` in
-  0) DEST=/usr/bin/$BASE ;;
-  *) DEST=$BASE ;;
-esac
+  case `id -u` in
+    0) DEST=/usr/bin/$BASE ;;
+    *) DEST=$BASE ;;
+  esac
 
-cat >$DEST <<EOF
+  cat >$DEST <<EOF
 #!/bin/sh
 exec wine "$PROG" "\$@"
 EOF
 
-chmod +x "$DEST"
+  chmod +x "$DEST"
 
-echo "Created $DEST ." 1>&2
+  echo "Created $DEST ." 1>&2
+}
+
+main() {
+  for ARG; do process "$ARG" || return $?; done
+}
+
+main "$@"
