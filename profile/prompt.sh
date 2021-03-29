c0a1b807f9c37211c10c9afd31b4dd5ce8fef09d
Roman Senn <roman.l.senn@gmail.com>
2021-03-29 10:38:26 +0000

    ...

--- profile/prompt.sh
+++ profile/prompt.sh
@@ -18,7 +18,10 @@ chr2dec() {
   echo "set ascii [scan \"$1\" \"%c\"]; puts -nonewline [format \"%d\" \${ascii}]" | tclsh
 }
 colors_from_hostname() {
-  S=1; H=${HOSTNAME%%.*}; N=${#H}; for I in $(seq 0 $((N - 1)) ); do  S=$((S * $(chr2dec "${H:$I:1}") )); S=$((S ^ 15)); done; C1="38;5;$(( S % 224 + 16))"; S=$((S / 224));  C2="38;5;$((S % 224 + 16))"; S=$((S / 224));  C3="38;5;$((S % 224 + 16))"
+  S=1; H=${HOSTNAME%%.*}; N=${#H}; for I in $(seq 0 $((N - 1)) ); do  S=$((S * $(chr2dec "${H:$I:1}") )); S=$((S ^ 15)); done; C1="$(( S % 224 + 16))"; S=$((S / 224));  C2="$((S % 224 + 16))"; S=$((S / 224));  C3="$((S % 224 + 16))"
+  C1="38;5;${C1#-}"
+  C2="38;5;${C2#-}"
+  C3="38;5;${C3#-}"
 } 
 case "$SYSTEM" in
   msys*) C1="1;35" C2="1;34" ;;
