d6be07a178b7de9bf2a54dff97999834b25f03a8
Roman Senn <roman.l.senn@gmail.com>
2022-02-17 04:50:43 +0100

    ...

--- bash/bash_functions.bash
+++ bash/bash_functions.bash
@@ -6576,13 +6576,14 @@ resolution() {
   s|Height=\([0-9]\+\)\s*pixels| \1|g
   s|[^\n]*:\s\+\([^\n:]*\)$|\1|
   s|\r\n|\n|g
+  s|^\s\+||
   s| *\n *|x|p
 }'; while [ $# -gt 0 ] ; do case "$1" in
     -m | --mult*) CMD="echo \$(($1 * $2))"; shift ;; 
     *) break ;;
   esac
   done
-  mminfo "$@"|${SED-sed} -n "$EXPR")
+  mminfo "$@" | grep -v '^Original' | ${SED-sed} -n "$EXPR")
 }                                                                                                                                                                                                                                                                                    
                                                                                                                                                                                                                                                                                      
 
