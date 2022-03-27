2fca6af1975c3b29c0df15a63bbf34b2165432e3
Roman L. Senn <roman.l.senn@gmail.com>
2022-03-27 03:47:42 +0000

    ...

--- sh/functions/pathmunge.sh
+++ sh/functions/pathmunge.sh
@@ -1,56 +1,44 @@
 pathmunge() {
-	if [ -e /bin/grep ]; then
-		GREP=/bin/grep
-	else
-		GREP=/usr/bin/grep
-	fi
-	while :; do
-		case "$1" in -s)
-			PATHSEP="$2"
-			shift 2
-			;;
-		-v)
-			PATHVAR="$2"
-			shift 2
-			;;
-		-e)
-			EXPORT="export "
-			shift
-			;;
-		-f)
-			FORCE=true
-			shift
-			;;
-		-a)
-			AFTER=true
-			shift
-			;;
-		*) break ;;
-		esac
-	done
-	: ${PATHVAR=PATH}
-	local IFS=":"
-	: ${OS=$(uname -o | head -n1)}
-	case "$OS:$1" in
-	[Mm]sys:*[:\\]*)
-		tmp="$1"
-		shift
-		set -- $(${PATHTOOL:-msyspath} "$tmp") "$@"
-		;;
-	esac
-	IFS=" "
-	FXPR="(^|${PATHSEP-:})$1($|${PATHSEP-:})"
-	if ! eval "echo \"\${${PATHVAR}}\" | $GREP -E -q \"\$FXPR\""; then
-		if [ "$2" = after -o "$AFTER" = true ]; then
-			CMD="${EXPORT}${PATHVAR}=\"\${${PATHVAR}:+\$${PATHVAR}${PATHSEP-:}}$1\""
-		else
-			CMD="${EXPORT}${PATHVAR}=\"$1\${${PATHVAR}:+${PATHSEP-:}\$${PATHVAR}}\""
-		fi
-	fi
-	[ "$FORCE" = true ] && CMD="pathremove \"$1\"
+  if [ -e /bin/grep ]; then
+    GREP=/bin/grep
+  else
+    GREP=/usr/bin/grep
+  fi
+  while :; do
+    case "$1" in
+      -s) PATHSEP="$2"
+        shift 2 ;;
+      -v) PATHVAR="$2"
+        shift 2 ;;
+      -e) EXPORT="export "
+        shift ;;
+      -f) FORCE=true
+        shift ;;
+      -a) AFTER=true
+        shift ;;
+      *) break ;;
+    esac
+  done
+  : ${PATHVAR=PATH}
+  local IFS=":"
+  : ${OS=$(uname -o | head -n1)}
+  case "$OS:$1" in
+    [Mm]sys:*[:\\]*) tmp="$1"
+      shift
+      set -- $(${PATHTOOL:-msyspath} "$tmp") "$@" ;;
+  esac
+  IFS=" "
+  FXPR="(^|${PATHSEP-:})$1(\$|${PATHSEP-:})"
+  if ! eval "echo \"\${$PATHVAR}\" | $GREP -E -q \"\$FXPR\""; then
+    if [ "$2" = after -o "$AFTER" = true ]; then
+      CMD="$EXPORT$PATHVAR=\"\${$PATHVAR:+\$$PATHVAR${PATHSEP-:}}$1\""
+    else
+      CMD="$EXPORT$PATHVAR=\"$1\${$PATHVAR:+${PATHSEP-:}\$$PATHVAR}\""
+    fi
+  fi
+  [ "$FORCE" = true ] && CMD="pathremove \"$1\"
     $CMD"
-	#    eval "CMD=\"${CMD//\""
-	[ "$DEBUG" = true ] && eval "echo \"+ $CMD\" 1>&2"
-	${EVAL:-eval} "$CMD"
-	unset PATHVAR
+  [ "$DEBUG" = true ] && eval "echo \"+ $CMD\" 1>&2"
+  ${EVAL:-eval} "$CMD"
+  unset PATHVAR;
 }
