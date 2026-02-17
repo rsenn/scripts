ecde7917f5e66895d54d81d4add5c6f9584d147f
Roman Senn <roman.l.senn@gmail.com>
2026-02-17 23:48:47 +0100

    ...

--- bash/bash_functions.bash
+++ bash/bash_functions.bash
@@ -6258,6 +6258,14 @@ pkg-name()
     done )
 }
 
+pkg-search() {
+	for arg; do 
+    pkg search "$arg" 2>/dev/null | pacman-joinlines | sed '/^Checking\b/d; /^Sorting\b/d; /^Full Text\b/d; /^\[/d;  pkg-search,^\([^ /]*\)/\([^ ]*\),\1,'
+  done | (IFS=" $IFS"; while read -r NAME VER ARCH _ REST; do
+    printf "%-40s %-8s  %-7s  %pkg-search\n" "$NAME" "$VER" "$ARCH" "$REST"
+  done)
+}
+
 pkginst()
 {
     ( PKGS=`pkgsearch "$@"`;
